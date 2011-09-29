b4b16f0cb12a2ae9343d73ad176ee47cd8b8d1dc
kaelima <kaelima@live.se>
2011-09-29 15:35:46 +0200

    Parse SMSG_BATTLEFIELD_MGR_STATE_CHANGE and SMSG_BATTLEFIELD_MGR_ENTRY_INVITE.
    Also changed guids in MSG_BATTLEGROUND_PLAYER_POSITIONS to be displayed as guids :)

--- WowPacketParser/Parsing/Parsers/BattlegroundHandler.cs
+++ WowPacketParser/Parsing/Parsers/BattlegroundHandler.cs
@@ -16,7 +16,7 @@ namespace WowPacketParser.Parsing.Parsers
 
             for (var i = 0; i < count1; i++)
             {
-                packet.ReadInt64("[" + i + "] Player GUID");
+                packet.ReadGuid("[" + i + "] Player GUID");
                 Console.WriteLine("[" + i + "] Position: " + packet.ReadVector2());
             }
 
@@ -24,9 +24,24 @@ namespace WowPacketParser.Parsing.Parsers
 
             for (var i = 0; i < count2; i++)
             {
-                packet.ReadInt64("[" + i + "] Player GUID");
+                packet.ReadGuid("[" + i + "] Player GUID");
                 Console.WriteLine("[" + i + "] Position: " + packet.ReadVector2());
             }
+        }
+
+        [Parser(Opcode.SMSG_BATTLEFIELD_MGR_STATE_CHANGE)]
+        public static void HandleBattlefieldMgrStateChanged(Packet packet)
+        {
+            packet.ReadEnum<BattlegroundStatus>("Old status", TypeCode.UInt32);
+            packet.ReadEnum<BattlegroundStatus>("New status", TypeCode.UInt32);
+        }
+
+        [Parser(Opcode.SMSG_BATTLEFIELD_MGR_ENTRY_INVITE)]
+        public static void HandleBattlefieldMgrInviteSend(Packet packet)
+        {
+            packet.ReadInt32("Unk int32 1");
+            packet.ReadInt32("Unk int32 2");
+            packet.ReadTime("Invite lasts until");
         }
     }
 }
