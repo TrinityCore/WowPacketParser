697b10e031daf082253c0495adcae3821da6b25b
kaelima <kaelima@live.se>
2011-09-29 14:55:34 +0200

    Add BattlegroundStatus enum (for later use)

---
+++ WowPacketParser/Enums/BattlegroundStatus.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WowPacketParser.Enums
+{
+    public enum BattlegroundStatus
+    {
+        None = 0,               // first status, should mean bg is not instance
+        WaitQueue = 1,          // means bg is empty and waiting for queue
+        WaitJoin = 2,           // this means, that BG has already started and it is waiting for more players
+        InProgress = 3,         // means bg is running
+        WaitLeave = 4           // means some faction has won BG and it is ending
+    }
+}
