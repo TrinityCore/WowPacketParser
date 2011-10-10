bc17069c3c7ff6754adafc38fe61f717fbba8d25
Kaelima <kaelima@live.se>
2011-10-10 15:01:14 +0300

    Fix parsing of SMSG_ACTION_BUTTONS.

--- WowPacketParser/Parsing/Parsers/ActionBarHandler.cs
+++ WowPacketParser/Parsing/Parsers/ActionBarHandler.cs
@@ -11,20 +11,26 @@ namespace WowPacketParser.Parsing.Parsers
         [Parser(Opcode.SMSG_ACTION_BUTTONS)]
         public static void HandleInitialButtons(Packet packet)
         {
-            packet.ReadByte("Talent Spec");
+            var spec = packet.ReadByte("Talent Spec");
 
-            for (var i = 0; i < 144; i++)
+            if (spec == 2)
+                return;
+
+            if (!spec || spec == 1)
             {
-                var packed = packet.ReadInt32();
+                for (var i = 0; i < 144; i++)
+                {
+                    var packed = packet.ReadInt32();
 
-                if (packed == 0)
-                    continue;
+                    if (packed == 0)
+                        continue;
 
-                var action = packed & 0x00FFFFFF;
-                Console.WriteLine("Action " + i + ": " + action);
-
-                var type = (ActionButtonType)((packed & 0xFF000000) >> 24);
-                Console.WriteLine("Type " + i + ": " + type);
+                    var action = packed & 0x00FFFFFF;
+                    Console.WriteLine("Action " + i + ": " + action);
+    
+                    var type = (ActionButtonType)((packed & 0xFF000000) >> 24);
+                    Console.WriteLine("Type " + i + ": " + type);
+                }
             }
         }
     }
