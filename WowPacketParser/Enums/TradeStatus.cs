8c0329506a175f82fc333dd84af1917f9b348253
Spp <spp@jorge.gr>
2011-11-08 13:38:07 +0100

    3.3.5a: Add all TRADE opcodes

---
+++ WowPacketParser/Enums/TradeStatus.cs
@@ -0,0 +1,30 @@
+namespace WowPacketParser.Enums
+{
+    public enum TradeStatus
+    {
+        Busy          = 0,
+        BeginTrade    = 1,
+        OpenWindow    = 2,
+        Canceled      = 3,
+        Accept        = 4,
+        Busy2         = 5,
+        NoTarget      = 6,
+        BackToTrade   = 7,
+        Tradecomplete = 8,
+        Unk9          = 9,
+        TargetToFar   = 10,
+        WrongFaction  = 11,
+        CloseWindow   = 12,
+        Unk13         = 13,
+        IgnoringYou   = 14,
+        YouStunned    = 15,
+        TargetStunned = 16,
+        YouDead       = 17,
+        TargetDead    = 18,
+        YouLogout     = 19,
+        TargetLogout  = 20,
+        TrialAccount  = 21,
+        OnlyConjured  = 22,
+        NotEligible   = 23
+    }
+}
---
+++ WowPacketParser/Parsing/Parsers/TradeHandler.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Text;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+
+namespace WowPacketParser.Parsing.Parsers
+{
+    public static class TradeHandler
+    {
+        [Parser(Opcode.CMSG_INITIATE_TRADE)]
+        public static void HandleInitiateTrade(Packet packet)
+        {
+            packet.ReadGuid("GUID");
+        }
+
+        [Parser(Opcode.CMSG_SET_TRADE_ITEM)]
+        public static void HandleTradeItem(Packet packet)
+        {
+            packet.ReadByte("Trade Slot");
+            packet.ReadByte("Bag");
+            packet.ReadByte("Slot");
+        }
+
+        [Parser(Opcode.CMSG_CLEAR_TRADE_ITEM)]
+        public static void HandleClearTradeItem(Packet packet)
+        {
+            packet.ReadByte("Slot");
+        }
+
+        [Parser(Opcode.CMSG_SET_TRADE_GOLD)]
+        public static void HandleTradeGold(Packet packet)
+        {
+            packet.ReadUInt32("Gold");
+        }
+
+        [Parser(Opcode.SMSG_TRADE_STATUS)]
+        public static void HandleTradeStatus(Packet packet)
+        {
+            var status = packet.ReadEnum<TradeStatus>("Status", TypeCode.UInt32);
+            switch (status)
+            {
+                case TradeStatus.BeginTrade:
+                    packet.ReadGuid("GUID");
+                    break;
+                case TradeStatus.OpenWindow:
+                    packet.ReadUInt32("Trade Id");
+                    break;
+                case TradeStatus.CloseWindow:
+                    packet.ReadUInt32("Unk UInt32 1");
+                    packet.ReadByte("Unk Byte");
+                    packet.ReadUInt32("Unk UInt32 2");
+                    break;
+                case TradeStatus.OnlyConjured:
+                case TradeStatus.NotEligible:
+                    packet.ReadByte("Unk Byte");
+                    break;
+            }
+        }
+
+        [Parser(Opcode.SMSG_TRADE_STATUS_EXTENDED)]
+        public static void HandleTradeStatusExtended(Packet packet)
+        {
+            packet.ReadByte("Trader");
+            packet.ReadUInt32("Trade Id");
+            packet.ReadUInt32("Unk Slot 1");
+            packet.ReadUInt32("Unk Slot 2");
+            packet.ReadUInt32("Gold");
+            packet.ReadEntryWithName<Int32>(StoreNameType.Spell, "Spell ID");
+
+            // Hack! Can't figure this condition
+            while (packet.GetPosition() != packet.GetLength())
+            {
+                var slot = packet.ReadByte("Slot Index");
+                packet.ReadUInt32("Item Entry", slot);
+                packet.ReadUInt32("Item Display ID", slot);
+                packet.ReadUInt32("Item Count", slot);
+                packet.ReadUInt32("Item Wrapped", slot);
+                packet.ReadUInt64("Item Gift Creator GUID", slot);
+                packet.ReadUInt32("Item Perm Enchantment Id", slot);
+                for (var i = 0; i < 3; ++i)
+                    packet.ReadUInt32("Item Enchantment Id", slot, i);
+                packet.ReadGuid("Item Creator GUID", slot);
+                packet.ReadInt32("Item Spell Charges", slot);
+                packet.ReadInt32("Item Suffix Factor", slot);
+                packet.ReadInt32("Item Random Property ID", slot);
+                packet.ReadUInt32("Item Lock ID", slot);
+                packet.ReadUInt32("Item Max Durability", slot);
+                packet.ReadUInt32("Item Durability", slot);
+            }
+        }
+
+        [Parser(Opcode.CMSG_ACCEPT_TRADE)]
+        public static void HandleAcceptTrade(Packet packet)
+        {
+            packet.ReadUInt32("Unk UInt32");
+        }
+
+        [Parser(Opcode.CMSG_IGNORE_TRADE)]
+        [Parser(Opcode.CMSG_BUSY_TRADE)]
+        [Parser(Opcode.CMSG_CANCEL_TRADE)]
+        [Parser(Opcode.CMSG_UNACCEPT_TRADE)]
+        [Parser(Opcode.CMSG_BEGIN_TRADE)]
+        public static void HandleNullTrade(Packet packet)
+        {
+        }
+    }
+}
