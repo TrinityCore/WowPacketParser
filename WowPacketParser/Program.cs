e4a1afd286b55406d1e6a421c57a57f39de0d073
Spp <spp@jorge.gr>
2011-11-16 10:15:14 +0100

    Program: Use Threads to read files.

--- WowPacketParser/Program.cs
+++ WowPacketParser/Program.cs
@@ -16,6 +16,84 @@ namespace WowPacketParser
 {
     public static class Program
     {
+        private static void ReadFile(string file, string[] filters, string[] ignoreFilters, int packetNumberLow, int packetNumberHigh, int packetsToRead, DumpFormatType dumpFormat, int threads, bool sqlOutput, bool prompt)
+        {
+            string fileName = Path.GetFileName(file);
+            Console.WriteLine("{0}: Opening file", fileName);
+            Console.WriteLine("{0}: Reading packets...", fileName);
+
+            try
+            {
+                var packets = Reader.Read(file, filters, ignoreFilters, packetNumberLow, packetNumberHigh, packetsToRead, (dumpFormat == DumpFormatType.SummaryHeader));
+                if (packets.Count <= 0)
+                {
+                    Console.WriteLine("{0}: Packet count is 0", fileName);
+                    return;
+                }
+
+                if (dumpFormat == DumpFormatType.Bin || dumpFormat == DumpFormatType.Pkt)
+                {
+                    SniffType format = dumpFormat == DumpFormatType.Bin ? SniffType.Bin : SniffType.Pkt;
+                    var fileExtension = dumpFormat.ToString().ToLower();
+                    Console.WriteLine("{0}: Copying {1} packets to .{2} format...", fileName, packets.Count, fileExtension);
+
+                    var dumpFileName = Path.ChangeExtension(file, null) + "_excerpt." + fileExtension;
+                    var writer = new BinaryPacketWriter(format, dumpFileName, Encoding.ASCII);
+                    writer.Write(packets);
+                }
+                else
+                {
+                    var numberOfThreads = threads != 0 ? threads.ToString() : "a recommended number of";
+
+                    Console.WriteLine("{0}: Assumed version: {1}", fileName, ClientVersion.Build);
+                    Console.WriteLine("{0}: Parsing {1} packets with {2} threads...", fileName, packets.Count, numberOfThreads);
+
+                    Statistics.Total = (uint)packets.Count;
+
+                    Statistics.StartTime = DateTime.Now;
+                    var outFileName = Path.ChangeExtension(file, null) + "_parsed";
+                    var outLogFileName = outFileName + ".txt";
+                    var outSqlFileName = outFileName + ".sql";
+
+                    SQLStore.Initialize(outSqlFileName, sqlOutput);
+
+                    bool headersOnly = (dumpFormat == DumpFormatType.TextHeader || dumpFormat == DumpFormatType.SummaryHeader);
+                    if (threads == 0) // Number of threads is automatically choosen by the Parallel library
+                        packets.AsParallel().SetCulture().ForAll(packet =>
+                        {
+                            Handler.Parse(packet, headersOnly);
+                        });
+                    else
+                        packets.AsParallel().SetCulture().WithDegreeOfParallelism(threads).ForAll(packet =>
+                        {
+                            Handler.Parse(packet, headersOnly);
+                        });
+
+                    Console.WriteLine("{0}: Writing data to file...", fileName);
+                    SQLStore.WriteToFile();
+                    if (dumpFormat != DumpFormatType.None)
+                        Handler.WriteToFile(packets, outLogFileName);
+
+                    Statistics.EndTime = DateTime.Now;
+
+                    Console.WriteLine(Statistics.Stats(fileName));
+                    Console.WriteLine("{0}: Saved file to '{1}'", fileName, outLogFileName);
+                    Console.WriteLine();
+                    Statistics.Reset();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                EndPrompt(prompt);
+            }
+        }
+
         private static void Main(string[] args)
         {
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
@@ -138,82 +216,16 @@ namespace WowPacketParser
                 }
             }
 
-            foreach (string file in files)
-            {
-                Console.WriteLine("Opening file '{0}'", Path.GetFileName(file));
-                Console.WriteLine("Reading packets...");
-
-                try
+            if (threads == 0) // Number of threads is automatically choosen by the Parallel library
+                files.AsParallel().SetCulture().ForAll(file =>
                 {
-                    var packets = Reader.Read(file, filters, ignoreFilters, packetNumberLow, packetNumberHigh, packetsToRead, (dumpFormat == DumpFormatType.SummaryHeader));
-                    if (packets.Count <= 0)
-                    {
-                        Console.WriteLine("Packet count is 0");
-                        continue;
-                    }
-
-                    if (dumpFormat == DumpFormatType.Bin || dumpFormat == DumpFormatType.Pkt)
-                    {
-                        SniffType format = dumpFormat == DumpFormatType.Bin ? SniffType.Bin : SniffType.Pkt;
-                        var fileExtension = dumpFormat.ToString().ToLower();
-                        Console.WriteLine("Copying {0} packets to .{1} format...", packets.Count, fileExtension);
-
-                        var dumpFileName = Path.ChangeExtension(file, null) + "_excerpt." + fileExtension;
-                        var writer = new BinaryPacketWriter(format, dumpFileName, Encoding.ASCII);
-                        writer.Write(packets);
-                    }
-                    else
-                    {
-                        var numberOfThreads = threads != 0 ? threads.ToString() : "a recommended number of";
-
-                        Console.WriteLine("Assumed version: {0}", ClientVersion.Build);
-                        Console.WriteLine("Parsing {0} packets with {1} threads...", packets.Count, numberOfThreads);
-
-                        Statistics.Total = (uint)packets.Count;
-
-                        Statistics.StartTime = DateTime.Now;
-                        var outFileName = Path.ChangeExtension(file, null) + "_parsed";
-                        var outLogFileName = outFileName + ".txt";
-                        var outSqlFileName = outFileName + ".sql";
-
-                        SQLStore.Initialize(outSqlFileName, sqlOutput);
-
-                        bool headersOnly = (dumpFormat == DumpFormatType.TextHeader || dumpFormat == DumpFormatType.SummaryHeader);
-                        if (threads == 0) // Number of threads is automatically choosen by the Parallel library
-                            packets.AsParallel().SetCulture().ForAll(packet =>
-                            {
-                                Handler.Parse(packet, headersOnly);
-                            });
-                        else
-                            packets.AsParallel().SetCulture().WithDegreeOfParallelism(threads).ForAll(packet =>
-                            {
-                                Handler.Parse(packet, headersOnly);
-                            });
-
-                        Console.WriteLine("Writing data to file...");
-                        SQLStore.WriteToFile();
-                        if (dumpFormat != DumpFormatType.None)
-                            Handler.WriteToFile(packets, outLogFileName);
-
-                        Statistics.EndTime = DateTime.Now;
-
-                        Console.WriteLine(Statistics.Stats());
-                        Console.WriteLine("Saved file to '{0}'", outLogFileName);
-                        Console.WriteLine();
-                        Statistics.Reset();
-                    }
-                }
-                catch (Exception ex)
+                    ReadFile(file, filters, ignoreFilters, packetNumberLow, packetNumberHigh, packetsToRead, dumpFormat, threads, sqlOutput, prompt);
+                });
+            else
+                files.AsParallel().SetCulture().WithDegreeOfParallelism(threads).ForAll(file =>
                 {
-                    Console.WriteLine(ex.GetType());
-                    Console.WriteLine(ex.Message);
-                    Console.WriteLine(ex.StackTrace);
-                }
-                finally
-                {
-                    EndPrompt(prompt);
-                }
-            }
+                    ReadFile(file, filters, ignoreFilters, packetNumberLow, packetNumberHigh, packetsToRead, dumpFormat, threads, sqlOutput, prompt);
+                });
         }
 
         private static void EndPrompt(bool prompt)
