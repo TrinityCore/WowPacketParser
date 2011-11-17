1f9c3519ea60872b36e3bfaef5c7e4f81ac00ffb
Spp <spp@jorge.gr>
2011-11-17 14:35:39 +0100

    Missing file from one of the previous commits

---
+++ WowPacketParser/Enums/BankSlotResult.cs
@@ -0,0 +1,10 @@
+﻿namespace WowPacketParser.Enums
+{
+    public enum  BankSlotResult
+    {
+        TooMany        = 0,
+        NotEnoughMoney = 1,
+        NotBanker      = 2,
+        Ok             = 3
+    }
+}
