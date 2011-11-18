b1db503861c73f19089c01c2faeb7c69987633bf
Spp <spp@jorge.gr>
2011-11-18 16:59:04 +0100

    Correction to previous commit... 0x1FF6 --> 0x10F, not 0x106 :D

--- WowPacketParser/Enums/HighGuidType.cs
+++ WowPacketParser/Enums/HighGuidType.cs
@@ -5,9 +5,9 @@ namespace WowPacketParser.Enums
         Player          = 0x000, // Seen 0x280 for players too
         InstanceSave    = 0x104,
         Group           = 0x105,
-        Guild           = 0x106,
         BattleGround    = 0x109,
         MOTransport     = 0x10C,
+        Guild           = 0x10F,
         Item            = 0x400, // Container
         DynObject       = 0xF00, // Corpses
         GameObject      = 0xF01,
