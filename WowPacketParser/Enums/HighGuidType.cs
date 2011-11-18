7bb5ae6561b525ce0eca422d59c1ff71da193c7c
Spp <spp@jorge.gr>
2011-11-18 16:40:38 +0100

    4.4.2a: Add Guild HighGuildType

--- WowPacketParser/Enums/HighGuidType.cs
+++ WowPacketParser/Enums/HighGuidType.cs
@@ -5,6 +5,7 @@ namespace WowPacketParser.Enums
         Player          = 0x000, // Seen 0x280 for players too
         InstanceSave    = 0x104,
         Group           = 0x105,
+        Guild           = 0x106,
         BattleGround    = 0x109,
         MOTransport     = 0x10C,
         Item            = 0x400, // Container
