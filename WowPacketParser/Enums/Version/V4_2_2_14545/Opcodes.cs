117fb4c5944af14fcf655a40aee077206d16d16c
Spp <spp@jorge.gr>
2011-12-21 12:07:06 +0100

    4.2.2a: SMSG_SERVER_FIRST_ACHIEVEMENT (by weclub)

--- WowPacketParser/Enums/Version/V4_2_2_14545/Opcodes.cs
+++ WowPacketParser/Enums/Version/V4_2_2_14545/Opcodes.cs
@@ -436,6 +436,7 @@ namespace WowPacketParser.Enums.Version//.V4_2_2_14545
             {Opcode.SMSG_SELL_ITEM, 0x9CD3},
             {Opcode.SMSG_SERVERTIME, 0x01CD7},
             {Opcode.SMSG_SERVER_MESSAGE, 0xC873},
+            {Opcode.SMSG_SERVER_FIRST_ACHIEVEMENT, 0x0CDF},
             {Opcode.SMSG_SET_FLAT_SPELL_MODIFIER, 0x88D3}, // unsure: can be 0x98FE
             {Opcode.SMSG_SET_PCT_SPELL_MODIFIER, 0x98FE}, // unsure: can be 0x88D3
             {Opcode.SMSG_SET_PHASE_SHIFT, 0x204C},
