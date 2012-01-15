3a18546d1e31586cc5b54c129f8c759e13851daa
Subv2112 <s.v.h21@hotmail.com>
2012-01-15 17:30:17 -0500

    4.2.2: Fixed the CMSG MSG_QUEST_PUSH_RESULT

--- WowPacketParser/Parsing/Parsers/QuestHandler.cs
+++ WowPacketParser/Parsing/Parsers/QuestHandler.cs
@@ -837,6 +837,8 @@ namespace WowPacketParser.Parsing.Parsers
         public static void HandleQuestPushResult(Packet packet)
         {
             packet.ReadGuid("GUID");
+            if (packet.Direction == Direction.ServerToClient)
+                packet.ReadUInt32("Quest Id");
             packet.ReadEnum<QuestPartyResult>("Result", TypeCode.Byte);
         }
 
