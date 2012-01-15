c161a8db94e5e474fbf1e44349cbf7464d398a70
Subv2112 <s.v.h21@hotmail.com>
2012-01-15 17:38:17 -0500

    4.2.2: Fixed SMSG_RAID_INSTANCE_INFO

--- WowPacketParser/Parsing/Parsers/InstanceHandler.cs
+++ WowPacketParser/Parsing/Parsers/InstanceHandler.cs
@@ -142,10 +142,15 @@ namespace WowPacketParser.Parsing.Parsers
             {
                 packet.ReadEntryWithName<Int32>(StoreNameType.Map, "Map ID", i);
                 packet.ReadEnum<MapDifficulty>("Map Difficulty", TypeCode.UInt32, i);
+                if (ClientVersion.AddedInVersion(ClientVersionBuild.V4_2_2_14545))
+                    packet.ReadUInt32("Unk1", i);
                 packet.ReadGuid("Instance GUID", i);
                 packet.ReadBoolean("Expired", i);
                 packet.ReadBoolean("Extended", i);
                 packet.ReadUInt32("Reset Time", i);
+                
+                if (ClientVersion.AddedInVersion(ClientVersionBuild.V4_2_2_14545))
+                    packet.ReadUInt32("Unk2", i);
             }
         }
     }
