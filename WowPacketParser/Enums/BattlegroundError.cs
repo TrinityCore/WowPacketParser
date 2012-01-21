bd3378efa54497534655f5cc4f2e2cc5cc00e353
Drake Fish <drakefish@hotmail.com>
2012-01-21 21:09:19 +0000

    4.2.2: Added SMSG_JOINED_BATTLEGROUND_QUEUE handler and an updated BattlegroundError enum. Still needs some fixing but it reads correctly.

--- WowPacketParser/Enums/BattlegroundError.cs
+++ WowPacketParser/Enums/BattlegroundError.cs
@@ -19,4 +19,35 @@ namespace WowPacketParser.Enums
         NotAllowedWhileInRandomQueue      = -14,
         NotAllowedWhileInNotRandomQueue   = -15
     }
+
+    public enum BattlegroundError430
+    {
+        GroupJoinDeserter               = 2,
+        IcorrectPartySize               = 3,
+        TooManyQueues                   = 4,
+        NotAllowedWhileQueuedForNonRated= 5,
+        NotAllowedWhileQueuedForArena   = 6,
+        TeamHasLeftArenaQueue           = 7,
+        NotAllowedInBattleground        = 8,
+        JoinXPGain                      = 9, // error text doesn't exist ingame?
+        PlayersNotInSameRangeIndex      = 10,
+        CouldntJoinQueueInTime          = 11,
+        JoinFailedAsGroup               = 12,
+        TeamHasLeftQueue                = 13, // same message as 7
+        NotAllowedWhileInPVEQueue       = 14,
+        NotAllowedWhileInRandomQueue    = 15,
+        NotAllowedWhileInNotRandomQueue = 16,
+        BGDevOnly                       = 17,
+        BGInvitationDecline             = 18,
+        MeetingStoneNotFound            = 19,
+        WargameRequestFailed            = 20,
+        IncorrectPartySize              = 22,
+        NotAllowedOnTournamentRealm     = 23,
+        PlayersFromOtherRealmInParty    = 24,
+        QueueSuccessful                 = 30,
+        RemovedBecauseOfLevelGrant      = 33,
+        RemovedBecauseOfFactionChange   = 34,
+        JoinAsGroupFailed               = 35,
+        GroupPlayerAlreadyQueued        = 43,
+    }
 }
