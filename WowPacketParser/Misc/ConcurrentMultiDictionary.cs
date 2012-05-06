611d7464dbb25443c094dada87b3f0514018aba3
Nay <dnpd.dd@gmail.com>
2012-05-07 00:06:27 +0100

    Add a ConcurrentMultiDictionary collection that will be used for upcoming SQL building code

---
+++ WowPacketParser/Misc/ConcurrentMultiDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace WowPacketParser.Misc
+{
+    public class ConcurrentMultiDictionary<TKey, TValue> : ConcurrentDictionary<TKey, List<TValue>>
+    {
+        public bool TryAdd(TKey key, TValue value)
+        {
+            List<TValue> container;
+            if (!TryGetValue(key, out container))
+            {
+                container = new List<TValue>();
+                if (!TryAdd(key, container))
+                    return false;
+            }
+
+            container.Add(value);
+            return true;
+        }
+
+        public bool ContainsValue(TKey key, TValue value)
+        {
+            List<TValue> container;
+            return TryGetValue(key, out container) && container.Contains(value);
+        }
+
+        public List<TValue> GetValues(TKey key)
+        {
+            List<TValue> container;
+            return TryGetValue(key, out container) ? container : new List<TValue>();
+        }
+    }
+}
