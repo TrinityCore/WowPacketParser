b5505f8ffb75a85df3792f4b30a44a9ec67244ae
Nay <dnpd.dd@gmail.com>
2012-05-12 21:02:34 +0100

    Catch/handle IOExceptions thrown by Reader.Read (invalid file types and others)

--- WowPacketParser/Loading/SniffFile.cs
+++ WowPacketParser/Loading/SniffFile.cs
@@ -59,7 +59,9 @@ namespace WowPacketParser.Loading
 
                     Store.Store.Flags = _sqlOutput;
 
-                    ReadPackets();
+                    if (!ReadPackets())
+                        return;
+
                     ParsePackets();
 
                     if (_sqlOutput != SQLOutputFlags.None)
@@ -71,7 +73,8 @@ namespace WowPacketParser.Loading
                 }
                 case DumpFormatType.Pkt:
                 {
-                    ReadPackets();
+                    if (!ReadPackets())
+                        return;
 
                     if (_splitOutput)
                         SplitBinaryDump();
@@ -88,10 +91,21 @@ namespace WowPacketParser.Loading
             }
         }
 
-        private void ReadPackets()
+        private bool ReadPackets()
         {
             Trace.WriteLine(string.Format("{0}: Reading packets...", _logPrefix));
-            _packets = new LinkedList<Packet>(Reader.Read(_fileName));
+            try
+            {
+                _packets = new LinkedList<Packet>(Reader.Read(_fileName));
+                return true;
+            }
+            catch (IOException ex)
+            {
+                Trace.WriteLine(ex.Message);
+                Trace.WriteLine("Skipped.");
+                return false;
+            }
+            
         }
 
         private void ParsePackets()
