e8cdc81c2308349da839cc9ea1e5271d473f016c
Nay <dnpd.dd@gmail.com>
2012-07-22 12:24:53 +0100

    4.3.4 SMSG_RESEARCH_SETUP_HISTORY handler

---
+++ WowPacketParser/Parsing/Parsers/ArchaelogyHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+
+namespace WowPacketParser.Parsing.Parsers
+{
+    public static class ArchaelogyHandler
+    {
+        [Parser(Opcode.SMSG_RESEARCH_SETUP_HISTORY)]
+        public static void HandleResearchSetupHistory(Packet packet)
+        {
+            var count = packet.ReadBits("Count", 22);
+
+            for (int i = 0; i < count; ++i)
+            {
+                packet.ReadInt32("ResearchProject.Id", i);
+                packet.ReadInt32("Count", i);
+                packet.ReadTime("Time", i);
+            }
+        }
+    }
+}
