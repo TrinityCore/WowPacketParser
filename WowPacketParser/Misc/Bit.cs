b3902675a21d7739ad004b2a224623e03c1f9b53
Nay <dnpd.dd@gmail.com>
2012-07-28 14:27:13 +0100

    Change Bit class to struct

--- WowPacketParser/Misc/Bit.cs
+++ WowPacketParser/Misc/Bit.cs
@@ -1,13 +1,16 @@
 ﻿namespace WowPacketParser.Misc
 {
-    public class Bit
+    public struct Bit
     {
-        Bit(bool value) { Value = value; }
-        Bit(byte value) { Value = value != 0; }
-        public bool Value { get; set; }
+        private bool _value;
+
+        Bit(bool value) { _value = value; }
+        Bit(byte value) { _value = value != 0; }
+        public bool Value { get { return _value; } }
         public static implicit operator byte(Bit b) { return (byte)(b.Value == true ? 1 : 0); }
         public static implicit operator bool(Bit b) { return b.Value; }
         public static implicit operator Bit(bool b) { return new Bit(b); }
         public static implicit operator Bit(byte b) { return new Bit(b); }
+        public override string ToString() { return _value.ToString(); }
     }
 }
