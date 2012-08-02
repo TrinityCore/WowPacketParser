5d841101743af448dcab2ccb17657beb325e6867
Spp <spp@jorge.gr>
2012-08-02 13:44:48 +0200

    4.3.4: Handler for CMSG_EJECT_PASSENGER

--- WowPacketParser/Parsing/Parsers/MiscellaneousHandler.cs
+++ WowPacketParser/Parsing/Parsers/MiscellaneousHandler.cs
@@ -150,6 +150,7 @@ namespace WowPacketParser.Parsing.Parsers
         [Parser(Opcode.CMSG_DUEL_CANCELLED)]
         [Parser(Opcode.SMSG_REFER_A_FRIEND_EXPIRED)]
         [Parser(Opcode.CMSG_PLAYER_VEHICLE_ENTER)]
+        [Parser(Opcode.CMSG_EJECT_PASSENGER)]
         public static void HandleReadGuid(Packet packet)
         {
             packet.ReadGuid("GUID");
