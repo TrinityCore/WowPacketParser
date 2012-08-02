988d800753bef9b70b7ccf09900dd360ea157baf
Nay <dnpd.dd@gmail.com>
2012-08-02 16:40:51 +0100

    4.3.4 CMSG_ITEM_REFUND handler

--- WowPacketParser/Parsing/Parsers/ItemHandler.cs
+++ WowPacketParser/Parsing/Parsers/ItemHandler.cs
@@ -166,6 +166,7 @@ namespace WowPacketParser.Parsing.Parsers
         [Parser(Opcode.CMSG_ITEM_REFUND_INFO)]
         [Parser(Opcode.SMSG_READ_ITEM_OK)]
         [Parser(Opcode.SMSG_READ_ITEM_FAILED)]
+        [Parser(Opcode.CMSG_ITEM_REFUND)]
         public static void HandleReadItem(Packet packet)
         {
             packet.ReadGuid("Item GUID");
