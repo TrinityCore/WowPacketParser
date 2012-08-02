95b80f918071d976ff28f4c672732debe383a018
horn <pankrac.ja@seznam.cz>
2012-08-02 13:42:10 +0200

    Handler for CMSG_QUERY_GUILD_MEMBER_RECIPES

--- WowPacketParser/Parsing/Parsers/GuildHandler.cs
+++ WowPacketParser/Parsing/Parsers/GuildHandler.cs
@@ -632,6 +632,51 @@ namespace WowPacketParser.Parsing.Parsers
             packet.WriteGuid("GUID", guid);
         }
 
+        [Parser(Opcode.CMSG_QUERY_GUILD_MEMBER_RECIPES)]
+        public static void HandleGuildMembersRecipes(Packet packet)
+        {
+            packet.ReadInt32("Skill ID");
+            var guildGuid = new byte[8];
+            var guid = new byte[8];
+
+            guid[2] = packet.ReadBit();
+            guildGuid[1] = packet.ReadBit();
+            guid[1] = packet.ReadBit();
+            guildGuid[0] = packet.ReadBit();
+            guildGuid[6] = packet.ReadBit();
+            guid[7] = packet.ReadBit();
+            guildGuid[4] = packet.ReadBit();
+            guildGuid[3] = packet.ReadBit();
+            guildGuid[7] = packet.ReadBit();
+            guid[5] = packet.ReadBit();
+            guid[0] = packet.ReadBit();
+            guildGuid[5] = packet.ReadBit();
+            guid[3] = packet.ReadBit();
+            guid[6] = packet.ReadBit();
+            guildGuid[2] = packet.ReadBit();
+            guid[4] = packet.ReadBit();
+
+            packet.ReadXORByte(guid, 2);
+            packet.ReadXORByte(guid, 6);
+            packet.ReadXORByte(guildGuid, 4);
+            packet.ReadXORByte(guildGuid, 2);
+            packet.ReadXORByte(guid, 1);
+            packet.ReadXORByte(guildGuid, 7);
+            packet.ReadXORByte(guildGuid, 3);
+            packet.ReadXORByte(guildGuid, 1);
+            packet.ReadXORByte(guid, 3);
+            packet.ReadXORByte(guid, 0);
+            packet.ReadXORByte(guildGuid, 0);
+            packet.ReadXORByte(guid, 7);
+            packet.ReadXORByte(guid, 4);
+            packet.ReadXORByte(guildGuid, 5);
+            packet.ReadXORByte(guildGuid, 6);
+            packet.ReadXORByte(guid, 5);
+
+            packet.WriteGuid("Guild GUID", guildGuid);
+            packet.WriteGuid("Player GUID", guid);
+        }
+
         [Parser(Opcode.CMSG_GUILD_REMOVE, ClientVersionBuild.V4_0_6_13596, ClientVersionBuild.V4_3_4_15595)]
         public static void HandleGuildRemove406(Packet packet)
         {
