310f306e6b5a805714dccd2177cfa01778f7c6ab
QAston <qaston@gmail.com>
2012-08-17 21:29:32 +0200

    Fix a crash.

--- WowPacketParser/Enums/GuildBankRightsFlag.cs
+++ WowPacketParser/Enums/GuildBankRightsFlag.cs
@@ -9,6 +9,6 @@ namespace PacketParser.Enums
         ViewTab      = 0x01,
         WithdrawItem = 0x02,
         UpdateText   = 0x04,
-        All          = -1
+        //All          = -1
     }
 }
