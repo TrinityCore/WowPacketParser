5f929930a35ced4eb097f0154fc74c0f4bcb0867
Vincent_Michael <Vincent_Michael@gmx.de>
2013-03-09 04:02:20 +0100

    5.2 Add more opcodes

--- WowPacketParser/Enums/Version/V5_2_0_16650/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_2_0_16650/Opcodes.cs
@@ -11,6 +11,10 @@ namespace WowPacketParser.Enums.Version.V5_2_0_16650
 
         private static readonly BiDictionary<Opcode, int> Opcs = new BiDictionary<Opcode, int>
         {
+            {Opcode.CMSG_CREATURE_QUERY, 0x1C83},
+            {Opcode.CMSG_GAMEOBJECT_QUERY, 0x0118},
+            {Opcode.SMSG_CREATURE_QUERY_RESPONSE, 0x01B5},
+            {Opcode.SMSG_GAMEOBJECT_QUERY_RESPONSE, 0x1024},
             {Opcode.SMSG_UPDATE_OBJECT, 0x1406},
         };
     }
