137b9ba5ad26ab31979ff409abf45a8868e970af
Vincent_Michael <Vincent_Michael@gmx.de>
2013-06-22 13:29:43 +0200

    5.3.0 Add new opcode

--- WowPacketParser/Enums/Version/V5_3_0_16981/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_3_0_16981/Opcodes.cs
@@ -14,6 +14,7 @@ namespace WowPacketParser.Enums.Version.V5_3_0_16981
             {Opcode.CMSG_AUTH_SESSION, 0x09F1},
             {Opcode.SMSG_AUTH_CHALLENGE, 0x0221},
             {Opcode.SMSG_AUTH_RESPONSE, 0x0890},
+            {Opcode.SMSG_UPDATE_OBJECT, 0x0C65},
         };
     }
 }
