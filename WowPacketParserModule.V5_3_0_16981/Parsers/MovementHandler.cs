d58482522522dbfd98228d445cbfe7a5b41386f0
RaknarAtlantis <raknaratlantis@gmail.com>
2013-08-24 19:04:14 +0200

    Added SMSG_BINDPOINTUPDATE for 5.3.0

---
+++ WowPacketParserModule.V5_3_0_16981/Parsers/MovementHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V5_3_0_16981.Parsers
+{
+    public static class MovementHandler
+    {
+        [Parser(Opcode.SMSG_BINDPOINTUPDATE)]
+        public static void HandleBindPointUpdate(Packet packet)
+        {
+            packet.ReadSingle("Position Z");
+            packet.ReadSingle("Position Y");
+            packet.ReadEntryWithName<Int32>(StoreNameType.Map, "Map Id");
+            packet.ReadEntryWithName<Int32>(StoreNameType.Zone, "Zone Id");
+            packet.ReadSingle("Position X");
+        }
+    }
+}
