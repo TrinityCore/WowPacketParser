8a4c3b094d3fec0e6b332df6dfb04f94469bea3c
Vincent-Michael <Vincent_Michael@gmx.de>
2013-08-31 20:36:47 +0200

    Added opcode SMSG_PLAY_SOUND for 5.3.0

--- WowPacketParser/Enums/Version/V5_3_0_16981/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_3_0_16981/Opcodes.cs
@@ -273,6 +273,7 @@ namespace WowPacketParser.Enums.Version.V5_3_0_16981
             {Opcode.SMSG_PET_UNLEARN_CONFIRM, 0x0F9D},
             {Opcode.SMSG_PLAYED_TIME, 0x0995},
             {Opcode.SMSG_PLAYERBOUND, 0x12DD},
+            {Opcode.SMSG_PLAY_SOUND, 0x017C},
             {Opcode.SMSG_PLAY_SPELL_VISUAL, 0x01C1},
             {Opcode.SMSG_PONG, 0x1121},
             {Opcode.SMSG_POWER_UPDATE, 0x0DC5},
