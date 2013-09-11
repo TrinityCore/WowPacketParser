617965075fc333d2b58e2b3abcc410792ed816c3
Vincent-Michael <Vincent_Michael@gmx.de>
2013-09-11 19:02:48 +0200

    Added some opcodes for 5.4.0

--- WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
@@ -11,6 +11,9 @@ namespace WowPacketParser.Enums.Version.V5_4_0_17359
 
         private static readonly BiDictionary<Opcode, int> Opcs = new BiDictionary<Opcode, int>
         {
+            {Opcode.CMSG_AUTH_SESSION, 0x0790},
+            {Opcode.SMSG_AUTH_CHALLENGE, 0x016A},
+            {Opcode.SMSG_AUTH_RESPONSE, 0x090E},
         };
     }
 }
