140a1aa71ae8f10293d1c732ed7f62251b7c0417
zorix <zajast@o2.pl>
2013-09-11 19:08:39 +0200

    Added some opcodes Added handler for SMSG_AUTH_CHALLENGE

--- WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
@@ -11,9 +11,14 @@ namespace WowPacketParser.Enums.Version.V5_4_0_17359
 
         private static readonly BiDictionary<Opcode, int> Opcs = new BiDictionary<Opcode, int>
         {
+            {Opcode.CMSG_LOG_DISCONNECT, 0x0380},
             {Opcode.CMSG_AUTH_SESSION, 0x0790},
+            {Opcode.SMSG_ADDON_INFO, 0x0128},
             {Opcode.SMSG_AUTH_CHALLENGE, 0x016A},
             {Opcode.SMSG_AUTH_RESPONSE, 0x090E},
+            {Opcode.SMSG_CLIENTCACHE_VERSION, 0x0825},
+            {Opcode.SMSG_MOTD, 0x082A},
+            {Opcode.SMSG_TUTORIAL_FLAGS, 0x1F35},
         };
     }
 }
---
+++ WowPacketParserModule.V5_4_0_17359/Parsers/SessionHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using Guid = WowPacketParser.Misc.Guid;
+using CoreParsers = WowPacketParser.Parsing.Parsers;
+
+namespace WowPacketParserModule.V5_4_0_17359.Parsers
+{
+    public static class SessionHandler
+    {
+        [Parser(Opcode.SMSG_AUTH_CHALLENGE)]
+        public static void HandleServerAuthChallenge(Packet packet)
+        {
+            packet.ReadUInt32("Key pt1");
+            packet.ReadUInt32("Key pt2");
+            packet.ReadUInt32("Key pt3");
+            packet.ReadUInt32("Key pt4");
+            packet.ReadUInt32("Key pt5");
+            packet.ReadUInt32("Key pt6");
+            packet.ReadUInt32("Key pt7");
+            packet.ReadUInt32("Key pt8");
+            packet.ReadByte("Unk Byte");
+            packet.ReadUInt32("Server Seed");
+        }
+    }
+}
