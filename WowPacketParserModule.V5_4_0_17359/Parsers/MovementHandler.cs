1e2daa6819892215b34980d93958bd02de5e1edc
Vincent-Michael <Vincent_Michael@gmx.de>
2013-09-12 16:20:14 +0200

    Added SMSG_LOGIN_VERIFY_WORLD handler for 5.4.0

---
+++ WowPacketParserModule.V5_4_0_17359/Parsers/MovementHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V5_4_0_17359.Parsers
+{
+    public static class MovementHandler
+    {
+        [Parser(Opcode.SMSG_LOGIN_VERIFY_WORLD)]
+        public static void HandleLoginVerifyWorld(Packet packet)
+        {
+            var pos = new Vector4();
+
+            pos.Z = packet.ReadSingle();
+            packet.ReadUInt32("MapId");
+            pos.Y = packet.ReadSingle();
+            pos.X = packet.ReadSingle();
+
+            pos.O = packet.ReadSingle();
+            packet.WriteLine("Position: {0}", pos);
+        }
+    }
+}
