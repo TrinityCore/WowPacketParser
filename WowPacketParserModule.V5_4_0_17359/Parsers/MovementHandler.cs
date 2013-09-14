838b43d44e7b00dfbad2f2ba102164f52ee9f3cf
Raknar <raknaratlantis@gmail.com>
2013-09-14 11:19:09 +0200

    added opcode and handler SMSG_SET_PHASE_SHIFT

--- WowPacketParserModule.V5_4_0_17359/Parsers/MovementHandler.cs
+++ WowPacketParserModule.V5_4_0_17359/Parsers/MovementHandler.cs
@@ -35,5 +35,37 @@ namespace WowPacketParserModule.V5_4_0_17359.Parsers
 
             packet.WriteLine("Position: {0}", pos);
         }
+
+        [Parser(Opcode.SMSG_SET_PHASE_SHIFT)]
+        public static void HandlePhaseShift(Packet packet)
+        {
+            packet.ReadUInt32("UInt32 1");
+            //packet.ReadUInt32("UInt32 2");
+
+            var count = packet.ReadUInt32() / 2;
+            packet.WriteLine("Inactive Terrain swap count: {0}", count);
+            for (var i = 0; i < count; ++i)
+                packet.ReadEntryWithName<Int16>(StoreNameType.Map, "Inactive Terrain swap", i);
+
+            count = packet.ReadUInt32() / 2;
+            packet.WriteLine("Phases count: {0}", count);
+            for (var i = 0; i < count; ++i)
+                packet.ReadUInt16("Phase id", i); // Phase.dbc
+
+            count = packet.ReadUInt32() / 2;
+            packet.WriteLine("WorldMapArea swap count: {0}", count);
+            for (var i = 0; i < count; ++i)
+                packet.ReadUInt16("WorldMapArea swap", i);
+
+
+            count = packet.ReadUInt32() / 2;
+            packet.WriteLine("Active Terrain swap count: {0}", count);
+            for (var i = 0; i < count; ++i)
+                packet.ReadEntryWithName<Int16>(StoreNameType.Map, "Active Terrain swap", i);
+
+            var guid = packet.StartBitStream(0, 2, 1, 5, 3, 7, 4, 6);
+            packet.ParseBitStream(guid, 0, 5, 4, 7, 6, 2, 1, 3);
+            packet.WriteGuid("GUID", guid);
+        }
     }
 }
