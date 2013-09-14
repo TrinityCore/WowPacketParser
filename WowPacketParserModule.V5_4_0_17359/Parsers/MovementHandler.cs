0b50c7b1b0b4c8271673fb7d2203e9a79224606c
Raknar <raknaratlantis@gmail.com>
2013-09-14 11:21:51 +0200

    small typo

--- WowPacketParserModule.V5_4_0_17359/Parsers/MovementHandler.cs
+++ WowPacketParserModule.V5_4_0_17359/Parsers/MovementHandler.cs
@@ -43,9 +43,10 @@ namespace WowPacketParserModule.V5_4_0_17359.Parsers
             //packet.ReadUInt32("UInt32 2");
 
             var count = packet.ReadUInt32() / 2;
-            packet.WriteLine("Inactive Terrain swap count: {0}", count);
+            packet.WriteLine("WorldMapArea swap count: {0}", count);
             for (var i = 0; i < count; ++i)
-                packet.ReadEntryWithName<Int16>(StoreNameType.Map, "Inactive Terrain swap", i);
+                packet.ReadUInt16("WorldMapArea swap", i);
+            
 
             count = packet.ReadUInt32() / 2;
             packet.WriteLine("Phases count: {0}", count);
@@ -53,9 +54,9 @@ namespace WowPacketParserModule.V5_4_0_17359.Parsers
                 packet.ReadUInt16("Phase id", i); // Phase.dbc
 
             count = packet.ReadUInt32() / 2;
-            packet.WriteLine("WorldMapArea swap count: {0}", count);
+            packet.WriteLine("Inactive Terrain swap count: {0}", count);
             for (var i = 0; i < count; ++i)
-                packet.ReadUInt16("WorldMapArea swap", i);
+                packet.ReadEntryWithName<Int16>(StoreNameType.Map, "Inactive Terrain swap", i);
 
 
             count = packet.ReadUInt32() / 2;
