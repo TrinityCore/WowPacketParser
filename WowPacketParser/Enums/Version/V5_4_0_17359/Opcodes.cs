7dfd134752ac1a3cb034ddd829a504cbab1b5b23
Vincent-Michael <Vincent_Michael@gmx.de>
2013-09-18 20:49:08 +0200

    Added SMSG_THREAT_UPDATE opcode for 5.4.0

--- WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
@@ -78,8 +78,9 @@ namespace WowPacketParser.Enums.Version.V5_4_0_17359
             {Opcode.SMSG_SPELLNONMELEEDAMAGELOG, 0x1085},
             {Opcode.SMSG_SPELL_GO, 0x1C17},
             {Opcode.SMSG_SPELL_START, 0x10B6},
+            {Opcode.SMSG_THREAT_UPDATE, 0x000B},
             {Opcode.SMSG_TUTORIAL_FLAGS, 0x1F35},
-            {Opcode.SMSG_UPDATE_OBJECT, 0x17D9},
+            {Opcode.SMSG_UPDATE_OBJECT, 0x17D9}
         };
     }
 }
