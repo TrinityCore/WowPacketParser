683e6a3c1c379cd28c41dfcdd8544df60a2ce446
Vincent-Michael <Vincent_Michael@gmx.de>
2013-09-20 11:08:41 +0200

    Added SMSG_REDIRECT_CLIENT opcode for 5.4.0

--- WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
@@ -79,6 +79,7 @@ namespace WowPacketParser.Enums.Version.V5_4_0_17359
             {Opcode.SMSG_QUEST_QUERY_RESPONSE, 0x19AB},
             {Opcode.SMSG_REALM_QUERY_RESPONSE, 0x042B},
             {Opcode.SMSG_REALM_SPLIT, 0x0099},
+            {Opcode.SMSG_REDIRECT_CLIENT, 0x0143},
             {Opcode.SMSG_SEND_SERVER_LOCATION, 0x0883},
             {Opcode.SMSG_SET_PHASE_SHIFT, 0x0919},
             {Opcode.SMSG_SPELLHEALLOG, 0x1594},
