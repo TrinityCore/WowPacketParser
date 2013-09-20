eee201f58699202c440aad0dafde610a3176c8a0
Vincent-Michael <Vincent_Michael@gmx.de>
2013-09-20 11:26:36 +0200

    Added SMSG_SET_PROFICIENCY opcode & handler (copy and paste from 5.3.0)

--- WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
@@ -84,6 +84,7 @@ namespace WowPacketParser.Enums.Version.V5_4_0_17359
             {Opcode.SMSG_REDIRECT_CLIENT, 0x0143},
             {Opcode.SMSG_SEND_SERVER_LOCATION, 0x0883},
             {Opcode.SMSG_SET_PHASE_SHIFT, 0x0919},
+            {Opcode.SMSG_SET_PROFICIENCY, 0x012C},
             {Opcode.SMSG_SPELLHEALLOG, 0x1594},
             {Opcode.SMSG_SPELLNONMELEEDAMAGELOG, 0x1085},
             {Opcode.SMSG_SPELL_GO, 0x1C17},
---
+++ WowPacketParserModule.V5_4_0_17359/Parsers/ItemHandler.cs
@@ -0,0 +1,17 @@
+using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V5_4_0_17359.Parsers
+{
+    public static class ItemHandler
+    {
+        [Parser(Opcode.SMSG_SET_PROFICIENCY)]
+        public static void HandleSetProficency(Packet packet)
+        {
+            packet.ReadEnum<UnknownFlags>("Mask", TypeCode.UInt32);
+            packet.ReadEnum<ItemClass>("Class", TypeCode.Byte);
+        }
+    }
+}
