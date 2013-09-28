14501b3c4968e4638602e40bc218514a4724bd31
Vincent-Michael <Vincent_Michael@gmx.de>
2013-09-28 20:10:41 +0200

    Added SMSG_GROUP_LIST handler for 5.4.0

---
+++ WowPacketParserModule.V5_4_0_17359/Parsers/GroupHandler.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V5_4_0_17359.Parsers
+{
+    public static class GroupHandler
+    {
+        [Parser(Opcode.SMSG_GROUP_LIST)]
+        public static void HandleGroupList(Packet packet)
+        {
+            var guid1 = new byte[8];
+            var guid2 = new byte[8];
+            var guid3 = new byte[8];
+
+            guid1[1] = packet.ReadBit();
+            guid2[7] = packet.ReadBit();
+
+            var memberCount = packet.ReadBits("Member Count", 21);
+            
+            var bitsED = new uint[memberCount];
+            var guid4 = new byte[memberCount][];
+            for (var i = 0; i < memberCount; i++)
+            {
+                guid4[i] = new byte[8];
+                bitsED[i] = packet.ReadBits(6);
+                packet.StartBitStream(guid4[i], 3, 0, 4, 7, 6, 1, 5, 2);
+            }
+
+            guid1[2] = packet.ReadBit();
+            guid2[0] = packet.ReadBit();
+
+            var bit2C = packet.ReadBit();
+            var bit80 = packet.ReadBit();
+
+            if (bit80)
+                packet.StartBitStream(guid3, 2, 0, 3, 7, 4, 1, 6, 5);
+
+            packet.StartBitStream(guid2, 2, 1, 6);
+            guid1[7] = packet.ReadBit();
+            guid2[4] = packet.ReadBit();
+            packet.StartBitStream(guid1, 4, 3);
+            guid2[3] = packet.ReadBit();
+
+            if (bit2C)
+            {
+                packet.ReadBit("bit2B");
+                packet.ReadBit("bit20");
+            }
+
+            guid1[6] = packet.ReadBit();
+            var bit38 = packet.ReadBit();
+            packet.StartBitStream(guid1, 5, 0);
+            guid2[5] = packet.ReadBit();
+
+            if (bit2C)
+            {
+                packet.ReadInt32("Int18");
+                packet.ReadByte("Byte2A");
+                packet.ReadSingle("Float24");
+                packet.ReadByte("Byte28");
+                packet.ReadEnum<InstanceStatus>("Group Type Status", TypeCode.Byte);
+                packet.ReadByte("Byte21");
+                packet.ReadByte("Byte29");
+                packet.ReadInt32("Int1C");
+            }
+            
+            for (var i = 0; i < memberCount; i++)
+            {
+                packet.ReadXORByte(guid4[i], 5);
+                packet.ReadEnum<GroupUpdateFlag>("Update Flags", TypeCode.Byte, i);
+                packet.ReadEnum<LfgRoleFlag>("Role", TypeCode.Byte, i);
+                packet.ReadXORByte(guid4[i], 3);
+                packet.ReadXORByte(guid4[i], 6);
+                packet.ReadByte("Sub Group", i);
+                packet.ReadWoWString("Name", bitsED[i], i);
+                packet.ReadXORByte(guid4[i], 0);
+                packet.ReadXORByte(guid4[i], 2);
+                packet.ReadEnum<GroupMemberStatusFlag>("Status", TypeCode.Byte, i);
+                packet.ReadXORByte(guid4[i], 7);
+                packet.ReadXORByte(guid4[i], 1);
+                packet.ReadXORByte(guid4[i], 4);
+
+                packet.WriteGuid("Member GUID", guid4[i], i);
+            }
+
+            packet.ReadInt32("Counter");
+
+            if (bit80)
+            {
+                packet.ReadXORByte(guid3, 0);
+                packet.ReadXORByte(guid3, 1);
+                packet.ReadXORByte(guid3, 3);
+                packet.ReadXORByte(guid3, 7);
+                packet.ReadXORByte(guid3, 6);
+                packet.ReadXORByte(guid3, 2);
+                packet.ReadByte("Byte78");
+                packet.ReadXORByte(guid3, 5);
+                packet.ReadEnum<ItemQuality>("Loot Threshold", TypeCode.Byte);
+                packet.ReadXORByte(guid3, 4);
+                packet.WriteGuid("Looter GUID", guid3);
+            }
+
+            packet.ReadXORByte(guid2, 3);
+            packet.ReadEnum<MapDifficulty>("Dungeon Difficulty", TypeCode.Byte);
+            packet.ReadXORByte(guid1, 7);
+
+            if (bit38)
+            {
+                packet.ReadInt32("Int34");
+                packet.ReadInt32("Int30");
+            }
+
+            packet.ReadXORByte(guid2, 0);
+            packet.ReadXORByte(guid2, 6);
+            packet.ReadXORByte(guid1, 3);
+
+            packet.ReadEnum<LootMethod>("Loot Method", TypeCode.Byte);
+
+            packet.ReadXORByte(guid1, 1);
+            packet.ReadXORByte(guid1, 0);
+            packet.ReadXORByte(guid1, 2);
+            packet.ReadXORByte(guid1, 4);
+            packet.ReadXORByte(guid1, 6);
+
+            packet.ReadEnum<GroupTypeFlag>("Group Type", TypeCode.Byte);
+            packet.ReadInt32("Int48");
+
+            packet.ReadXORByte(guid2, 2);
+            packet.ReadXORByte(guid2, 1);
+            packet.ReadXORByte(guid2, 4);
+            packet.ReadXORByte(guid2, 7);
+            packet.ReadXORByte(guid2, 5);
+            packet.ReadXORByte(guid1, 5);
+
+            packet.WriteGuid("Leader GUID", guid1);
+            packet.WriteGuid("Group GUID", guid2);
+
+        }
+    }
+}
