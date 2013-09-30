d99f127bb865023d2c1c0cd9918eb1184aeba721
Vincent-Michael <Vincent_Michael@gmx.de>
2013-09-30 23:28:45 +0200

    Added CMSG_DUNGEON_FINDER_GET_SYSTEM_INFO handler for 5.4.0 (thx to Zori)

---
+++ WowPacketParserModule.V5_4_0_17359/Parsers/LfgHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V5_4_0_17359.Parsers
+{
+    public static class LfgHandler
+    {
+        [Parser(Opcode.CMSG_DUNGEON_FINDER_GET_SYSTEM_INFO)]
+        public static void HandleDungeonFinderGetSystemInfo(Packet packet)
+        {
+            packet.ReadByte("Unk Byte");
+            packet.ReadBit("Unk boolean");
+        }
+    }
+}
