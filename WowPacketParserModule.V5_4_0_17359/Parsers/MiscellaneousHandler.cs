2bb8b35e50b06d4003e03e40e8e16a3a1731a1df
Vincent-Michael <Vincent_Michael@gmx.de>
2013-10-01 06:55:25 +0200

    Added SMSG_UNKNOWN_1166 opcode and handler for 5.4.0

--- WowPacketParserModule.V5_4_0_17359/Parsers/MiscellaneousHandler.cs
+++ WowPacketParserModule.V5_4_0_17359/Parsers/MiscellaneousHandler.cs
@@ -643,5 +643,26 @@ namespace WowPacketParserModule.V5_4_0_17359.Parsers
 
             packet.WriteGuid("Guid", guid);
         }
+
+        [Parser(Opcode.SMSG_UNKNOWN_1166)]
+        public static void HandleUnknown1166(Packet packet)
+        {
+            var guid = new byte[8];
+
+            packet.StartBitStream(guid, 2, 1, 7, 0, 5, 3, 4, 6);
+            packet.ReadByte("Byte1D");
+            packet.ReadXORByte(guid, 7);
+            packet.ReadXORByte(guid, 4);
+            packet.ReadXORByte(guid, 5);
+            packet.ReadByte("Byte1C");
+            packet.ReadInt32("Int18"); // spellId?
+            packet.ReadXORByte(guid, 3);
+            packet.ReadXORByte(guid, 6);
+            packet.ReadXORByte(guid, 0);
+            packet.ReadXORByte(guid, 1);
+            packet.ReadXORByte(guid, 2);
+
+            packet.WriteGuid("Guid", guid);
+        }
     }
 }
