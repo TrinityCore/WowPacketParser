b18558a882c3e5745fccdb4afc65c8d9379c5703
Vincent-Michael <Vincent_Michael@gmx.de>
2013-10-04 06:45:48 +0200

    Added SMSG_TOTEM_CREATED opcode and handler for 5.4.0

--- WowPacketParser/Enums/Opcode.cs
+++ WowPacketParser/Enums/Opcode.cs
@@ -1847,6 +1847,7 @@ namespace WowPacketParser.Enums
         SMSG_UNKNOWN_2178,
         SMSG_UNKNOWN_2205,
         SMSG_UNKNOWN_2235,
+        SMSG_UNKNOWN_2308,
         SMSG_UNKNOWN_2321,
         SMSG_UNKNOWN_2327,
         SMSG_UNKNOWN_2338,
--- WowPacketParserModule.V5_4_0_17359/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V5_4_0_17359/Parsers/SpellHandler.cs
@@ -1294,5 +1294,26 @@ namespace WowPacketParserModule.V5_4_0_17359.Parsers
             packet.WriteGuid("Guid6", guid6);
             packet.WriteGuid("Guid3", guid3);
         }
+
+        [Parser(Opcode.SMSG_TOTEM_CREATED)]
+        public static void HandleTotemCreated(Packet packet)
+        {
+            var guid = new byte[8];
+
+            packet.StartBitStream(guid, 2, 6, 4, 0, 7, 3, 5, 1);
+            packet.ReadUInt32("Duration");
+            packet.ReadXORByte(guid, 1);
+            packet.ReadXORByte(guid, 3);
+            packet.ReadEntryWithName<UInt32>(StoreNameType.Spell, "Spell Id");
+            packet.ReadXORByte(guid, 7);
+            packet.ReadXORByte(guid, 4);
+            packet.ReadXORByte(guid, 0);
+            packet.ReadByte("Slot");
+            packet.ReadXORByte(guid, 6);
+            packet.ReadXORByte(guid, 2);
+            packet.ReadXORByte(guid, 5);
+
+            packet.WriteGuid("Guid", guid);
+        }
     }
 }
