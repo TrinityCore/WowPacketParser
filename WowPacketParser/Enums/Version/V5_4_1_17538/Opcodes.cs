f6576dd9b8a54d43c493ed7e23269c724b373531
Vincent_Michael <Vincent_Michael@gmx.de>
2013-10-31 17:06:03 +0100

    Add some opcodes for 5.4.1

--- WowPacketParser/Enums/Version/V5_4_1_17538/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_1_17538/Opcodes.cs
@@ -11,7 +11,14 @@ namespace WowPacketParser.Enums.Version.V5_4_1_17538
 
         private static readonly BiDictionary<Opcode, int> Opcs = new BiDictionary<Opcode, int>
         {
-            {Opcode.SMSG_UPDATE_OBJECT, 0x0C22},
+            {Opcode.CMSG_CREATURE_QUERY, 0x1647},
+            {Opcode.CMSG_DB_QUERY_BULK, 0x01E4},
+            {Opcode.CMSG_GAMEOBJECT_QUERY, 0x1677},
+            {Opcode.SMSG_GOSSIP_MESSAGE, 0x03FC},
+            {Opcode.SMSG_CREATURE_QUERY_RESPONSE, 0x011C},
+            {Opcode.SMSG_DB_REPLY, 0x1406},
+            {Opcode.SMSG_GAMEOBJECT_QUERY_RESPONSE, 0x0916},
+            {Opcode.SMSG_UPDATE_OBJECT, 0x0C22}
         };
     }
 }
