c385b0a564a920339a9b8a0d01a53068b9d05ff6
Raknar <raknaratlantis@gmail.com>
2013-11-04 10:52:24 +0100

    add SMSG_AUTH_CHALLENGE, SMSG_MOTD parser wont spam 260 mb anymore

--- WowPacketParserModule.V5_4_1_17538/Parsers/SessionHandler.cs
+++ WowPacketParserModule.V5_4_1_17538/Parsers/SessionHandler.cs
@@ -10,6 +10,7 @@ namespace WowPacketParserModule.V5_4_1_17538.Parsers
 {
     public static class SessionHandler
     {
+
         [Parser(Opcode.CMSG_PLAYER_LOGIN)]
         public static void HandlePlayerLogin(Packet packet)
         {
@@ -18,6 +19,34 @@ namespace WowPacketParserModule.V5_4_1_17538.Parsers
             packet.ParseBitStream(guid, 7, 6, 0, 1, 4, 3, 2, 5);
             CoreParsers.SessionHandler.LoginGuid = new Guid(BitConverter.ToUInt64(guid, 0));
             packet.WriteGuid("Guid", guid);
+        }
+
+        [Parser(Opcode.SMSG_AUTH_CHALLENGE)]
+        public static void HandleServerAuthChallenge(Packet packet)
+        {
+            packet.ReadByte("Unk Byte");
+            packet.ReadUInt32("Key pt1");
+            packet.ReadUInt32("Key pt2");
+            packet.ReadUInt32("Key pt3");
+            packet.ReadUInt32("Key pt4");
+            packet.ReadUInt32("Key pt5");
+            packet.ReadUInt32("Key pt6");
+            packet.ReadUInt32("Key pt7");
+            packet.ReadUInt32("Key pt8");
+            packet.ReadUInt32("Server Seed");
+        }
+
+        [Parser(Opcode.SMSG_MOTD)]
+        public static void HandleMessageOfTheDay(Packet packet)
+        {
+            var lineCount = packet.ReadBits("Line Count", 4);
+            var lineLength = new int[lineCount];
+
+            for (var i = 0; i < lineCount; i++)
+                lineLength[i] = (int)packet.ReadBits(7);
+
+            for (var i = 0; i < lineCount; i++)
+                packet.ReadWoWString("Line", lineLength[i], i);
         }
      }
 }
