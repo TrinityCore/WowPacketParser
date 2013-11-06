3c3e5b9a79546a5486ee91555755e0e5a002a8b4
Raknar <raknaratlantis@gmail.com>
2013-11-06 13:24:09 +0100

    handle for CMSG_QUERY_PLAYER_NAME

--- WowPacketParserModule.V5_4_1_17538/Parsers/QueryHandler.cs
+++ WowPacketParserModule.V5_4_1_17538/Parsers/QueryHandler.cs
@@ -12,5 +12,31 @@ namespace WowPacketParserModule.V5_4_1_17359.Parsers
         {
             packet.ReadInt32("Entry");
         }
+
+        [Parser(Opcode.CMSG_QUERY_PLAYER_NAME)]
+        public static void HandlePlayerQueryName(Packet packet)
+        {
+            var guid = new byte[8];
+
+            guid[1] = packet.ReadBit();
+            guid[3] = packet.ReadBit();
+            guid[6] = packet.ReadBit();
+            guid[7] = packet.ReadBit();
+            guid[2] = packet.ReadBit();
+            guid[5] = packet.ReadBit();
+            var bit20 = packet.ReadBit("bit20");
+            guid[0] = packet.ReadBit();
+            var bit28 = packet.ReadBit("bit28");
+            guid[4] = packet.ReadBit();
+            
+            packet.ParseBitStream(guid, 4, 6, 7, 1, 2, 5, 0, 3);
+
+            if (bit20)
+                packet.ReadUInt32("unk20");
+
+            if (bit28)
+                packet.ReadUInt32("unk28");
+            packet.WriteGuid("Guid", guid);
+        }
     }
 }
