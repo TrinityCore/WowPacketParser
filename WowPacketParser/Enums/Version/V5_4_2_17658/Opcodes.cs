247824c87c6db108d603a69cbf5bc0cc1489cce5
AriDEV <aridev666@gmail.com>
2013-12-11 20:08:42 +0100

    Added Opcodes and UpdateFields i forgot to add last commit
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ WowPacketParser/Enums/Version/V5_4_2_17658/Opcodes.cs
@@ -0,0 +1,16 @@
+using WowPacketParser.Misc;
+
+namespace WowPacketParser.Enums.Version.V5_4_2_17658
+{
+    public static class Opcodes_5_4_2
+    {
+        public static BiDictionary<Opcode, int> Opcodes()
+        {
+            return Opcs;
+        }
+
+        private static readonly BiDictionary<Opcode, int> Opcs = new BiDictionary<Opcode, int>
+        {
+        };
+    }
+}
