da72051ed7c376700aa74580a1964b45427415cd
Vincent_Michael <Vincent_Michael@gmx.de>
2013-12-15 01:19:17 +0100

    Added SMSG_CREATURE_QUERY_RESPONSE handler for 5.4.2

--- WowPacketParserModule.V5_4_2_17658/Parsers/QueryHandler.cs
+++ WowPacketParserModule.V5_4_2_17658/Parsers/QueryHandler.cs
@@ -16,6 +16,95 @@ namespace WowPacketParserModule.V5_4_2_17658.Parsers
             packet.ReadInt32("Entry");
         }
 
+        [HasSniffData]
+        [Parser(Opcode.SMSG_CREATURE_QUERY_RESPONSE)]
+        public static void HandleCreatureQueryResponse(Packet packet)
+        {
+            var creature = new UnitTemplate();
+            var hasData = packet.ReadBit();
+            if (!hasData)
+                return; // nothing to do
+
+            creature.RacialLeader = packet.ReadBit("Racial Leader");
+
+            var bits2C = packet.ReadBits(6);
+            var bits24 = packet.ReadBits(11);
+            var qItemCount = packet.ReadBits(22);
+
+            var stringLens = new int[4][];
+            for (var i = 0; i < 4; i++)
+            {
+                stringLens[i] = new int[2];
+                stringLens[i][1] = (int)packet.ReadBits(11);
+                stringLens[i][0] = (int)packet.ReadBits(11);
+            }
+
+            var bits1C = (int)packet.ReadBits(11);
+
+            creature.DisplayIds = new uint[4];
+            creature.KillCredits = new uint[2];
+
+            creature.DisplayIds[1] = packet.ReadUInt32("Display ID 1");
+            creature.KillCredits[1] = packet.ReadUInt32("Kill Credit 2");
+            creature.Type = packet.ReadEnum<CreatureType>("Type", TypeCode.Int32);
+
+            var name = new string[4];
+            for (var i = 0; i < 4; ++i)
+            {
+                if (stringLens[i][0] > 1)
+                    name[i] = packet.ReadCString("Name", i);
+                if (stringLens[i][1] > 1)
+                    packet.ReadCString("Female Name", i);
+            }
+            creature.Name = name[0];
+
+            creature.Modifier2 = packet.ReadSingle("Modifier 2");
+
+            creature.TypeFlags = packet.ReadEnum<CreatureTypeFlag>("Type Flags", TypeCode.UInt32);
+            creature.TypeFlags2 = packet.ReadUInt32("Creature Type Flags 2"); // Missing enum
+
+            creature.Family = packet.ReadEnum<CreatureFamily>("Family", TypeCode.Int32);
+            creature.KillCredits[0] = packet.ReadUInt32("Kill Credit 1");
+            creature.DisplayIds[3] = packet.ReadUInt32("Display ID 3");
+
+            creature.QuestItems = new uint[qItemCount];
+            for (var i = 0; i < qItemCount; ++i)
+                creature.QuestItems[i] = (uint)packet.ReadEntryWithName<Int32>(StoreNameType.Item, "Quest Item", i);
+
+            creature.Modifier1 = packet.ReadSingle("Modifier 1");
+
+
+            if (bits24 > 1)
+                packet.ReadCString("String1C");
+
+            creature.MovementId = packet.ReadUInt32("Movement ID");
+            creature.Expansion = packet.ReadEnum<ClientType>("Expansion", TypeCode.UInt32);
+
+            if (bits2C > 1)
+                creature.IconName = packet.ReadCString("Icon Name");
+
+            creature.DisplayIds[2] = packet.ReadUInt32("Display ID 2");
+            creature.DisplayIds[0] = packet.ReadUInt32("Display ID 0");
+            creature.Rank = packet.ReadEnum<CreatureRank>("Rank", TypeCode.Int32);
+
+            if (bits1C > 1)
+                creature.SubName = packet.ReadCString("Sub Name");
+
+            var entry = packet.ReadEntry("Entry");
+
+            packet.AddSniffData(StoreNameType.Unit, entry.Key, "QUERY_RESPONSE");
+
+            Storage.UnitTemplates.Add((uint)entry.Key, creature, packet.TimeSpan);
+
+            var objectName = new ObjectName
+            {
+                ObjectType = ObjectType.Unit,
+                Name = creature.Name,
+            };
+            Storage.ObjectNames.Add((uint)entry.Key, objectName, packet.TimeSpan);
+
+        }
+
         [Parser(Opcode.CMSG_DB_QUERY_BULK)]
         public static void HandleDBQueryBulk(Packet packet)
         {
