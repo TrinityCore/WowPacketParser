f6bdce5af6c58d890d548cc82a852edde95b5cba
Vincent_Michael <Vincent_Michael@gmx.de>
2014-02-23 02:47:20 +0100

    Added more opcodes and struc for 5.4.7

--- WowPacketParserModule.V5_4_7_17898/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V5_4_7_17898/Parsers/SpellHandler.cs
@@ -31,5 +31,30 @@ namespace WowPacketParser.V5_4_7_17898.Parsers
             for (var i = 0; i < count; ++i)
                 packet.ReadEntryWithName<UInt32>(StoreNameType.Spell, "Spell ID", i);
         }
+
+        [Parser(Opcode.SMSG_INITIAL_SPELLS)]
+        public static void HandleInitialSpells(Packet packet)
+        {
+            var count = packet.ReadBits("Spell Count", 22);
+            packet.ReadBit("Unk Bit");
+
+            var spells = new List<uint>((int)count);
+            for (var i = 0; i < count; i++)
+            {
+                var spellId = packet.ReadEntryWithName<UInt32>(StoreNameType.Spell, "Spell ID", i);
+                spells.Add((uint)spellId);
+            }
+
+            var startSpell = new StartSpell { Spells = spells };
+
+            WoWObject character;
+            if (Storage.Objects.TryGetValue(CoreParsers.SessionHandler.LoginGuid, out character))
+            {
+                var player = character as Player;
+                if (player != null && player.FirstLogin)
+                    Storage.StartSpells.Add(new Tuple<Race, Class>(player.Race, player.Class), startSpell, packet.TimeSpan);
+            }
+        }
+
     }
 }
