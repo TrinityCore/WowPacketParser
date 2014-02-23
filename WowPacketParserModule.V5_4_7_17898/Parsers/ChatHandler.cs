e4dd3e75df5763e2d3914dee0091c8825c38558b
Raknar <raknaratlantis@gmail.com>
2014-02-23 14:00:19 +0100

    add missing things

---
+++ WowPacketParserModule.V5_4_7_17898/Parsers/ChatHandler.cs
@@ -0,0 +1,30 @@
+using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+using Guid = WowPacketParser.Misc.Guid;
+using ChatMessageType540 = WowPacketParserModule.V5_4_0_17359.Enums.ChatMessageType;
+
+namespace WowPacketParserModule.V5_4_7_17898.Parsers
+{
+    public static class ChatHandler
+    {
+        [Parser(Opcode.CMSG_MESSAGECHAT_GUILD)]
+        [Parser(Opcode.CMSG_MESSAGECHAT_INSTANCE)]
+        [Parser(Opcode.CMSG_MESSAGECHAT_OFFICER)]
+        [Parser(Opcode.CMSG_MESSAGECHAT_PARTY)]
+        [Parser(Opcode.CMSG_MESSAGECHAT_RAID)]
+        [Parser(Opcode.CMSG_MESSAGECHAT_RAID_WARNING)]
+        [Parser(Opcode.CMSG_MESSAGECHAT_SAY)]
+        [Parser(Opcode.CMSG_MESSAGECHAT_YELL)]
+        public static void HandleClientChatMessage(Packet packet)
+        {
+            packet.ReadEnum<Language>("Language", TypeCode.Int32);
+            var len = packet.ReadBits(8);
+            packet.ReadWoWString("Message", len);
+        }
+
+    }
+}
