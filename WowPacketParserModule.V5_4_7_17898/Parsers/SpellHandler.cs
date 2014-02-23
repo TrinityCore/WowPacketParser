f31cbb0339c32eaf1d53ac48252a48bb17677414
Vincent_Michael <Vincent_Michael@gmx.de>
2014-02-23 20:53:58 +0100

    Added SMSG_TALENTS_INFO struc for 5.4.7

--- WowPacketParserModule.V5_4_7_17898/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V5_4_7_17898/Parsers/SpellHandler.cs
@@ -97,5 +97,28 @@ namespace WowPacketParser.V5_4_7_17898.Parsers
                 packet.ReadEnum<SpellModOp>("Spell Mod", TypeCode.Byte, j);
             }
         }
+
+        [Parser(Opcode.SMSG_TALENTS_INFO)]
+        public static void ReadTalentInfo(Packet packet)
+        {
+            packet.ReadByte("Active Spec Group");
+            var specCount = packet.ReadBits("Spec Group count", 19);
+
+            var spentTalents = new uint[specCount];
+
+            for (var i = 0; i < specCount; ++i)
+                spentTalents[i] = packet.ReadBits("Spec Talent Count", 23, i);
+
+            for (var i = 0; i < specCount; ++i)
+            {
+                for (var j = 0; j < 6; ++j)
+                    packet.ReadUInt16("Glyph", i, j);
+
+                packet.ReadUInt32("Spec Id", i);
+
+                for (var j = 0; j < spentTalents[i]; ++j)
+                    packet.ReadUInt16("Talent Id", i, j);
+            }
+        }
     }
 }
