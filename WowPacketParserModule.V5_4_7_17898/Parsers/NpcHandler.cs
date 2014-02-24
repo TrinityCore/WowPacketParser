0902aa01595da01260afe3490aedb47da26bf7d8
Raknar <raknaratlantis@gmail.com>
2014-02-24 21:11:52 +0100

    handlers for CMSG_GOSSIP_HELLO, CMSG_GOSSIP_SELECT_OPTION

--- WowPacketParserModule.V5_4_7_17898/Parsers/NpcHandler.cs
+++ WowPacketParserModule.V5_4_7_17898/Parsers/NpcHandler.cs
@@ -13,6 +13,48 @@ namespace WowPacketParserModule.V5_4_7_17898.Parsers
 {
     public static class NpcHandler
     {
+        [Parser(Opcode.CMSG_GOSSIP_HELLO)]
+        public static void HandleGossipHello(Packet packet)
+        {
+            var guid = new byte[8];
+            packet.StartBitStream(guid, 6, 3, 4, 5, 1, 7, 2, 0);
+            packet.ParseBitStream(guid, 6, 0, 1, 7, 2, 5, 4, 3);
+
+            packet.WriteGuid("GUID", guid);
+        }
+
+        [Parser(Opcode.CMSG_GOSSIP_SELECT_OPTION)]
+        public static void HandleNpcGossipSelectOption(Packet packet)
+        {
+            var guid = new byte[8];
+
+            var menuEntry = packet.ReadUInt32("Menu Id");
+            var gossipId = packet.ReadUInt32("Gossip Id");
+
+            guid[2] = packet.ReadBit();
+            guid[4] = packet.ReadBit();
+            guid[7] = packet.ReadBit();
+            guid[1] = packet.ReadBit();
+            guid[5] = packet.ReadBit();
+            guid[6] = packet.ReadBit();
+            guid[0] = packet.ReadBit();
+            guid[3] = packet.ReadBit();
+            var bits8 = packet.ReadBits(8);
+
+            packet.ReadXORByte(guid, 5);
+            packet.ReadXORByte(guid, 6);
+            packet.ReadXORByte(guid, 7);
+            packet.ReadXORByte(guid, 3);
+            packet.ReadWoWString("Box Text", bits8);
+            packet.ReadXORByte(guid, 0);
+            packet.ReadXORByte(guid, 2);
+            packet.ReadXORByte(guid, 1);
+            packet.ReadXORByte(guid, 4);
+
+            Storage.GossipSelects.Add(Tuple.Create(menuEntry, gossipId), null, packet.TimeSpan);
+            packet.WriteGuid("GUID", guid);
+        }
+
         [HasSniffData]
         [Parser(Opcode.SMSG_NPC_TEXT_UPDATE)]
         public static void HandleNpcTextUpdate(Packet packet)
