41edda5b4b6a2a9259312d93350f264f78ccf9cd
Vincent_Michael <Vincent_Michael@gmx.de>
2014-02-27 18:43:44 +0100

    Added SMSG_DESTROY_OBJECT / CMSG_OBJECT_UPDATE_FAILED struc for 5.4.7

---
+++ WowPacketParserModule.V5_4_7_17898/Parsers/UpdateHandler.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+using CoreParsers = WowPacketParser.Parsing.Parsers;
+using Guid = WowPacketParser.Misc.Guid;
+using UpdateFields = WowPacketParser.Enums.Version.UpdateFields;
+
+namespace WowPacketParserModule.V5_4_7_17898.Parsers
+{
+    public static class UpdateHandler
+    {
+        [Parser(Opcode.SMSG_DESTROY_OBJECT)]
+        public static void HandleDestroyObject(Packet packet)
+        {
+            var guid = new byte[8];
+
+            guid[0] = packet.ReadBit();
+            guid[2] = packet.ReadBit();
+            guid[3] = packet.ReadBit();
+            guid[1] = packet.ReadBit();
+            guid[5] = packet.ReadBit();
+            packet.ReadBit("Despawn Animation");
+            guid[6] = packet.ReadBit();
+            guid[7] = packet.ReadBit();
+            guid[4] = packet.ReadBit();
+
+            packet.ParseBitStream(guid, 2, 1, 5, 4, 3, 6, 7, 0);
+
+            packet.WriteGuid("GUID", guid);
+        }
+
+        [Parser(Opcode.CMSG_OBJECT_UPDATE_FAILED)]
+        public static void HandleObjectUpdateFailed(Packet packet)
+        {
+            var guid = packet.StartBitStream(4, 6, 3, 0, 7, 5, 1, 2);
+            packet.ParseBitStream(guid, 4, 7, 0, 6, 5, 2, 1, 3);
+
+            packet.WriteGuid("Guid", guid);
+        }
+    }
+}
