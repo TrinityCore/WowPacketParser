9f0fdb89fa03c4d10fa66afd875cb3fcc1c94991
Raknar <raknaratlantis@gmail.com>
2014-03-01 15:06:03 +0100

    Add challenge mode handler

---
+++ WowPacketParserModule.V5_4_7_17898/Parsers/ChallengeModeHandler.cs
@@ -0,0 +1,14 @@
+using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+using Guid = WowPacketParser.Misc.Guid;
+
+namespace WowPacketParserModule.V5_4_7_17898.Parsers
+{
+    public static class ChallengeModeHandler
+    {
+	}
+}
