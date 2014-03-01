aaeb9eebe9b56a7ddcc3a98a8031f492bf5bba74
Vincent_Michael <Vincent_Michael@gmx.de>
2014-03-01 23:03:12 +0100

    Added SMSG_ACCOUNT_DATA_TIMES struc for 5.4.7

---
+++ WowPacketParserModule.V5_4_7_17898/Parsers/AccountDataHandler.cs
@@ -0,0 +1,21 @@
+using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V5_4_7_17898.Parsers
+{
+    public static class AccountDataHandler
+    {
+        [Parser(Opcode.SMSG_ACCOUNT_DATA_TIMES)]
+        public static void HandleAccountDataTimes(Packet packet)
+        {
+            packet.ReadUInt32("unk24");
+            packet.ReadTime("Server Time");
+
+            for (var i = 0; i < 8; ++i)
+                packet.ReadTime("[" + (AccountDataType)i + "]" + " Time");
+            packet.ReadBit("Unk Bit");
+        }
+    }
+}
