5865763460ea8c96cc843ec6a5804c858803c9c2
Vincent_Michael <Vincent_Michael@gmx.de>
2014-03-13 18:24:22 +0100

    Added SMSG_MAIL_LIST_RESULT struc for 5.4.7

--- WowPacketParserModule.V5_4_7_17898/Parsers/MailHandler.cs
+++ WowPacketParserModule.V5_4_7_17898/Parsers/MailHandler.cs
@@ -74,5 +74,112 @@ namespace WowPacketParserModule.V5_4_0_17359.Parsers
 
             packet.WriteGuid("GUID", guid);
         }
+
+        [Parser(Opcode.SMSG_MAIL_LIST_RESULT)]
+        public static void HandleMailListResult(Packet packet)
+        {
+            var count = packet.ReadBits("Count", 18);
+
+            var guid = new byte[count][];
+
+            var bit1C = new bool[count];
+            var bit24 = new bool[count];
+            var bit10 = new bool[count];
+            var bit2C = new bool[count];
+
+            var subjectLength = new uint[count];
+            var itemCount = new uint[count];
+            var bodyLength = new uint[count];
+            var bit84 = new uint[count][];
+
+            for (var i = 0; i < count; ++i)
+            {
+                bit2C[i] = packet.ReadBit();
+                subjectLength[i] = packet.ReadBits(8);
+                bit1C[i] = packet.ReadBit();
+                bit10[i] = packet.ReadBit();
+                bit24[i] = packet.ReadBit();
+
+                if (bit10[i])
+                {
+                    guid[i] = new byte[8];
+                    packet.StartBitStream(guid[i], 6, 0, 3, 2, 5, 7, 1, 4);
+                }
+
+                bodyLength[i] = packet.ReadBits(13);
+                itemCount[i] = packet.ReadBits(17);
+
+                bit84[i] = new uint[itemCount[i]];
+
+                for (var j = 0; j < itemCount[i]; ++j)
+                    bit84[i][j] = packet.ReadBit();
+            }
+
+            for (var i = 0; i < count; ++i)
+            {
+
+                for (var j = 0; j < itemCount[i]; ++j)
+                {
+                    packet.WriteLine("[{0}][{1}] bit84: {2}", i, j, bit84[i][j]);
+                    packet.ReadInt32("IntED", i, j);
+                    packet.ReadEntryWithName<UInt32>(StoreNameType.Item, "Item Id", i, j);
+                    packet.ReadInt32("IntED", i, j);
+                    packet.ReadInt32("Int14", i, j);
+                    packet.ReadByte("ByteED", i, j);
+
+                    for (var k = 0; k < 8; ++k)
+                    {
+                        packet.ReadInt32("Int14", i, j, k);
+                        packet.ReadInt32("Int14", i, j, k);
+                        packet.ReadInt32("Int14", i, j, k);
+                    }
+
+                    packet.ReadInt32("IntED", i, j);                    
+                    packet.ReadInt32("IntED", i, j);
+                    packet.ReadInt32("IntED", i, j);
+
+                    var len = packet.ReadInt32();
+
+                    packet.ReadBytes(len);
+
+                    packet.ReadInt32("IntED", i, j);
+                }
+
+                packet.ReadInt64("Int30", i);
+
+                if (bit1C[i])
+                    packet.ReadInt32("IntED", i);
+
+                packet.ReadInt32("IntED", i);
+
+                if (bit10[i])
+                {
+                    packet.ParseBitStream(guid[i], 7, 0, 5, 4, 3, 2, 3, 1);
+                    packet.WriteGuid("Guid", guid[i]);
+                }
+
+                packet.ReadByte("ByteED", i);
+
+                packet.ReadWoWString("Subject", subjectLength[i], i);
+
+                packet.ReadInt32("IntED", i);
+                packet.ReadInt32("IntED", i);
+                packet.ReadInt32("IntED", i);
+
+                if (bit24[i])
+                    packet.ReadInt32("IntED", i);
+
+                packet.ReadSingle("Time", i);
+                packet.ReadWoWString("Body", bodyLength[i], i);
+                packet.ReadInt32("Int14", i);
+
+                if (bit2C[i])
+                    packet.ReadInt32("IntED", i);
+
+                packet.ReadInt64("Int40", i);
+            }
+
+            packet.ReadUInt32("Total Mails");
+        }
     }
 }
