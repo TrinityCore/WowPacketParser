0901307347c4920e991bd813bea1280aa88a776d
AriDEV <aridev666@gmail.com>
2014-05-25 15:45:19 +0200

    Added Parsing of CMSG_AUCTION_HELLO & CMSG_GOSSIP_HELLO
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- WowPacketParser/Enums/Version/V5_4_8_18291/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_8_18291/Opcodes.cs
@@ -11,14 +11,17 @@ namespace WowPacketParser.Enums.Version.V5_4_8_18291
 
         private static readonly BiDictionary<Opcode, int> Opcs = new BiDictionary<Opcode, int>
         {
+            {Opcode.CMSG_AUCTION_HELLO, 0x0379}, // 18291
             {Opcode.CMSG_AUTH_SESSION, 0x00B2}, // 18291
+            {Opcode.CMSG_GOSSIP_HELLO, 0x12F3}, // 18291
+            {Opcode.CMSG_LIST_INVENTORY, 0x02D8}, // 18291
 			
             {Opcode.SMSG_AUTH_CHALLENGE, 0x0949}, // 18291
             {Opcode.SMSG_AUTH_RESPONSE, 0x0ABA}, // 18291
             {Opcode.SMSG_CREATURE_QUERY_RESPONSE, 0x048B}, // 18291
             {Opcode.SMSG_INITIAL_SPELLS, 0x045A}, // 18291
             {Opcode.SMSG_LOGIN_VERIFY_WORLD, 0x1C0F}, // 18291
-            {Opcode.SMSG_UPDATE_OBJECT, 0x1C0F}, // 18291
+            {Opcode.SMSG_UPDATE_OBJECT, 0x1792}, // 18291
 			
         };
     }
--- WowPacketParserModule.V5_4_8_18291/Parsers/NpcHandler.cs
+++ WowPacketParserModule.V5_4_8_18291/Parsers/NpcHandler.cs
@@ -13,5 +13,22 @@ namespace WowPacketParserModule.V5_4_8_18291.Parsers
 {
     public static class NpcHandler
     {
+        [Parser(Opcode.CMSG_GOSSIP_HELLO)]
+        public static void HandleGossipHello(Packet packet)
+        {
+            var GUID = new byte[8];
+            GUID = packet.StartBitStream(2, 4, 0, 3, 6, 7, 5, 1);
+            packet.ParseBitStream(GUID, 4, 7, 1, 0, 5, 3, 6, 2);
+            packet.WriteGuid("GUID", GUID);
+        }
+		
+		[Parser(Opcode.CMSG_AUCTION_HELLO)]
+        public static void HandleGossipHello(Packet packet)
+        {
+            var GUID = new byte[8];
+            GUID = packet.StartBitStream(1, 5, 2, 0, 3, 6, 4, 7);
+            packet.ParseBitStream(GUID, 2, 7, 1, 3, 5, 0, 4, 6);
+            packet.WriteGuid("GUID", GUID);
+        }
     }
 }
