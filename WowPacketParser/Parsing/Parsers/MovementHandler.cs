d321267c3437b0778c08bf88ade8f302faa7744b
Vincent_Michael <Vincent_Michael@gmx.de>
2014-06-22 03:19:05 +0200

    Added SMSG_SET_PHASE_SHIFT for 5.0.5

--- WowPacketParser/Parsing/Parsers/MovementHandler.cs
+++ WowPacketParser/Parsing/Parsers/MovementHandler.cs
@@ -1547,6 +1547,37 @@ namespace WowPacketParser.Parsing.Parsers
             }
         }
 
+        [Parser(Opcode.SMSG_SET_PHASE_SHIFT, ClientVersionBuild.V5_0_5_16048, ClientVersionBuild.V5_1_0_16309)]
+        public static void HandlePhaseShift505(Packet packet)
+        {
+            var count = packet.ReadUInt32() / 2;
+            packet.WriteLine("Inactive Terrain swap count: {0}", count);
+            for (var i = 0; i < count; ++i)
+                packet.ReadEntryWithName<Int16>(StoreNameType.Map, "Inactive Terrain swap", i);
+
+            packet.ReadUInt32("UInt32");
+
+            count = packet.ReadUInt32() / 2;
+            packet.WriteLine("Active Terrain swap count: {0}", count);
+            for (var i = 0; i < count; ++i)
+                packet.ReadEntryWithName<Int16>(StoreNameType.Map, "Active Terrain swap", i);
+
+            count = packet.ReadUInt32() / 2;
+            packet.WriteLine("Phases count: {0}", count);
+            for (var i = 0; i < count; ++i)
+                packet.ReadUInt16("Phase id", i); // Phase.dbc
+
+            count = packet.ReadUInt32() / 2;
+            packet.WriteLine("WorldMapArea swap count: {0}", count);
+            for (var i = 0; i < count; ++i)
+                packet.ReadUInt16("WorldMapArea swap", i);
+
+            var guid = packet.StartBitStream(3, 7, 1, 6, 0, 4, 5, 2);
+            packet.ParseBitStream(guid, 4, 3, 0, 6, 2, 7, 5, 1);
+
+            packet.WriteLine("GUID {0}", new Guid(BitConverter.ToUInt64(guid, 0)));
+        }
+
         [Parser(Opcode.SMSG_SET_PHASE_SHIFT, ClientVersionBuild.V5_1_0_16309)]
         public static void HandlePhaseShift510(Packet packet)
         {
