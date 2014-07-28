bfa135407c88295af53eb168fedb655f2384159c
Vincent_Michael <Vincent_Michael@gmx.de>
2014-07-28 19:33:43 +0200

    Added CMSG_SET_DUNGEON_DIFFICULTY struc for 5.4.8

--- WowPacketParserModule.V5_4_8_18291/Parsers/InstanceHandler.cs
+++ WowPacketParserModule.V5_4_8_18291/Parsers/InstanceHandler.cs
@@ -1,16 +1,18 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using WowPacketParser.Enums;
 using WowPacketParser.Misc;
 using WowPacketParser.Parsing;
-using WowPacketParser.Store;
-using WowPacketParser.Store.Objects;
 
 namespace WowPacketParserModule.V5_4_8_18291.Parsers
 {
     public static class InstanceHandler
     {
-
+        [Parser(Opcode.CMSG_SET_DUNGEON_DIFFICULTY)]
+        [Parser(Opcode.CMSG_SET_RAID_DIFFICULTY)]
+        [Parser(Opcode.SMSG_SET_RAID_DIFFICULTY)]
+        public static void HandleSetDifficulty(Packet packet)
+        {
+            packet.ReadEnum<MapDifficulty>("Difficulty", TypeCode.Int32);
+        }
     }
 }
