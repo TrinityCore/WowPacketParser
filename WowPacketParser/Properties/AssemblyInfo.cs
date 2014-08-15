c6042ba15b4f8872fcc8d5a26def3d2f96b01e35
DDuarte <dnpd.dd@gmail.com>
2014-08-15 19:46:11 +0100

    Target .NET 4.5 instead of 4.0

--- WowPacketParser/Properties/AssemblyInfo.cs
+++ WowPacketParser/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TrinityCore")]
 [assembly: AssemblyProduct("WowPacketParser")]
-[assembly: AssemblyCopyright("Copyright ©  2010-2013")]
+[assembly: AssemblyCopyright("Copyright ©  2010-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
