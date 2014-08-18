7fb4293f2db296751a6007eeece9378eac84ad6b
Vincent_Michael <Vincent_Michael@gmx.de>
2014-08-18 17:42:00 +0200

    Added missing file in b6c910c7f4e76f51800233fade65f55d55628664

---
+++ WowPacketParser/Store/Objects/VehicleTemplateAccessory.cs
@@ -0,0 +1,15 @@
+﻿﻿using WowPacketParser.SQL;
+
+namespace WowPacketParser.Store.Objects
+{
+    [DBTableName("vehicle_template_accessory")]
+    public class VehicleTemplateAccessory
+    {
+        
+        [DBFieldName("accessory_entry")]
+        public uint accessoryEntry;
+
+        [DBFieldName("seat_id")]
+        public int seatId;
+    }
+}
