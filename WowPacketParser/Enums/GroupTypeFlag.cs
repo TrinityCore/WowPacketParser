db88399a95b5a95edbcaef15a537431203383b86
DDuarte <dnpd.dd@gmail.com>
2014-09-11 17:19:57 +0100

    Update enum GroupTypeFlag

--- WowPacketParser/Enums/GroupTypeFlag.cs
+++ WowPacketParser/Enums/GroupTypeFlag.cs
@@ -5,12 +5,13 @@ namespace WowPacketParser.Enums
     [Flags]
     public enum GroupTypeFlag : byte
     {
-        Normal            = 0x00,
-        Battleground      = 0x01,
-        Raid              = 0x02,
-        Unk4              = 0x04,
-        LookingForDungeon = 0x08,
-        Unk10             = 0x10,
-        Unk40             = 0x40
+        Normal              = 0x00,
+        Battleground        = 0x01,
+        Raid                = 0x02,
+        LFGRestricted       = 0x04,
+        LookingForDungeon   = 0x08,
+        OnePersonParty      = 0x10,
+        Unk20               = 0x20,
+        IsEveryoneAssistant = 0x40
     }
 }
