0784adc6972ecd735b6315855cc7500894cc3bdb
Duarte Duarte <dnpd.dd@gmail.com>
2014-09-12 02:51:43 +0100

    Update GroupTypeFlag.cs

--- WowPacketParser/Enums/GroupTypeFlag.cs
+++ WowPacketParser/Enums/GroupTypeFlag.cs
@@ -10,8 +10,8 @@ namespace WowPacketParser.Enums
         Raid                = 0x02,
         LFGRestricted       = 0x04,
         LookingForDungeon   = 0x08,
-        OnePersonParty      = 0x10,
-        Unk20               = 0x20,
+        Unk10               = 0x10,
+        OnePersonParty      = 0x20,
         IsEveryoneAssistant = 0x40
     }
 }
