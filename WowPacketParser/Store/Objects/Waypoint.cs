bcee943d0dc283241c6785cc6979743b209cd230
Vincent_Michael <Vincent_Michael@gmx.de>
2014-09-20 17:46:43 +0200

    Added missing file

---
+++ WowPacketParser/Store/Objects/Waypoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using WowPacketParser.Enums;
+using WowPacketParser.SQL;
+using WowPacketParser.Misc;
+
+namespace WowPacketParser.Store.Objects
+{
+    public class Waypoint
+    {
+        public ICollection<WaypointData> WaypointData;
+        public ICollection<WaypointData> SplineWaypointData;
+
+        public SplineFlag434 SplineFlags;
+        public MovementFlag MovementFlags;
+    }
+
+    [DBTableName("waypoint_data")]
+    public class WaypointData
+    {
+        [DBFieldName("PointId")]
+        public uint PointId;
+
+        //[DBFieldName("Position")]
+        public Vector3 Position;
+
+        [DBFieldName("Time")]
+        public string Time;
+    }
+}
