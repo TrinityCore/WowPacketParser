e8c9e12f542cadeb31e51c50dff263e9db883121
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-17 20:01:12 +0200

    Added opcode CMSG_NAME_QUERY / SMSG_MESSAGECHAT / SMSG_NAME_QUERY_RESPONSE for 6.0.2

--- WowPacketParser/Enums/Version/V6_0_2_19033/Opcodes.cs
+++ WowPacketParser/Enums/Version/V6_0_2_19033/Opcodes.cs
@@ -17,10 +17,13 @@ namespace WowPacketParser.Enums.Version.V6_0_2_19033
             {Opcode.SMSG_AUTH_CHALLENGE, 0x10AA},
             {Opcode.SMSG_AUTH_RESPONSE, 0x0564},
             {Opcode.SMSG_CHAR_ENUM, 0x1154},
+            {Opcode.CMSG_NAME_QUERY, 0x0BA4},
             {Opcode.SMSG_CLIENTCACHE_VERSION, 0x10EF},
             {Opcode.SMSG_CREATURE_QUERY_RESPONSE, 0x0203},
             {Opcode.SMSG_DB_REPLY, 0x1574},
             {Opcode.SMSG_GAMEOBJECT_QUERY_RESPONSE, 0x08E3},
+            {Opcode.SMSG_MESSAGECHAT, 0x0E09},
+            {Opcode.SMSG_NAME_QUERY_RESPONSE, 0x1667},
             {Opcode.SMSG_REDIRECT_CLIENT, 0x1082},
             {Opcode.SMSG_UPDATE_OBJECT, 0x03EF}
         };
