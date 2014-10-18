ee8a96401ac1f7901a8634ff0d1b782f06acf741
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-18 20:55:54 +0200

    Added more opcodes for 6.0.2

--- WowPacketParser/Enums/Version/V5_4_8_18291/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_8_18291/Opcodes.cs
@@ -528,7 +528,7 @@ namespace WowPacketParser.Enums.Version.V5_4_8_18291
             {Opcode.SMSG_UPDATE_WORLD_STATE, 0x121B},
             {Opcode.SMSG_VOID_STORAGE_CONTENTS, 0x008B},
             {Opcode.SMSG_VOID_TRANSFER_RESULT, 0x1C9E},
-            {Opcode.SMSG_WARDEN_DATA, 0x14EB},
+            {Opcode.SMSG_WARDEN_DATA, 0x0C0A},
             {Opcode.SMSG_WEATHER, 0x06AB},
             {Opcode.SMSG_WHO, 0x161B},
             {Opcode.SMSG_WORLD_SERVER_INFO, 0x0082},
--- WowPacketParser/Enums/Version/V6_0_2_19033/Opcodes.cs
+++ WowPacketParser/Enums/Version/V6_0_2_19033/Opcodes.cs
@@ -13,8 +13,12 @@ namespace WowPacketParser.Enums.Version.V6_0_2_19033
         {
             {Opcode.CMSG_AUTH_SESSION, 0x1B05},
             {Opcode.CMSG_NAME_QUERY, 0x0BA4},
+            {Opcode.CMSG_PING, 0x1B75},
+            {Opcode.CMSG_REDIRECT_AUTH_PROOF, 0x1806},
+            {Opcode.CMSG_WARDEN_DATA, 0x00F3},
 
             {Opcode.SMSG_ADDON_INFO, 0x1400},
+            {Opcode.SMSG_ALL_ACHIEVEMENT_DATA_PLAYER, 0x01A4},
             {Opcode.SMSG_AUTH_CHALLENGE, 0x10AA},
             {Opcode.SMSG_AUTH_RESPONSE, 0x0564},
             {Opcode.SMSG_CHAR_ENUM, 0x1154},
@@ -22,12 +26,17 @@ namespace WowPacketParser.Enums.Version.V6_0_2_19033
             {Opcode.SMSG_CREATURE_QUERY_RESPONSE, 0x0203},
             {Opcode.SMSG_DB_REPLY, 0x1574},
             {Opcode.SMSG_GAMEOBJECT_QUERY_RESPONSE, 0x08E3},
+            {Opcode.SMSG_GUILD_QUERY_RESPONSE, 0x034A},
             {Opcode.SMSG_MESSAGECHAT, 0x0E09},
             {Opcode.SMSG_MONSTER_MOVE, 0x019C},
             {Opcode.SMSG_NAME_QUERY_RESPONSE, 0x1667},
             {Opcode.SMSG_PLAYER_MOVE, 0x128B},
+            {Opcode.SMSG_PONG, 0x1881},
             {Opcode.SMSG_REDIRECT_CLIENT, 0x1082},
-            {Opcode.SMSG_UPDATE_OBJECT, 0x03EF}
+            {Opcode.SMSG_SEND_SERVER_LOCATION, 0x1257},
+            {Opcode.SMSG_TUTORIAL_FLAGS, 0x0617},
+            {Opcode.SMSG_UPDATE_OBJECT, 0x03EF},
+            {Opcode.SMSG_WARDEN_DATA, 0x12EF},
         };
     }
 }
