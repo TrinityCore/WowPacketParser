76ec60e45afea6a54563ed9c87957501a7d345eb
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-19 23:51:26 +0200

    Added SMSG_LOGIN_VERIFY_WORLD structure for 6.0.2

---
+++ WowPacketParserModule.V6_0_2_19033/Parsers/MovementHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Enums.Version;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using CoreParsers = WowPacketParser.Parsing.Parsers;
+
+namespace WowPacketParserModule.V6_0_2_19033.Parsers
+{
+    public static class MovementHandler
+    {
+        [Parser(Opcode.SMSG_LOGIN_VERIFY_WORLD)]
+        public static void HandleLoginVerifyWorld(Packet packet)
+        {
+            var pos = new Vector4();
+
+            CoreParsers.MovementHandler.CurrentMapId = (uint)packet.ReadEntry<Int32>(StoreNameType.Map, "Map");
+            pos.X = packet.ReadSingle();
+            pos.Y = packet.ReadSingle();
+            pos.Z = packet.ReadSingle();
+            pos.O = packet.ReadSingle();
+            packet.ReadUInt32("Unk");
+
+            packet.AddValue("Position", pos);
+            packet.AddSniffData(StoreNameType.Map, (int)CoreParsers.MovementHandler.CurrentMapId, "NEW_WORLD");
+        }
+    }
+}
