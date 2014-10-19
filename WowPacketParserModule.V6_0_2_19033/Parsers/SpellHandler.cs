27ce4c143e9663a059a34e6e99cc14a20ab3badb
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-19 20:30:58 +0200

    Added SMSG_INITIAL_SPELLS structure for 6.0.2

--- WowPacketParserModule.V6_0_2_19033/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/SpellHandler.cs
@@ -20,5 +20,29 @@ namespace WowPacketParser.V6_0_2_19033.Parsers
                 packet.ReadEntry<Int32>(StoreNameType.Spell, "Spell ID", i);
             packet.ReadBit("Unk Bits");
         }
+
+        [Parser(Opcode.SMSG_INITIAL_SPELLS)]
+        public static void HandleInitialSpells(Packet packet)
+        {
+            packet.ReadBit("Unk Bit");
+            var count = packet.ReadUInt32("Spell Count");
+
+            var spells = new List<uint>((int)count);
+            for (var i = 0; i < count; i++)
+            {
+                var spellId = packet.ReadEntry<UInt32>(StoreNameType.Spell, "Spell ID", i);
+                spells.Add((uint)spellId);
+            }
+
+            var startSpell = new StartSpell { Spells = spells };
+
+            WoWObject character;
+            if (Storage.Objects.TryGetValue(CoreParsers.SessionHandler.LoginGuid, out character))
+            {
+                var player = character as Player;
+                if (player != null && player.FirstLogin)
+                    Storage.StartSpells.Add(new Tuple<Race, Class>(player.Race, player.Class), startSpell, packet.TimeSpan);
+            }
+        }
     }
 }
