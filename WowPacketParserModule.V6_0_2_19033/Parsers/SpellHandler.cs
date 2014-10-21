7039c66690918a4ee24e19eb51b0e5986fd79673
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-21 19:45:09 +0200

    Added SMSG_AURA_UPDATE structure for 6.0.2

--- WowPacketParserModule.V6_0_2_19033/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/SpellHandler.cs
@@ -102,5 +102,82 @@ namespace WowPacketParser.V6_0_2_19033.Parsers
                 }
             }
         }
+
+        [HasSniffData]
+        [Parser(Opcode.SMSG_AURA_UPDATE)]
+        public static void HandleAuraUpdate(Packet packet)
+        {
+            packet.ReadBit("bit16");
+            packet.ReadPackedGuid128("Guid");
+            var count = packet.ReadUInt32("AuraCount");
+            
+            var auras = new List<Aura>();
+            for (var i = 0; i < count; ++i)
+            {
+                var aura = new Aura();
+
+                packet.ReadByte("Slot", i);
+
+                packet.ResetBitReader();
+                var hasAura = packet.ReadBit("HasAura");
+                if (hasAura)
+                {
+                    var id = packet.ReadEntry<Int32>(StoreNameType.Spell, "Spell ID", i);
+                    aura.AuraFlags = packet.ReadEnum<AuraFlagMoP>("Flags", TypeCode.Byte, i);
+                    packet.ReadInt32("Effect Mask", i);
+                    packet.ResetBitReader();
+                    aura.Level = packet.ReadUInt16("Caster Level", i);
+                    aura.Charges = packet.ReadByte("Charges", i);
+
+                    var int72 = packet.ReadUInt32("Int56 Count");
+                    var effectCount = packet.ReadUInt32("Effect Count");
+
+                    for (var j = 0; j < int72; ++j)
+                        packet.ReadSingle("Float15", i, j);
+
+                    for (var j = 0; j < effectCount; ++j)
+                        packet.ReadSingle("Effect Value", i, j);
+
+                    var hasCasterGUID = packet.ReadBit("hasCasterGUID");
+                    var hasDuration = packet.ReadBit("hasDuration");
+                    var hasMaxDuration = packet.ReadBit("hasMaxDuration");
+
+                    if (hasCasterGUID)
+                        packet.ReadPackedGuid128("Caster Guid");
+
+
+                    if (hasDuration)
+                        aura.Duration = packet.ReadInt32("Duration", i);
+                    else
+                        aura.Duration = 0;
+
+                    if (hasMaxDuration)
+                        aura.MaxDuration = packet.ReadInt32("Max Duration", i);
+                    else
+                        aura.MaxDuration = 0;
+
+                    auras.Add(aura);
+                    packet.AddSniffData(StoreNameType.Spell, (int)aura.SpellId, "AURA_UPDATE");
+                }
+            }
+            // To-Do: Fix me
+            /*
+            if (Storage.Objects.ContainsKey(GUID))
+            {
+                var unit = Storage.Objects[GUID].Item1 as Unit;
+                if (unit != null)
+                {
+                    // If this is the first packet that sends auras
+                    // (hopefully at spawn time) add it to the "Auras" field,
+                    // if not create another row of auras in AddedAuras
+                    // (similar to ChangedUpdateFields)
+
+                    if (unit.Auras == null)
+                        unit.Auras = auras;
+                    else
+                        unit.AddedAuras.Add(auras);
+                }
+            }*/
+        }
     }
 }
