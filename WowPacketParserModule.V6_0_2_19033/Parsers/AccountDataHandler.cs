1c166b900820205266d10caee371eb885b86e006
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-21 21:38:59 +0200

    meh, (i should always files select)

---
+++ WowPacketParserModule.V6_0_2_19033/Parsers/AccountDataHandler.cs
@@ -0,0 +1,21 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V6_0_2_19033.Parsers
+{
+    public static class AccountDataHandler
+    {
+        [Parser(Opcode.SMSG_ACCOUNT_DATA_TIMES)]
+        public static void HandleAccountDataTimes(Packet packet)
+        {
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadTime("Server Time");
+
+            for (var i = 0; i < 8; ++i)
+            {
+                packet.ReadTime("[" + (AccountDataType)i + "]" + " Time");
+            }
+        }
+    }
+}
