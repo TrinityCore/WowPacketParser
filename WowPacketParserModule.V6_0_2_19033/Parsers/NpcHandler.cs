ba70dd775af6d39ae5c6475a2dd1b5b615ef0670
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-21 06:20:38 +0200

    Added missing file for 6.0.2

---
+++ WowPacketParserModule.V6_0_2_19033/Parsers/NpcHandler.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V6_0_2_19033.Parsers
+{
+    public static class NpcHandler
+    {
+        public static uint LastGossipPOIEntry = 0;
+
+        [Parser(Opcode.CMSG_GOSSIP_HELLO)]
+        [Parser(Opcode.CMSG_LIST_INVENTORY)]
+        [Parser(Opcode.SMSG_SHOW_BANK)]
+        public static void HandleNpcHello(Packet packet)
+        {
+            packet.ReadPackedGuid128("Guid");
+        }
+
+        [Parser(Opcode.CMSG_NPC_TEXT_QUERY)]
+        public static void HandleNpcTextQuery(Packet packet)
+        {
+            packet.ReadInt32("Entry");
+            packet.ReadPackedGuid128("Guid");
+        }
+
+        [HasSniffData]
+        [Parser(Opcode.SMSG_NPC_TEXT_UPDATE)]
+        public static void HandleNpcTextUpdate(Packet packet)
+        {
+            var npcText = new NpcTextMop();
+
+            var entry = packet.ReadEntry("Entry");
+            if (entry.Value) // Can be masked
+                return;
+
+            var hasData = packet.ReadBit();
+            if (!hasData)
+                return; // nothing to do
+
+            var size = packet.ReadInt32("Size");
+            var data = packet.ReadBytes(size);
+
+            var pkt = new Packet(data, packet.Opcode, packet.Time, packet.Direction, packet.Number, packet.Writer, packet.FileName);
+            npcText.Probabilities = new float[8];
+            npcText.BroadcastTextId = new uint[8];
+            for (var i = 0; i < 8; ++i)
+                npcText.Probabilities[i] = pkt.ReadSingle("Probability", i);
+            for (var i = 0; i < 8; ++i)
+                npcText.BroadcastTextId[i] = pkt.ReadUInt32("Broadcast Text Id", i);
+
+            pkt.ClosePacket(false);
+
+            packet.AddSniffData(StoreNameType.NpcText, entry.Key, "QUERY_RESPONSE");
+
+            Storage.NpcTextsMop.Add((uint)entry.Key, npcText, packet.TimeSpan);
+        }
+    }
+}
