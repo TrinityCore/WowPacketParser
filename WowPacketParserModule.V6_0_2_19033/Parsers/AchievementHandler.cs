b14694629e9a905e6e0799e86ac40a4c22684440
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-21 22:35:45 +0200

    Added SMSG_CRITERIA_UPDATE_PLAYER / SMSG_CRITERIA_UPDATE_ACCOUNT structure for 6.0.2

---
+++ WowPacketParserModule.V6_0_2_19033/Parsers/AchievementHandler.cs
@@ -0,0 +1,34 @@
+using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V6_0_2_19033.Parsers
+{
+    public static class AchievementHandler
+    {
+        [Parser(Opcode.SMSG_CRITERIA_UPDATE_PLAYER)]
+        public static void HandleCriteriaPlayer(Packet packet)
+        {
+            packet.ReadInt32("Criteria ID");
+            packet.ReadInt64("Counter");
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadInt32("Flags");
+            packet.ReadPackedTime("Time");
+            packet.ReadTime("TimeFromStart");
+            packet.ReadTime("TimeFromCreate");
+        }
+
+        [Parser(Opcode.SMSG_CRITERIA_UPDATE_ACCOUNT)]
+        public static void HandleCriteriaUpdateAccount(Packet packet)
+        {
+            packet.ReadInt32("Criteria ID");
+            packet.ReadInt64("Counter");
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadPackedTime("Time");
+            packet.ReadTime("TimeFromStart");
+            packet.ReadTime("TimeFromCreate");
+            packet.ReadBits("Flags", 4); // some flag... & 1 -> delete
+        }
+    }
+}
