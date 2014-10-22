3bc1925d3daa11fe6d07b0cbf79fdaee414a8e13
Subv <subv2112@gmail.com>
2014-10-22 11:39:24 -0500

    Corrected the structure of SMSG_AUCTION_COMMAND_RESULT

--- WowPacketParserModule.V6_0_2_19033/Parsers/AuctionHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/AuctionHandler.cs
@@ -23,15 +23,15 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
         [Parser(Opcode.SMSG_AUCTION_COMMAND_RESULT)]
         public static void HandleAuctionCommandResult(Packet packet)
         {
-            packet.ReadUInt32("Unk");
-            packet.ReadEnum<InventoryResult>("BagResult", TypeCode.UInt32);
-            packet.ReadUInt32("Unk");
-            packet.ReadUInt32("Unk");
+            packet.ReadUInt32("AuctionItemID");
+            packet.ReadEnum<AuctionHouseAction>("Command", TypeCode.UInt32);
+            packet.ReadEnum<AuctionHouseError>("ErrorCode", TypeCode.UInt32);
+            packet.ReadUInt32("BagResult");
             packet.ReadPackedGuid128("Guid");
 
             // One of the following is MinIncrement and the other is Money, order still unknown
-            packet.ReadUInt64("Unk");
-            packet.ReadUInt64("Unk");
+            packet.ReadUInt64("MinIncrement");
+            packet.ReadUInt64("Money");
         }
     }
 }
