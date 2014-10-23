0d05c2a8a2f09a8deec891a8ffe122047b9a36ed
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-23 06:10:32 +0200

    Added SMSG_GUILD_QUERY_RESPONSE structure for 6.0.2

--- WowPacketParserModule.V6_0_2_19033/Parsers/GuildHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/GuildHandler.cs
@@ -13,6 +13,39 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             packet.ReadPackedGuid128("Player Guid");
         }
 
+        [Parser(Opcode.SMSG_GUILD_QUERY_RESPONSE)]
+        public static void HandleGuildQueryResponse(Packet packet)
+        {
+            packet.ReadPackedGuid128("Guild Guid");
+
+            var hasData = packet.ReadBit();
+            if (hasData)
+            {
+                packet.ReadPackedGuid128("Guild Guid");
+                packet.ReadInt32("VirtualRealmAddress");
+                var rankCount = packet.ReadInt32("RankCount");
+                packet.ReadInt32("EmblemStyle");
+                packet.ReadInt32("EmblemColor");
+                packet.ReadInt32("BorderStyle");
+                packet.ReadInt32("BorderColor");
+                packet.ReadInt32("BackgroundColor");
+
+                for (var i = 0; i < rankCount; i++)
+                {
+                    packet.ReadInt32("RankID", i);
+                    packet.ReadInt32("RankOrder", i);
+
+                    packet.ResetBitReader();
+                    var rankNameLen = packet.ReadBits(7);
+                    packet.ReadWoWString("Rank Name", rankNameLen, i);
+                }
+
+                packet.ResetBitReader();
+                var nameLen = packet.ReadBits(7);
+                packet.ReadWoWString("Guild Name", nameLen);
+            }
+        }
+
         [Parser(Opcode.SMSG_GUILD_MOTD)]
         public static void HandleNewText(Packet packet)
         {
