b4e8d0c28e2f8218078820c4bf4cf303d95f3f88
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-25 00:56:57 +0200

    Added SMSG_CHANNEL_NOTIFY_JOINED opcode and structure for 6.0.2

--- WowPacketParserModule.V6_0_2_19033/Parsers/ChannelHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/ChannelHandler.cs
@@ -22,5 +22,19 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             packet.ReadWoWString("ChannelName", channelLength);
             packet.ReadWoWString("Password", passwordLength);
         }
+
+        [Parser(Opcode.SMSG_CHANNEL_NOTIFY_JOINED)]
+        public static void HandleChannelNotify(Packet packet)
+        {
+            var bits544 = packet.ReadBits(7);
+            var bits24 = packet.ReadBits(10);
+
+            packet.ReadByte("ChannelFlags");
+            packet.ReadInt32("ChatChannelID");
+            packet.ReadInt64("InstanceID");
+
+            packet.ReadWoWString("Channel", bits544);
+            packet.ReadWoWString("ChannelWelcomeMsg", bits24);
+        }
     }
 }
