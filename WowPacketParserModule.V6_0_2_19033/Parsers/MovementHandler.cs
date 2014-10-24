6b9972ea3fcd93a799e2188bbf7c5ba81058fc06
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-24 19:42:45 +0200

    Added

--- WowPacketParserModule.V6_0_2_19033/Parsers/MovementHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/MovementHandler.cs
@@ -87,5 +87,113 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
                 }
             }
         }
+
+        [Parser(Opcode.SMSG_MONSTER_MOVE)]
+        public static void HandleMonsterMove(Packet packet)
+        {
+            packet.ReadPackedGuid128("MoverGUID");
+
+            var pos = packet.ReadVector3("Position");
+            packet.ReadUInt32("Id");
+            packet.ReadVector3("Destination");
+
+            packet.ReadEnum<SplineFlag434>("Spline Flags", TypeCode.Int32);
+            packet.ReadByte("AnimTier");
+            packet.ReadUInt32("TierTransStartTime");
+            packet.ReadUInt32("Elapsed");
+            packet.ReadUInt32("MoveTime");
+            packet.ReadSingle("JumpGravity");
+            packet.ReadUInt32("SpecialTime");
+            var points = packet.ReadInt32("Waypoints");
+
+            packet.ReadByte("Mode");
+            packet.ReadByte("VehicleExitVoluntary");
+
+            packet.ReadPackedGuid128("TransportGUID");
+            packet.ReadByte("VehicleSeat");
+
+            var packedDeltas = packet.ReadInt32();
+
+            Vector3 endpos = new Vector3();
+            for (int i = 0; i < points; i++)
+            {
+                var spot = packet.ReadVector3();
+
+                // client always taking first point
+                if (i == 0)
+                    endpos = spot;
+
+                packet.AddValue("Waypoint", spot, i);
+            }
+
+            var waypoints = new Vector3[packedDeltas];
+            for (int i = 0; i < packedDeltas; i++)
+            {
+                var vec = packet.ReadPackedVector3();
+                waypoints[i].X = vec.X;
+                waypoints[i].Y = vec.Y;
+                waypoints[i].Z = vec.Z;
+            }
+
+
+            var type = packet.ReadBits("Face", 2);
+            var monsterSplineFilter = packet.ReadBit("Has MonsterSplineFilter");
+
+            switch (type)
+            {
+                case 2:
+                {
+                   packet.ReadVector3("FaceSpot");
+                   break;
+                }
+                case 3:
+                {
+                    packet.ReadPackedGuid128("Facing GUID");
+                    break;
+                }
+                case 4:
+                {
+                    packet.ReadSingle("FaceDirection");
+                    break;
+                }
+            }
+
+            if (monsterSplineFilter)
+            {
+                var count = packet.ReadUInt32("MonsterSplineFilterKey");
+                packet.ReadSingle("BaseSpeed");
+                packet.ReadInt16("StartOffset");
+                packet.ReadSingle("DistToPrevFilterKey");
+
+                for (int i = 0; i < count; i++)
+                {
+                    packet.ReadInt16("IDx", i);
+                    packet.ReadInt16("Speed", i);
+                }
+
+                packet.ReadInt16("AddedToStart");
+                packet.ReadBits("FilterFlags", 2);
+            }
+
+            packet.ResetBitReader();
+            packet.ReadBit("CrzTeleport");
+            packet.ReadBits("Unk Bit", 2);
+
+            // Calculate mid pos
+            var mid = new Vector3();
+            mid.X = (pos.X + endpos.X) * 0.5f;
+            mid.Y = (pos.Y + endpos.Y) * 0.5f;
+            mid.Z = (pos.Z + endpos.Z) * 0.5f;
+            for (var i = 0; i < packedDeltas; ++i)
+            {
+                var vec = new Vector3
+                {
+                    X = mid.X - waypoints[i].X,
+                    Y = mid.Y - waypoints[i].Y,
+                    Z = mid.Z - waypoints[i].Z,
+                };
+                packet.AddValue("Waypoint", vec, i);
+            }
+        }
     }
 }
