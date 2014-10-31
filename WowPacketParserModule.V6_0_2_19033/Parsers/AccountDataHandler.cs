13b848f1daa5f44b2e3224f7ad37b0667aa11da8
Vincent_Michael <Vincent_Michael@gmx.de>
2014-10-31 04:51:31 +0100

    Added CMSG_UPDATE_ACCOUNT_DATA opcode and structure for 6.0.2/6.0.3

--- WowPacketParserModule.V6_0_2_19033/Parsers/AccountDataHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/AccountDataHandler.cs
@@ -17,5 +17,26 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
                 packet.ReadTime("[" + (AccountDataType)i + "]" + " Time");
             }
         }
+
+        [Parser(Opcode.CMSG_UPDATE_ACCOUNT_DATA)]
+        public static void HandleClientUpdateAccountData(Packet packet)
+        {
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadTime("Time");
+
+            // Size
+            var decompCount = packet.ReadInt32();
+            var compCount = packet.ReadInt32();
+
+            packet.ResetBitReader();
+
+            packet.ReadEnum<AccountDataType>("Data Type", 3);
+
+            var pkt = packet.Inflate(compCount, decompCount, false);
+
+            var data = pkt.ReadWoWString(decompCount);
+
+            packet.AddValue("CompressedData", data);
+        }
     }
 }
