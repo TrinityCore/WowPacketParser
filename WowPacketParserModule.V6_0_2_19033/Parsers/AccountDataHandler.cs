40aaa96786370a2dd9cfc20a488fa515d3fefc75
Vincent_Michael <Vincent_Michael@gmx.de>
2014-11-04 00:40:31 +0100

    Sync with trinitycore and addet structure for CMSG_REQUEST_ACCOUNT_DATA

--- WowPacketParserModule.V6_0_2_19033/Parsers/AccountDataHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/AccountDataHandler.cs
@@ -1,4 +1,5 @@
-﻿using WowPacketParser.Enums;
+﻿using System;
+using WowPacketParser.Enums;
 using WowPacketParser.Misc;
 using WowPacketParser.Parsing;
 
@@ -34,6 +35,13 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             var data = pkt.ReadWoWString(decompCount);
 
             packet.AddValue("CompressedData", data);
+        }
+
+        [Parser(Opcode.CMSG_REQUEST_ACCOUNT_DATA)]
+        public static void HandleRequestAccountData(Packet packet)
+        {
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadEnum<AccountDataType>("Data Type", 3);
         }
 
         [Parser(Opcode.SMSG_UPDATE_ACCOUNT_DATA)]
