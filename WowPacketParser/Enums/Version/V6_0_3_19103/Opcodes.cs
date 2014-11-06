53a3046eea5706194077b6ae9a6e58c0f52f30a6
Vincent_Michael <Vincent_Michael@gmx.de>
2014-11-07 00:01:34 +0100

    Added CMSG_BATTLE_PAY_GET_PRODUCT_LIST_QUERY opcode for 6.0.3

--- WowPacketParser/Enums/Version/V6_0_3_19103/Opcodes.cs
+++ WowPacketParser/Enums/Version/V6_0_3_19103/Opcodes.cs
@@ -19,6 +19,7 @@ namespace WowPacketParser.Enums.Version.V6_0_3_19103
             {Opcode.CMSG_AUTH_SESSION, 0x0487},
             {Opcode.CMSG_AREATRIGGER, 0x01B4},
             {Opcode.CMSG_BANKER_ACTIVATE, 0x1B24},
+            {Opcode.CMSG_BATTLE_PAY_GET_PRODUCT_LIST_QUERY, 0x1389},
             {Opcode.CMSG_CANCEL_TRADE, 0x1159},
             {Opcode.CMSG_CAST_SPELL, 0x08FE},
             {Opcode.CMSG_CHAR_CREATE, 0x070D},
