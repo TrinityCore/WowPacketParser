84d2d79615993144671eb25d4d574eec814e1d94
Vincent_Michael <Vincent_Michael@gmx.de>
2014-11-08 05:53:00 +0100

    Added SMSG_BATTLEFIELD_RATED_INFO opcode and structure for 6.0.3

---
+++ WowPacketParserModule.V6_0_2_19033/Parsers/BattlegroundHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V6_0_2_19033.Parsers
+{
+    public static class BattlegroundHandler
+    {
+
+        [Parser(Opcode.SMSG_BATTLEFIELD_RATED_INFO)]
+        public static void HandleBattlefieldRatedInfo(Packet packet)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                packet.ReadInt32("PersonalRating", i);
+                packet.ReadInt32("Ranking", i);
+                packet.ReadInt32("SeasonPlayed", i);
+                packet.ReadInt32("SeasonWon", i);
+                packet.ReadInt32("WeeklyPlayed", i);
+                packet.ReadInt32("WeeklyWon", i);
+                packet.ReadInt32("BestWeeklyRating", i);
+                packet.ReadInt32("BestSeasonRating", i);
+            }
+        }
+    }
+}
