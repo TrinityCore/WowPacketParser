f8fc9ff071aad0cb1fd868ffdb30d0bf7e299cbd
Vincent_Michael <Vincent_Michael@gmx.de>
2014-11-09 18:52:42 +0100

    Added CMSG_CHAR_RACE_OR_FACTION_CHANGE opcode and structure for 6.0.3

--- WowPacketParser/Enums/Version/V6_0_3_19103/Opcodes.cs
+++ WowPacketParser/Enums/Version/V6_0_3_19103/Opcodes.cs
@@ -29,6 +29,7 @@ namespace WowPacketParser.Enums.Version.V6_0_3_19103
             {Opcode.CMSG_CHAR_CREATE, 0x070D},
             {Opcode.CMSG_CHAR_DELETE, 0x030E},
             {Opcode.CMSG_CHAR_ENUM, 0x0918},
+            {Opcode.CMSG_CHAR_RACE_OR_FACTION_CHANGE, 0x1209},
             {Opcode.CMSG_CHAR_UNDELETE_ENUM, 0x0F2D},
             {Opcode.CMSG_CREATURE_QUERY, 0x0505},
             {Opcode.CMSG_DB_QUERY_BULK, 0x138B | 0x10000},
--- WowPacketParserModule.V6_0_2_19033/Parsers/CharacterHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/CharacterHandler.cs
@@ -203,6 +203,41 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             packet.ReadWoWString("NewName", bits16);
         }
 
+        [Parser(Opcode.CMSG_CHAR_RACE_OR_FACTION_CHANGE)]
+        public static void HandleCharRaceOrFactionChange(Packet packet)
+        {
+            packet.ReadBit("FactionChange");
+
+            var bits20 = packet.ReadBits(6);
+
+            var bit93 = packet.ReadBit("HasSkinID");
+            var bit96 = packet.ReadBit("HasHairColor");
+            var bit89 = packet.ReadBit("HasHairStyleID");
+            var bit17 = packet.ReadBit("HasFacialHairStyleID");
+            var bit19 = packet.ReadBit("HasFaceID");
+
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadByte("SexID");
+            packet.ReadByte("RaceID");
+
+            packet.ReadWoWString("Name", bits20);
+
+            if (bit93)
+                packet.ReadByte("SkinID");
+
+            if (bit96)
+                packet.ReadByte("HairColorID");
+
+            if (bit89)
+                packet.ReadByte("HairStyleID");
+
+            if (bit17)
+                packet.ReadByte("FacialHairStyleID");
+
+            if (bit19)
+                packet.ReadByte("FaceID");
+        }
+
         [Parser(Opcode.CMSG_SET_LOOT_SPECIALIZATION)]
         public static void HandleSetLootSpecialization(Packet packet)
         {
