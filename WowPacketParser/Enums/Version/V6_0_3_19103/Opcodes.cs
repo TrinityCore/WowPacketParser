185414a9db3c61f0346507312e6d8a408b011ffe
Vincent_Michael <Vincent_Michael@gmx.de>
2014-11-10 23:35:09 +0100

    Added SMSG_GROUP_LIST opcode for 603 (todo: addet structure)

--- WowPacketParser/Enums/Version/V6_0_3_19103/Opcodes.cs
+++ WowPacketParser/Enums/Version/V6_0_3_19103/Opcodes.cs
@@ -55,6 +55,7 @@ namespace WowPacketParser.Enums.Version.V6_0_3_19103
             {Opcode.CMSG_GAMEOBJ_USE, 0x082E},
             {Opcode.CMSG_GOSSIP_HELLO, 0x00F3},
             {Opcode.CMSG_GOSSIP_SELECT_OPTION, 0x0332 | 0x10000},
+            {Opcode.SMSG_GROUP_LIST, 0x15BE},
             {Opcode.CMSG_GUILD_QUERY, 0x0930},
             {Opcode.CMSG_GUILD_QUERY_RANKS, 0x0C37},
             {Opcode.CMSG_GUILD_REQUEST_PARTY_STATE, 0x0A8E},
