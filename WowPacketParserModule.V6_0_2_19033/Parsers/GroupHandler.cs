d2c781f3afa58ac12bcbfb9827848ec0e1904d7a
Vincent_Michael <Vincent_Michael@gmx.de>
2014-11-12 19:29:10 +0100

    Added SMSG_GROUP_LIST structure for 6.0.3

--- WowPacketParserModule.V6_0_2_19033/Parsers/GroupHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/GroupHandler.cs
@@ -19,5 +19,75 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             packet.ReadPackedGuid128("Sender");
             packet.ReadVector2("Position");
         }
+
+        [Parser(Opcode.SMSG_GROUP_LIST)]
+        public static void HandleGroupList(Packet packet)
+        {
+            packet.ReadByte("PartyFlags");
+            packet.ReadByte("PartyIndex");
+            packet.ReadByte("PartyType");
+
+            packet.ReadInt32("MyIndex");
+            packet.ReadPackedGuid128("LeaderGUID");
+            packet.ReadInt32("SequenceNum");
+            packet.ReadPackedGuid128("PartyGUID");
+
+            var int13 = packet.ReadInt32("PlayerListCount");
+            for (int i = 0; i < int13; i++)
+            {
+                packet.ResetBitReader();
+                var bits76 = packet.ReadBits(6);
+
+                packet.ReadPackedGuid128("Guid", i);
+
+                packet.ReadByte("Connected", i);
+                packet.ReadByte("Subgroup", i);
+                packet.ReadByte("Flags", i);
+                packet.ReadByte("RolesAssigned", i);
+                packet.ReadByte("Unk Byte", i);
+
+                packet.ReadWoWString("Name", bits76, i);
+            }
+
+            packet.ResetBitReader();
+
+            var bit68 = packet.ReadBit("HasLfgInfo");
+            var bit144 = packet.ReadBit("HasLootSettings");
+            var bit164 = packet.ReadBit("HasDifficultySettings");
+
+            if (bit68)
+            {
+                packet.ReadByte("MyLfgFlags");
+                packet.ReadInt32("LfgSlot");
+                packet.ReadInt32("MyLfgRandomSlot");
+                packet.ReadByte("MyLfgPartialClear");
+                packet.ReadSingle("MyLfgGearDiff");
+                packet.ReadByte("MyLfgStrangerCount");
+                packet.ReadByte("MyLfgKickVoteCount");
+                packet.ReadByte("LfgBootCount");
+
+                packet.ResetBitReader();
+
+                packet.ReadBit("LfgAborted");
+                packet.ReadBit("MyLfgFirstReward");
+            }
+
+            if (bit144)
+            {
+                packet.ReadByte("LootMethod");
+                packet.ReadPackedGuid128("LootMaster");
+                packet.ReadByte("LootThreshold");
+            }
+
+            if (bit164)
+            {
+                packet.ReadInt32("Unk Int4");
+                //for (int i = 0; i < 2; i++)
+                //{
+                    packet.ReadInt32("DungeonDifficultyID");
+                    packet.ReadInt32("RaidDifficultyID");
+                //}
+            }
+        }
     }
 }
