82c956937f911094c2b583be559c658faebb48d9
Vincent-Michael <Vincent_Michael@gmx.de>
2015-01-02 04:41:19 +0100

    Added SMSG_TRADE_UPDATED opcode and structure for 6.0.3

--- WowPacketParserModule.V6_0_2_19033/Parsers/TradeHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/TradeHandler.cs
@@ -11,5 +11,57 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
         {
             packet.ReadUInt32("State Index");
         }
+
+        [Parser(Opcode.SMSG_TRADE_UPDATED)]
+        public static void HandleTradeUpdated(Packet packet)
+        {
+            packet.ReadByte("WhichPlayer");
+
+            packet.ReadInt32("ID");
+            packet.ReadInt32("CurrentStateIndex");
+            packet.ReadInt32("ClientStateIndex");
+
+            packet.ReadInt64("Gold");
+
+            // Order guessed
+            packet.ReadInt32("CurrencyType");
+            packet.ReadInt32("CurrencyQuantity");
+            packet.ReadInt32("ProposedEnchantment");
+
+            var count = packet.ReadInt32("ItemCount");
+
+            for (int i = 0; i < count; i++)
+            {
+                packet.ReadByte("Slot", i);
+                packet.ReadInt32("EntryID", i);
+                packet.ReadInt32("StackCount", i);
+
+                packet.ReadPackedGuid128("GiftCreator", i);
+
+                packet.ResetBitReader();
+
+                var bit32 = packet.ReadBit("HasUnwrapped", i);
+                if (bit32)
+                {
+                    ItemHandler.ReadItemInstance(packet, i);
+
+                    packet.ReadInt32("EnchantID", i);
+                    packet.ReadInt32("OnUseEnchantmentID", i);
+
+                    for (int j = 0; j < 3; j++)
+                        packet.ReadInt32("SocketEnchant", i, j);
+
+                    packet.ReadPackedGuid128("Creator", i);
+
+                    packet.ReadInt32("MaxDurability", i);
+                    packet.ReadInt32("Durability", i);
+                    packet.ReadInt32("Charges", i);
+
+                    packet.ResetBitReader();
+
+                    packet.ReadBit("Lock", i);
+                }
+            }
+        }
     }
 }
