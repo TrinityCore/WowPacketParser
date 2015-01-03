a900e044a77757a4ba0d6877ffe9891dfc423e2f
Vincent-Michael <Vincent_Michael@gmx.de>
2015-01-03 19:59:57 +0100

    Added SMSG_PET_STABLE_LIST opcode for 6.0.3

--- WowPacketParser/Enums/Version/V6_0_3_19103/Opcodes.cs
+++ WowPacketParser/Enums/Version/V6_0_3_19103/Opcodes.cs
@@ -527,6 +527,7 @@ namespace WowPacketParser.Enums.Version.V6_0_3_19103
             {Opcode.SMSG_PET_NAME_QUERY_RESPONSE, 0x0D0E},
             {Opcode.SMSG_PET_SPECIALIZATION, 0x1989},
             {Opcode.SMSG_PET_SPELLS, 0x0174},
+            {Opcode.SMSG_PET_STABLE_LIST, 0x0B1F},
             {Opcode.SMSG_PET_LEARNED_SPELL, 0x1822},
             {Opcode.SMSG_PET_REMOVED_SPELL, 0x011B},
             {Opcode.SMSG_PET_GUIDS, 0x150F},
