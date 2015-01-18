7ab2a4a5c89fbfaf40ebb4f0aa5419088a910e3a
Vincent-Michael <Vincent_Michael@gmx.de>
2015-01-18 20:45:04 +0100

    Fix unks

--- WowPacketParserModule.V6_0_2_19033/Parsers/GroupHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/GroupHandler.cs
@@ -254,8 +254,8 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
         [Parser(Opcode.SMSG_PARTY_MEMBER_STATE)]
         public static void HandlePartyMemberState(Packet packet)
         {
-            packet.ReadBit("");
-            packet.ReadPackedGuid128("");
+            packet.ReadBit("ForEnemy");
+            packet.ReadPackedGuid128("MemberGuid");
 
             for (var i = 0; i < 2; i++)
                 packet.ReadByte("Unk704", i);
