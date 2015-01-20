64ce36da249904f2024127f1aa948d550b0fb0cd
Nayd <dnpd.dd@gmail.com>
2015-01-20 17:49:43 +0000

    Update LootError enum

--- WowPacketParser/Enums/LootError.cs
+++ WowPacketParser/Enums/LootError.cs
@@ -3,9 +3,13 @@
     public enum LootError
     {
         DidntKill            = 0,
+        //Unk1               = 1,
+        //Unk2               = 2,
+        //Unk3               = 3,
         TooFar               = 4,
         BadFacing            = 5,
         Locked               = 6,
+        //Unk7               = 7,
         NotStanding          = 8,
         Stunned              = 9,
         PlayerNotFound       = 10,
@@ -13,8 +17,9 @@
         MasterInvFull        = 12,
         MasterUniqueItem     = 13,
         MasterOther          = 14,
-        AlreadyPickpockted   = 15,
+        AlreadyPickpocketed  = 15,
         NotWhileShapeshifted = 16,
-        NoError              = 17
+        NoLoot               = 17, // NC
+        None                 = 18  // NC
     }
 }
