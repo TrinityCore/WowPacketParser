4bb888e08d90210a7046a58fe44a91120ba9b67f
Vincent-Michael <Vincent_Michael@gmx.de>
2015-01-22 21:26:58 +0100

    Added CMSG_QUERY_SCENARIO_POI, SMSG_SCENARIO_POIS, SMSG_SCENARIO_PROGRESS_UPDATE, SMSG_SCENARIO_STATE structure for 6.0.3

--- WowPacketParserModule.V6_0_2_19033/Parsers/SceneHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/SceneHandler.cs
@@ -6,6 +6,19 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
 {
     public static class SceneHandler
     {
+        public static void ReadCriteriaProgress(Packet packet, params object[] idx)
+        {
+            packet.ReadInt32("Id");
+            packet.ReadInt64("Quantity");
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadPackedTime("Date");
+            packet.ReadTime("TimeFromStart");
+            packet.ReadTime("TimeFromCreate");
+
+            packet.ResetBitReader();
+            packet.ReadBits("Flags", 4);
+        }
+
         [Parser(Opcode.SMSG_CANCEL_SCENE)]
         [Parser(Opcode.CMSG_SCENE_PLAYBACK_COMPLETE)]
         [Parser(Opcode.CMSG_SCENE_PLAYBACK_CANCELED)]
@@ -53,6 +66,79 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
         public static void HandleSceneObjectPetBattleFinished(Packet packet)
         {
             packet.ReadPackedGuid128("SceneObjectGUID");
+        }
+
+        [Parser(Opcode.CMSG_QUERY_SCENARIO_POI)]
+        public static void HandleQueryScenarioPOI(Packet packet)
+        {
+            var missingScenarioPOITreeCount = packet.ReadInt32("MissingScenarioPOITreeCount");
+            for (var i = 0; i < missingScenarioPOITreeCount; i++)
+                packet.ReadInt32("MissingScenarioPOITreeIDs", i);
+        }
+
+        [Parser(Opcode.SMSG_SCENARIO_POIS)]
+        public static void HandleScenarioPOIs(Packet packet)
+        {
+            var scenarioPOIDataCount = packet.ReadInt32("ScenarioPOIDataCount");
+            for (var i = 0; i < scenarioPOIDataCount; i++)
+            {
+                packet.ReadInt32("CriteriaTreeID");
+
+                var scenarioBlobDataCount = packet.ReadInt32("ScenarioBlobDataCount");
+                for (int j = 0; j < scenarioBlobDataCount; j++)
+                {
+                    packet.ReadInt32("BlobID", i, j);
+                    packet.ReadInt32("MapID", i, j);
+                    packet.ReadInt32("WorldMapAreaID", i, j);
+                    packet.ReadInt32("Floor", i, j);
+                    packet.ReadInt32("Priority", i, j);
+                    packet.ReadInt32("Flags", i, j);
+                    packet.ReadInt32("WorldEffectID", i, j);
+                    packet.ReadInt32("PlayerConditionID", i, j);
+
+                    var scenarioPOIPointDataCount = packet.ReadInt32("ScenarioPOIPointDataCount", i, j);
+                    for (int k = 0; k < scenarioPOIPointDataCount; k++)
+                    {
+                        packet.ReadInt32("X", i, j, k);
+                        packet.ReadInt32("Y", i, j, k);
+                    }
+                }
+            }
+        }
+
+        [Parser(Opcode.SMSG_SCENARIO_PROGRESS_UPDATE)]
+        public static void HandleScenarioProgressUpdate(Packet packet)
+        {
+            ReadCriteriaProgress(packet);
+        }
+
+        [Parser(Opcode.SMSG_SCENARIO_STATE)]
+        public static void HandleScenarioState(Packet packet)
+        {
+            packet.ReadInt32("ScenarioID");
+            packet.ReadInt32("CurrentStep");
+            packet.ReadInt32("DifficultyID");
+            packet.ReadInt32("WaveCurrent");
+            packet.ReadInt32("WaveMax");
+            packet.ReadInt32("TimerDuration");
+
+            var int36 = packet.ReadInt32("CriteriaProgressCount");
+            var int20 = packet.ReadInt32("BonusObjectiveDataCount");
+
+            for (int i = 0; i < int36; i++)
+                ReadCriteriaProgress(packet, i);
+
+            for (int i = 0; i < int20; i++)
+            {
+                packet.ReadInt32("BonusObjectiveID", i);
+
+                packet.ResetBitReader();
+                packet.ReadBit("ObjectiveComplete", i);
+            }
+
+            packet.ResetBitReader();
+
+            packet.ReadBit("ScenarioComplete");
         }
     }
 }
