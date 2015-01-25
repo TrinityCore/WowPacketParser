65442709cccad74b0843b44126b51ac5e3fdf834
Vincent-Michael <Vincent_Michael@gmx.de>
2015-01-25 02:32:44 +0100

    Added SMSG_BATTLEGROUND_PLAYER_LEFT structure for 6.0.3

--- WowPacketParserModule.V6_0_2_19033/Parsers/BattlegroundHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/BattlegroundHandler.cs
@@ -298,6 +298,7 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
         }
 
         [Parser(Opcode.SMSG_BATTLEGROUND_PLAYER_JOINED)]
+        [Parser(Opcode.SMSG_BATTLEGROUND_PLAYER_LEFT)]
         public static void HandleBattlegroundPlayerJoined(Packet packet)
         {
             packet.ReadPackedGuid128("Guid");
