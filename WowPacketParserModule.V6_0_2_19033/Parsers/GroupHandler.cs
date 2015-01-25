612ef38763fd02cf13ecbecec569d95f07e8d580
Vincent-Michael <Vincent_Michael@gmx.de>
2015-01-25 05:31:21 +0100

    Added CMSG_SET_PARTY_LEADER structure for 6.0.3

--- WowPacketParserModule.V6_0_2_19033/Parsers/GroupHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/GroupHandler.cs
@@ -525,5 +525,12 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             packet.ReadPackedGuid128("ChangedUnit");
             packet.ReadInt32("Role");
         }
+
+        [Parser(Opcode.CMSG_SET_PARTY_LEADER)]
+        public static void HandleSetPartyLeader(Packet packet)
+        {
+            packet.ReadByte("PartyIndex");
+            packet.ReadPackedGuid128("Target");
+        }
     }
 }
