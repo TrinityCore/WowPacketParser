251d0abf6880d4274eeb57ec6843e7280990c686
Vincent-Michael <Vincent_Michael@gmx.de>
2015-01-25 05:53:22 +0100

    Added CMSG_DF_SET_ROLES structure for 6.0.3

--- WowPacketParserModule.V6_0_2_19033/Parsers/LfgHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/LfgHandler.cs
@@ -451,5 +451,12 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
         {
             packet.ReadBit("TeleportOut");
         }
+
+        [Parser(Opcode.CMSG_DF_SET_ROLES)]
+        public static void HandleDFSetRoles(Packet packet)
+        {
+            packet.ReadUInt32("RolesDesired");
+            packet.ReadByte("PartyIndex");
+        }
     }
 }
