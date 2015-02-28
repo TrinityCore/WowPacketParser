f6319e5fdbe6b966bffeb2ae34b47f273f59afe9
Vincent-Michael <Vincent_Michael@gmx.de>
2015-02-28 03:07:54 +0100

    Added more opcodes for 6.1.0

--- WowPacketParser/Enums/Version/V6_1_0_19678/Opcodes.cs
+++ WowPacketParser/Enums/Version/V6_1_0_19678/Opcodes.cs
@@ -6,18 +6,41 @@ namespace WowPacketParser.Enums.Version.V6_1_0_19678
     {
         public static BiDictionary<Opcode, int> Opcodes(Direction direction)
         {
-            if (direction == Direction.ClientToServer || direction == Direction.BNClientToServer)
-                return ClientOpcodes;
-            if (direction == Direction.ServerToClient || direction == Direction.BNServerToClient)
-                return ServerOpcodes;
+            switch (direction)
+            {
+                case Direction.ClientToServer:
+                case Direction.BNClientToServer:
+                    return ClientOpcodes;
+                case Direction.ServerToClient:
+                case Direction.BNServerToClient:
+                    return ServerOpcodes;
+            }
             return MiscOpcodes;
         }
 
-        private static readonly BiDictionary<Opcode, int> ClientOpcodes = new BiDictionary<Opcode, int>();
+        private static readonly BiDictionary<Opcode, int> ClientOpcodes = new BiDictionary<Opcode, int>
+        {
+            {Opcode.CMSG_AUTH_SESSION, 0x1872},
+            {Opcode.CMSG_GUILD_QUERY, 0x19B3},
+            {Opcode.CMSG_WARDEN_DATA, 0x11E3},
+        };
 
         private static readonly BiDictionary<Opcode, int> ServerOpcodes = new BiDictionary<Opcode, int>
         {
+            {Opcode.SMSG_ADDON_INFO, 0x1F5C},
+            {Opcode.SMSG_AUTH_CHALLENGE, 0x0403},
+            {Opcode.SMSG_AUTH_RESPONSE, 0x0B61},
+            {Opcode.SMSG_BATTLE_PAY_GET_DISTRIBUTION_LIST_RESPONSE, 0x17A3},
+            {Opcode.SMSG_BATTLE_PAY_GET_PURCHASE_LIST_RESPONSE, 0x1FC9}, 
+            {Opcode.SMSG_CLIENTCACHE_VERSION, 0x116C},
+            {Opcode.SMSG_CHAR_ENUM, 0x13F2},
+            {Opcode.SMSG_FEATURE_SYSTEM_STATUS_GLUE_SCREEN, 0x117A},
+            {Opcode.SMSG_GUILD_QUERY_RESPONSE, 0x06F3},
+            {Opcode.SMSG_REDIRECT_CLIENT, 0x0413},
+            {Opcode.SMSG_SET_TIME_ZONE_INFORMATION, 0x15B4},
+            {Opcode.SMSG_UNDELETE_COOLDOWN_STATUS_RESPONSE, 0x1DDB},
             {Opcode.SMSG_UPDATE_OBJECT, 0x1762},
+            {Opcode.SMSG_WARDEN_DATA, 0x110A},
         };
 
         private static readonly BiDictionary<Opcode, int> MiscOpcodes = new BiDictionary<Opcode, int>();
