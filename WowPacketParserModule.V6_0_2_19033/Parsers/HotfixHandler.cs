b217a0a65d4b45e6e9669de6ab8e4ca53c5a2988
Vincent-Michael <Vincent_Michael@gmx.de>
2015-02-28 20:10:59 +0100

    Fix ItemExtendedCost for 6.1.0 (To-Do: Research unk)

--- WowPacketParserModule.V6_0_2_19033/Parsers/HotfixHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/HotfixHandler.cs
@@ -192,8 +192,12 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
                 case DB2Hash.ItemExtendedCost: // New structure - 6.0.2
                 {
                     db2File.ReadUInt32("Item Extended Cost ID");
-                    db2File.ReadUInt32("Required Honor Points");
-                    db2File.ReadUInt32("Required Arena Points");
+                    if (!ClientVersion.AddedInVersion(ClientVersionBuild.V6_1_0_19678))
+                    {
+                        db2File.ReadUInt32("Required Honor Points");
+                        db2File.ReadUInt32("Required Arena Points");
+                    }
+
                     db2File.ReadUInt32("Required Arena Slot");
                     for (var i = 0; i < 5; ++i)
                         db2File.ReadUInt32("Required Item", i);
@@ -213,6 +217,8 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
                     db2File.ReadUInt32("Required Faction Standing");
                     db2File.ReadUInt32("Requirement Flags");
                     db2File.ReadInt32<AchievementId>("Required Achievement");
+                    if (ClientVersion.AddedInVersion(ClientVersionBuild.V6_1_0_19678))
+                        db2File.ReadInt32("Unk1 Wod61x");
                     break;
                 }
                 case DB2Hash.ItemCurrencyCost:
