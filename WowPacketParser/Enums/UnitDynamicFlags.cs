63e86fce48c6e39a45a42b61e3076748f8f388b0
Vincent-Michael <Vincent_Michael@gmx.de>
2015-06-06 21:28:18 +0200

    Update UnitDynamicFlags for WoD

--- WowPacketParser/Enums/UnitDynamicFlags.cs
+++ WowPacketParser/Enums/UnitDynamicFlags.cs
@@ -15,4 +15,19 @@ namespace WowPacketParser.Enums
         ReferAFriendLinked    = 0x0040,
         TappedByAllThreatList = 0x0080
     }
+
+    [Flags]
+    public enum UnitDynamicFlagsWOD
+    {
+        None                  = 0x0000,
+        HideModel             = 0x0001,
+        Lootable              = 0x0002,
+        TrackUnit             = 0x0004,
+        Tapped                = 0x0008,
+        TappedByPlayer        = 0x0010,
+        EmpathyInfo           = 0x0020,
+        AppearDead            = 0x0040,
+        ReferAFriendLinked    = 0x0080,
+        TappedByAllThreatList = 0x0100
+    }
 }
