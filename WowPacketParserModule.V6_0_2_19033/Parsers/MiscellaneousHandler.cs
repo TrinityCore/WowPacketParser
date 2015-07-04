b7e3608b7f1ab9e90633b70f2872555c7518b892
Vincent-Michael <Vincent_Michael@gmx.de>
2015-07-04 21:57:55 +0200

    Added missing line

--- WowPacketParserModule.V6_0_2_19033/Parsers/MiscellaneousHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/MiscellaneousHandler.cs
@@ -245,7 +245,9 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             packet.ReadByte("IsTournamentRealm");
             packet.ReadTime("WeeklyReset");
 
-            packet.ReadBit("XRealmPvpAlert");
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V6_2_0_20173))
+                packet.ReadBit("XRealmPvpAlert");
+
             var hasRestrictedAccountMaxLevel = packet.ReadBit("HasRestrictedAccountMaxLevel");
             var hasRestrictedAccountMaxMoney = packet.ReadBit("HasRestrictedAccountMaxMoney");
             var hasIneligibleForLootMask = packet.ReadBit("HasIneligibleForLootMask");
