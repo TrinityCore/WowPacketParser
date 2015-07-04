b7810ffb5d79beaa882952520531efd5f4e55a43
Vincent-Michael <Vincent_Michael@gmx.de>
2015-07-04 20:12:01 +0200

    Update SMSG_UPDATE_OBJECT for 6.2.0

--- WowPacketParserModule.V6_0_2_19033/Parsers/UpdateHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/UpdateHandler.cs
@@ -182,12 +182,12 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
                         packet.ResetBitReader();
 
                         packet.ReadBitsE<SplineFlag434>("SplineFlags", 25, index);
-                        var face = packet.ReadBits("Face", 2, index);
+                        var face = packet.ReadBits("Face", ClientVersion.AddedInVersion(ClientVersionBuild.V6_2_0_20173) ? 4 : 2, index);
 
                         var hasJumpGravity = packet.ReadBit("HasJumpGravity", index);
                         var hasSpecialTime = packet.ReadBit("HasSpecialTime", index);
 
-                        packet.ReadBitsE<SplineMode>("Mode", 2, index);
+                        packet.ReadBitsE<SplineMode>("Mode", ClientVersion.AddedInVersion(ClientVersionBuild.V6_2_0_20173) ? 4 : 2, index);
 
                         var hasSplineFilterKey = packet.ReadBit("HasSplineFilterKey", index);
 
