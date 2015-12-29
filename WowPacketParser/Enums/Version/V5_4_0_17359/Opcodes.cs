1d7649635c8222fb03321c594479a946a5e3525b
vincent_michael <vincent_michael@gmx.de>
2015-12-29 23:26:34 +0100

    Fixed build in bc5d77a57e2961241e315042ecfbb318302570c6

--- WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_0_17359/Opcodes.cs
@@ -38,7 +38,7 @@ namespace WowPacketParser.Enums.Version.V5_4_0_17359
             {Opcode.CMSG_QUERY_PAGE_TEXT, 0x1082},
             {Opcode.CMSG_QUERY_PET_NAME, 0x1428},
             {Opcode.CMSG_PLAYER_LOGIN, 0x1BC7},
-            {Opcode.CMSG_QUERY_WORLD_COUNTDOWN_TIMER, 0x10A8},
+            {Opcode.CMSG_QUERY_COUNTDOWN_TIMER, 0x10A8},
             {Opcode.CMSG_QUERY_QUEST_COMPLETION_NPCS, 0x1084},
             {Opcode.CMSG_QUEST_POI_QUERY, 0x1287},
             {Opcode.CMSG_REALM_SPLIT, 0x129F},
