e8126478eecd3addbe2f9f942db39d8d5a78a9b2
Ehsan619 <ehsan.hesam13@gmail.com>
2016-07-26 18:50:16 +0430

    Update to 7.0.3 Added version support for 22280, 22289, 22293 Use 22280 as base, previous builds ignored nothing has changed since 222xx Added UpdateFiedls thanks to @Shauren Added +251 ServerMessages and +650 ClientMessages More Incoming

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -257,6 +257,10 @@ namespace WowPacketParser.Misc
                     case ClientVersionBuild.V6_2_4_21676:
                     case ClientVersionBuild.V6_2_4_21742:
                         return ClientVersionBuild.V6_0_2_19033;
+                    case ClientVersionBuild.V7_0_3_22280:
+                    case ClientVersionBuild.V7_0_3_22289:
+                    case ClientVersionBuild.V7_0_3_22293:
+                        return ClientVersionBuild.V7_0_3_22280;
                     default:
                         return Build;
                 }
