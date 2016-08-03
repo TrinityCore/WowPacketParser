33408634cc19be3008f4e1bfc8725a6c2b849e2d
DDuarte <dnpd.dd@gmail.com>
2016-08-03 04:15:56 +0100

    7.x AuctionHouse packets changes
    Source: https://github.com/TrinityCore/TrinityCore/commit/836221aaf5420afe8ff4719780426251c222b87d

---
+++ WowPacketParserModule.V7_0_3_22280/Parsers/ItemHandler.cs
@@ -0,0 +1,13 @@
+using WowPacketParser.Misc;
+
+namespace WowPacketParserModule.V7_0_3_22280.Parsers
+{
+    public static class ItemHandler
+    {
+        public static void ReadItemGemInstanceData(Packet packet, params object[] idx)
+        {
+            packet.ReadByte("Slot", idx);
+            V6_0_2_19033.Parsers.ItemHandler.ReadItemInstance(packet, "Item", idx);
+        }
+    }
+}
