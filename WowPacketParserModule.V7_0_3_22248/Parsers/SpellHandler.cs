b7b5c9d1a433f522ea034bd3bfff85d0b2827930
vincent-michael <vincent_michael@gmx.de>
2016-08-11 09:31:58 +0200

    Update SMSG_SPELL_START / SMSG_SPELL_GO structure for 7.0.3

--- WowPacketParserModule.V7_0_3_22248/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/SpellHandler.cs
@@ -1,20 +1,137 @@
-using System.Collections.Generic;
 using WowPacketParser.Enums;
 using WowPacketParser.Misc;
 using WowPacketParser.Parsing;
-using WowPacketParser.Store;
-using WowPacketParser.Store.Objects;
-using SpellCastFailureReason = WowPacketParser.Enums.Version.V6_1_0_19678.SpellCastFailureReason;
 
 namespace WowPacketParserModule.V7_0_3_22248.Parsers
 {
     public static class SpellHandler
     {
+        public static void ReadSpellCastData(Packet packet, params object[] idx)
+        {
+            packet.ReadPackedGuid128("CasterGUID", idx);
+            packet.ReadPackedGuid128("CasterUnit", idx);
+
+            packet.ReadPackedGuid128("CastID", idx);
+            packet.ReadPackedGuid128("OriginalCastID", idx);
+
+            packet.ReadInt32<SpellId>("SpellID", idx);
+            packet.ReadUInt32("SpellXSpellVisualID", idx);
+
+            packet.ReadUInt32("CastFlags", idx);
+            packet.ReadUInt32("CastTime", idx);
+
+            V6_0_2_19033.Parsers.SpellHandler.ReadMissileTrajectoryResult(packet, idx, "MissileTrajectory");
+
+            packet.ReadInt32("Ammo.DisplayID", idx);
+
+            packet.ReadByte("DestLocSpellCastIndex", idx);
+
+            V6_0_2_19033.Parsers.SpellHandler.ReadCreatureImmunities(packet, idx, "Immunities");
+
+            V6_0_2_19033.Parsers.SpellHandler.ReadSpellHealPrediction(packet, idx, "Predict");
+
+            packet.ResetBitReader();
+
+            packet.ReadBits("CastFlagsEx", 22, idx);
+            var hitTargetsCount = packet.ReadBits("HitTargetsCount", 16, idx);
+            var missTargetsCount = packet.ReadBits("MissTargetsCount", 16, idx);
+            var missStatusCount = packet.ReadBits("MissStatusCount", 16, idx);
+            var remainingPowerCount = packet.ReadBits("RemainingPowerCount", 9, idx);
+
+            var hasRuneData = packet.ReadBit("HasRuneData", idx);
+
+            var targetPointsCount = packet.ReadBits("TargetPointsCount", 16, idx);
+
+            for (var i = 0; i < missStatusCount; ++i)
+                V6_0_2_19033.Parsers.SpellHandler.ReadSpellMissStatus(packet, idx, "MissStatus", i);
+
+            ReadSpellTargetData(packet, idx, "Target");
+
+            for (var i = 0; i < hitTargetsCount; ++i)
+                packet.ReadPackedGuid128("HitTarget", idx, i);
+
+            for (var i = 0; i < missTargetsCount; ++i)
+                packet.ReadPackedGuid128("MissTarget", idx, i);
+
+            for (var i = 0; i < remainingPowerCount; ++i)
+                V6_0_2_19033.Parsers.SpellHandler.ReadSpellPowerData(packet, idx, "RemainingPower", i);
+
+            if (hasRuneData)
+                V6_0_2_19033.Parsers.SpellHandler.ReadRuneData(packet, idx, "RemainingRunes");
+
+            for (var i = 0; i < targetPointsCount; ++i)
+                V6_0_2_19033.Parsers.SpellHandler.ReadLocation(packet, idx, "TargetPoints", i);
+        }
+
+        public static void ReadSpellTargetData(Packet packet, params object[] idx)
+        {
+            packet.ResetBitReader();
+
+            packet.ReadBitsE<TargetFlag>("Flags", 25, idx);
+            var hasSrcLoc = packet.ReadBit("HasSrcLocation", idx);
+            var hasDstLoc = packet.ReadBit("HasDstLocation", idx);
+            var hasOrient = packet.ReadBit("HasOrientation", idx);
+            var hasMapID = packet.ReadBit("hasMapID ", idx);
+            var nameLength = packet.ReadBits(7);
+
+            packet.ReadPackedGuid128("Unit", idx);
+            packet.ReadPackedGuid128("Item", idx);
+
+            if (hasSrcLoc)
+                V6_0_2_19033.Parsers.SpellHandler.ReadLocation(packet, "SrcLocation");
+
+            if (hasDstLoc)
+                V6_0_2_19033.Parsers.SpellHandler.ReadLocation(packet, "DstLocation");
+
+            if (hasOrient)
+                packet.ReadSingle("Orientation", idx);
+
+            if (hasMapID)
+                packet.ReadInt32("MapID", idx);
+
+            packet.ReadWoWString("Name", nameLength, idx);
+        }
+
+        public static void ReadSpellCastLogData(Packet packet, params object[] idx)
+        {
+            packet.ReadInt64("Health", idx);
+            packet.ReadInt32("AttackPower", idx);
+            packet.ReadInt32("SpellPower", idx);
+
+            var spellLogPowerDataCount = packet.ReadBits("SpellLogPowerData", 9, idx);
+
+            // SpellLogPowerData
+            for (var i = 0; i < spellLogPowerDataCount; ++i)
+            {
+                packet.ReadInt32("PowerType", idx, i);
+                packet.ReadInt32("Amount", idx, i);
+                packet.ReadInt32("Cost", idx, i);
+            }
+        }
+
         [Parser(Opcode.SMSG_SPELL_PREPARE)]
         public static void SpellPrepare(Packet packet)
         {
             packet.ReadPackedGuid128("ClientCastID");
             packet.ReadPackedGuid128("ServerCastID");
+        }
+
+        [Parser(Opcode.SMSG_SPELL_START)]
+        public static void HandleSpellStart(Packet packet)
+        {
+            ReadSpellCastData(packet, "Cast");
+        }
+
+        [Parser(Opcode.SMSG_SPELL_GO)]
+        public static void HandleSpellGo(Packet packet)
+        {
+            ReadSpellCastData(packet, "Cast");
+
+            packet.ResetBitReader();
+
+            var hasLogData = packet.ReadBit();
+            if (hasLogData)
+                ReadSpellCastLogData(packet, "LogData");
         }
     }
 }
