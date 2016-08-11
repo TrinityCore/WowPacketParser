2ba3b62de490bb446fd80f59b6b8b2c0c700d66c
vincent-michael <vincent_michael@gmx.de>
2016-08-11 11:13:45 +0200

    Updated SMSG_SPELL_NON_MELEE_DAMAGE_LOG structure for 7.0.3

---
+++ WowPacketParserModule.V7_0_3_22248/Parsers/CombatLogHandler.cs
@@ -0,0 +1,69 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V7_0_3_22248.Parsers
+{
+    public static class CombatLogHandler
+    {
+        public static void ReadSandboxScalingData(Packet packet, params object[] idx)
+        {
+            packet.ReadByte("Type", idx);
+            packet.ReadByte("TargetLevel", idx);
+            packet.ReadByte("Expansion", idx);
+            packet.ReadByte("Class", idx);
+            packet.ReadByte("TargetMinScalingLevel", idx);
+            packet.ReadByte("TargetMaxScalingLevel", idx);
+            packet.ReadInt16("PlayerLevelDelta", idx);
+            packet.ReadSByte("TargetScalingLevelDelta", idx);
+        }
+
+        [Parser(Opcode.SMSG_SPELL_NON_MELEE_DAMAGE_LOG)]
+        public static void HandleSpellNonMeleeDmgLog(Packet packet)
+        {
+            packet.ReadPackedGuid128("Me");
+            packet.ReadPackedGuid128("CasterGUID");
+            packet.ReadPackedGuid128("CastID");
+
+            packet.ReadInt32<SpellId>("SpellID");
+            packet.ReadInt32("Damage");
+            packet.ReadInt32("OverKill");
+
+            packet.ReadByte("SchoolMask");
+
+            packet.ReadInt32("ShieldBlock");
+            packet.ReadInt32("Resisted");
+            packet.ReadInt32("Absorbed");
+
+            packet.ResetBitReader();
+
+            packet.ReadBit("Periodic");
+
+            packet.ReadBitsE<AttackerStateFlags>("Flags", 7);
+
+            var hasDebugData = packet.ReadBit("HasDebugData");
+            var hasLogData = packet.ReadBit("HasLogData");
+            var hasSandboxScaling = packet.ReadBit("HasSandboxScaling");
+
+            if (hasDebugData)
+            {
+                packet.ReadSingle("CritRoll");
+                packet.ReadSingle("CritNeeded");
+                packet.ReadSingle("HitRoll");
+                packet.ReadSingle("HitNeeded");
+                packet.ReadSingle("MissChance");
+                packet.ReadSingle("DodgeChance");
+                packet.ReadSingle("ParryChance");
+                packet.ReadSingle("BlockChance");
+                packet.ReadSingle("GlanceChance");
+                packet.ReadSingle("CrushChance");
+            }
+
+            if (hasLogData)
+                SpellHandler.ReadSpellCastLogData(packet, "SpellCastLogData");
+
+            if (hasSandboxScaling)
+                ReadSandboxScalingData(packet, "SandboxScalingData");
+        }
+    }
+}
