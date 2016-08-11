820503234753629dac6eb6d1535a3475405abc95
vincent-michael <vincent_michael@gmx.de>
2016-08-11 11:29:57 +0200

    Update SMSG_FEATURE_SYSTEM_STATUS_GLUE_SCREEN structure for 7.0.3

---
+++ WowPacketParserModule.V7_0_3_22248/Parsers/MiscellaneousHandler.cs
@@ -0,0 +1,29 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V7_0_3_22248.Parsers
+{
+    public static class MiscellaneousHandler
+    {
+
+        [Parser(Opcode.SMSG_FEATURE_SYSTEM_STATUS_GLUE_SCREEN)]
+        public static void HandleFeatureSystemStatusGlueScreen(Packet packet)
+        {
+            packet.ReadBit("BpayStoreEnabled");
+            packet.ReadBit("BpayStoreAvailable");
+            packet.ReadBit("BpayStoreDisabledByParentalControls");
+            packet.ReadBit("CharUndeleteEnabled");
+            packet.ReadBit("CommerceSystemEnabled");
+            packet.ReadBit("Unk14");
+            packet.ReadBit("WillKickFromWorld");
+            packet.ReadBit("IsExpansionPreorderInStore");
+            packet.ReadBit("KioskModeEnabled");
+            packet.ReadBit("NoHandler"); // not accessed in handler
+            packet.ReadBit("TrialBoostEnabled");
+
+            packet.ReadInt32("TokenPollTimeSeconds");
+            packet.ReadInt32E<ConsumableTokenRedeem>("TokenRedeemIndex");
+        }
+    }
+}
