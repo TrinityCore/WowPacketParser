6a6a694df3fe7581f13c4ade534490baea816b73
vincent-michael <vincent_michael@gmx.de>
2016-08-11 15:42:56 +0200

    Updated SMSG_ITEM_PUSH_RESULT structure for 7.0.3

--- WowPacketParserModule.V7_0_3_22248/Parsers/ItemHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/ItemHandler.cs
@@ -1,4 +1,6 @@
+using WowPacketParser.Enums;
 using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
 
 namespace WowPacketParserModule.V7_0_3_22248.Parsers
 {
@@ -8,6 +10,38 @@ namespace WowPacketParserModule.V7_0_3_22248.Parsers
         {
             packet.ReadByte("Slot", idx);
             V6_0_2_19033.Parsers.ItemHandler.ReadItemInstance(packet, "Item", idx);
+        }
+
+        [Parser(Opcode.SMSG_ITEM_PUSH_RESULT)]
+        public static void HandleItemPushResult(Packet packet)
+        {
+            packet.ReadPackedGuid128("PlayerGUID");
+
+            packet.ReadByte("Slot");
+
+            packet.ReadInt32("SlotInBag");
+
+            packet.ReadUInt32("QuestLogItemID");
+            packet.ReadUInt32("Quantity");
+            packet.ReadUInt32("QuantityInInventory");
+            packet.ReadInt32("DungeonEncounterID");
+
+            packet.ReadUInt32("BattlePetBreedID");
+            packet.ReadUInt32("BattlePetBreedQuality");
+            packet.ReadUInt32("BattlePetSpeciesID");
+            packet.ReadUInt32("BattlePetLevel");
+
+            packet.ReadPackedGuid128("ItemGUID");
+
+            packet.ResetBitReader();
+
+            packet.ReadBit("Pushed");
+            packet.ReadBit("Created");
+            packet.ReadBits("DisplayText", 3);
+            packet.ReadBit("IsBonusRoll");
+            packet.ReadBit("IsEncounterLoot");
+
+            V6_0_2_19033.Parsers.ItemHandler.ReadItemInstance(packet, "ItemInstance");
         }
     }
 }
