2f00d2de9e1235ba72d562e5cb965b344f8729c6
vincent-michael <vincent_michael@gmx.de>
2016-08-12 20:22:00 +0200

    Updated SMSG_QUERY_TIME_RESPONSE structure for 7.0.3

---
+++ WowPacketParserModule.V7_0_3_22248/Parsers/SessionHandler.cs
@@ -0,0 +1,16 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V7_0_3_22248.Parsers
+{
+    public static class SessionHandler
+    {
+
+        [Parser(Opcode.SMSG_QUERY_TIME_RESPONSE)]
+        public static void HandleQueryTimeResponse(Packet packet)
+        {
+            packet.ReadTime("CurrentTime");
+        }
+    }
+}
