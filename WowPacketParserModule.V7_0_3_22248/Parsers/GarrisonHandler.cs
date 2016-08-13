a9d6fb693c02497dfccf6e48c9866970e12a8f51
vincent-michael <vincent_michael@gmx.de>
2016-08-13 17:34:52 +0200

    Updated SMSG_DISPLAY_TOAST structure for 7.0.3

---
+++ WowPacketParserModule.V7_0_3_22248/Parsers/GarrisonHandler.cs
@@ -0,0 +1,35 @@
+using System.Reflection.Emit;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V7_0_3_22248.Parsers
+{
+    public static class GarrisonHandler
+    {
+        [Parser(Opcode.SMSG_DISPLAY_TOAST)]
+        public static void HandleDisplayToast(Packet packet)
+        {
+            packet.ReadUInt64("Quantity");
+
+            packet.ReadByte("DisplayToastMethod");
+            packet.ReadUInt32("QuestID");
+
+            packet.ResetBitReader();
+
+            packet.ReadBit("Mailed");
+
+            var type = packet.ReadBits("Type", 2);
+            if (type == 0)
+            {
+                packet.ReadBit("BonusRoll");
+                V6_0_2_19033.Parsers.ItemHandler.ReadItemInstance(packet);
+                packet.ReadInt32("SpecializationID");
+                packet.ReadInt32("ItemQuantity?");
+            }
+
+            if (type == 1)
+                packet.ReadInt32("CurrencyID");
+        }
+    }
+}
