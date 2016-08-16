425a612f37dcc59e0063bc511bf5bdfc3edc1bf3
vincent-michael <vincent_michael@gmx.de>
2016-08-16 05:54:06 +0200

    Added SceneScript db2 structure

---
+++ WowPacketParserModule.V7_0_3_22248/Hotfix/SceneScriptEntry.cs
@@ -0,0 +1,14 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Hotfix;
+
+namespace WowPacketParserModule.V7_0_3_22248.Hotfix
+{
+    [HotfixStructure(DB2Hash.SceneScript)]
+    public class SceneScriptEntry
+    {
+        public string Name { get; set; }
+        public string Script { get; set; }
+        public ushort PreviousScript { get; set; }
+        public ushort NextScript { get; set; }
+    }
+}
