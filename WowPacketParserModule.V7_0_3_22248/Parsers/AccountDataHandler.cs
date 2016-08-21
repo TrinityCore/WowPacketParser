36712db525012d5792db00f388398e71871e5d93
vincent-michael <vincent_michael@gmx.de>
2016-08-21 22:01:44 +0200

    Added CMSG_SAVE_CLIENT_VARIABLES structure for 7.0.3

---
+++ WowPacketParserModule.V7_0_3_22248/Parsers/AccountDataHandler.cs
@@ -0,0 +1,24 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V7_0_3_22248.Parsers
+{
+    public static class AccountDataHandler
+    {
+
+        [Parser(Opcode.CMSG_SAVE_CLIENT_VARIABLES)]
+        public static void HandleSaveClientVarables(Packet packet)
+        {
+            var varablesCount = packet.ReadUInt32("VarablesCount");
+
+            for (var i = 0; i < varablesCount; ++i)
+            {
+                var variableNameLen = packet.ReadBits(6);
+                var valueLen = packet.ReadBits(10);
+
+                packet.WriteLine($" VariableName: \"{ packet.ReadWoWString((int)variableNameLen) }\" Value: \"{ packet.ReadWoWString((int)valueLen) }\"");
+            }
+        }
+    }
+}
