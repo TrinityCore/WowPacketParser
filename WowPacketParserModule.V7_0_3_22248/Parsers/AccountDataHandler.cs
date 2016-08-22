87ded8ef383b6c4e1d496144fc5871c87337d707
vincent-michael <vincent_michael@gmx.de>
2016-08-22 20:48:14 +0200

    Updated SMSG_RAID_INSTANCE_MESSAGE structure for 7.0.3

--- WowPacketParserModule.V7_0_3_22248/Parsers/AccountDataHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/AccountDataHandler.cs
@@ -16,7 +16,7 @@ namespace WowPacketParserModule.V7_0_3_22248.Parsers
                 var variableNameLen = packet.ReadBits(6);
                 var valueLen = packet.ReadBits(10);
 
-                packet.WriteLine($"[{i.ToString() }]VariableName: \"{ packet.ReadWoWString((int)variableNameLen) }\" Value: \"{ packet.ReadWoWString((int)valueLen) }\"");
+                packet.WriteLine($"[{ i.ToString() }] VariableName: \"{ packet.ReadWoWString((int)variableNameLen) }\" Value: \"{ packet.ReadWoWString((int)valueLen) }\"");
             }
         }
 
