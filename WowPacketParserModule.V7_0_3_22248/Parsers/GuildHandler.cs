773773ea4b4fe5eca945a7b0057f0897b603c5da
vincent-michael <vincent_michael@gmx.de>
2016-10-08 23:07:28 +0200

    Updated SMSG_GUILD_ROSTER structure for 7.0.3

--- WowPacketParserModule.V7_0_3_22248/Parsers/GuildHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/GuildHandler.cs
@@ -39,5 +39,60 @@ namespace WowPacketParserModule.V7_0_3_22248.Parsers
                 packet.ReadWoWString("Guild Name", nameLen);
             }
         }
+
+        [Parser(Opcode.SMSG_GUILD_ROSTER)]
+        public static void HandleGuildRoster(Packet packet)
+        {
+            packet.ReadUInt32("NumAccounts");
+            packet.ReadPackedTime("CreateDate");
+            packet.ReadUInt32("GuildFlags");
+            var int20 = packet.ReadUInt32("MemberDataCount");
+
+            packet.ResetBitReader();
+            var bits2037 = packet.ReadBits(10);
+            var bits9 = packet.ReadBits(11);
+
+            for (var i = 0; i < int20; ++i)
+            {
+                packet.ReadPackedGuid128("Guid", i);
+
+                packet.ReadUInt32("RankID", i);
+                packet.ReadUInt32<AreaId>("AreaID", i);
+                packet.ReadUInt32("PersonalAchievementPoints", i);
+                packet.ReadUInt32("GuildReputation", i);
+
+                packet.ReadSingle("LastSave", i);
+
+                for (var j = 0; j < 2; ++j)
+                {
+                    packet.ReadUInt32("DbID", i, j);
+                    packet.ReadUInt32("Rank", i, j);
+                    packet.ReadUInt32("Step", i, j);
+                }
+
+                packet.ReadUInt32("VirtualRealmAddress", i);
+
+                packet.ReadByteE<GuildMemberFlag>("Status", i);
+                packet.ReadByte("Level", i);
+                packet.ReadByteE<Class>("ClassID", i);
+                packet.ReadByteE<Gender>("Gender", i);
+
+                packet.ResetBitReader();
+
+                var bits36 = packet.ReadBits(6);
+                var bits92 = packet.ReadBits(8);
+                var bits221 = packet.ReadBits(8);
+
+                packet.ReadBit("Authenticated", i);
+                packet.ReadBit("SorEligible", i);
+
+                packet.ReadWoWString("Name", bits36, i);
+                packet.ReadWoWString("Note", bits92, i);
+                packet.ReadWoWString("OfficerNote", bits221, i);
+            }
+
+            packet.ReadWoWString("WelcomeText", bits2037);
+            packet.ReadWoWString("InfoText", bits9);
+        }
     }
 }
