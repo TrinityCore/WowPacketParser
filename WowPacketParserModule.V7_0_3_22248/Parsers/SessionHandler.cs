a28af95b06b35b078fe6adac81642dbd9b312330
vincent-michael <vincent_michael@gmx.de>
2016-10-13 21:57:35 +0200

    Added SMSG_ENABLE_ENCRYPTION structure for Legion

--- WowPacketParserModule.V7_0_3_22248/Parsers/SessionHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/SessionHandler.cs
@@ -6,11 +6,14 @@ namespace WowPacketParserModule.V7_0_3_22248.Parsers
 {
     public static class SessionHandler
     {
-
         [Parser(Opcode.SMSG_QUERY_TIME_RESPONSE)]
         public static void HandleQueryTimeResponse(Packet packet)
         {
             packet.ReadTime("CurrentTime");
+        }
+        [Parser(Opcode.SMSG_ENABLE_ENCRYPTION)]
+        public static void HandleSessionZero(Packet packet)
+        {
         }
     }
 }
