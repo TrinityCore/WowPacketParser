0667d875f7c57e73324b2d31259319c664dabcf4
vincent-michael <vincent_michael@gmx.de>
2016-10-23 21:48:55 +0200

    Added SMSG_AREA_TRIGGER_RE_SHAPE structure for 7.0.3

--- WowPacketParserModule.V7_0_3_22248/Parsers/AreaTriggerHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/AreaTriggerHandler.cs
@@ -25,5 +25,41 @@ namespace WowPacketParserModule.V7_0_3_22248.Parsers
             packet.ReadPackedGuid128("TriggerGUID");
             ReadAreaTriggerSpline(packet);
         }
+
+        [Parser(Opcode.SMSG_AREA_TRIGGER_RE_SHAPE)]
+        public static void HandleAreaTriggerReShape(Packet packet)
+        {
+            packet.ReadPackedGuid128("TriggerGUID");
+
+            packet.ResetBitReader();
+            var hasAreaTriggerSpline = packet.ReadBit("HasAreaTriggerSpline");
+            var hasAreaTriggerUnkType = packet.ReadBit("HasAreaTriggerUnkType");
+
+            if (hasAreaTriggerSpline)
+                ReadAreaTriggerSpline(packet);
+
+            if (hasAreaTriggerUnkType)
+            {
+                packet.ResetBitReader();
+                var unk1 = packet.ReadBit("AreaTriggerUnk1");
+                var hasCenter = packet.ReadBit("HasCenter");
+                packet.ReadBit("Unk bit 703 1");
+                packet.ReadBit("Unk bit 703 2");
+
+                packet.ReadUInt32("Unk UInt 1");
+                packet.ReadInt32("Unk Int 1");
+                packet.ReadUInt32("Unk UInt 2");
+                packet.ReadSingle("Radius");
+                packet.ReadSingle("BlendFromRadius");
+                packet.ReadSingle("InitialAngel");
+                packet.ReadSingle("ZOffset");
+
+                if (unk1)
+                    packet.ReadPackedGuid128("AreaTriggerUnkGUID");
+
+                if (hasCenter)
+                    packet.ReadVector3("Center");
+            }
+        }
     }
 }
