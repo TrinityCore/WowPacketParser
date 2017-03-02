6a6a4961e65ad67e14a3e34361ff9f7423789f0a
vincent-michael <vincent_michael@gmx.de>
2017-03-02 20:24:33 +0100

    Fixed DungeonEncounter structure

--- WowPacketParserModule.V7_0_3_22248/Hotfix/DungeonEncounterEntry.cs
+++ WowPacketParserModule.V7_0_3_22248/Hotfix/DungeonEncounterEntry.cs
@@ -3,9 +3,10 @@ using WowPacketParser.Hotfix;
 
 namespace WowPacketParserModule.V7_0_3_22248.Hotfix
 {
-    [HotfixStructure(DB2Hash.DungeonEncounter, HasIndexInData = false)]
+    [HotfixStructure(DB2Hash.DungeonEncounter)]
     public class DungeonEncounterEntry
     {
+        public uint ID { get; set; }
         public string Name { get; set; }
         public uint CreatureDisplayID { get; set; }
         public ushort MapID { get; set; }
