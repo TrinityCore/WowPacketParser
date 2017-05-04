3cbe758ed34ce7c7be5d8602de0dd1050efda1f0
Golrag <golrag.jeremy@gmail.com>
2017-05-04 12:05:33 +0200

    Add some hotfixes:
    * PrestigeLevelInfoEntry
    * PvpRewardEntry
    * RewardPackEntry
    * RewardPackXItemEntry

---
+++ WowPacketParserModule.V7_0_3_22248/Hotfix/PrestigeLevelInfoEntry.cs
@@ -0,0 +1,14 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Hotfix;
+
+namespace WowPacketParserModule.V7_0_3_22248.Hotfix
+{
+    [HotfixStructure(DB2Hash.PrestigeLevelInfo, HasIndexInData = false)]
+    public class PrestigeLevelInfoEntry
+    {
+        public uint IconID { get; set; }
+        public string PrestigeText { get; set; }
+        public byte PrestigeLevel { get; set; }
+        public byte Flags { get; set; }
+    }
+}
