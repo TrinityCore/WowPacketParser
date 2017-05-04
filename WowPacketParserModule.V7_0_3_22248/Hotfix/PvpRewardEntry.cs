c56162b656193ae0a4f5f06aa221836d3c4abdee
Golrag <Golrag@users.noreply.github.com>
2017-05-04 12:19:58 +0200

    Add some hotfixes: (#311)
    * PrestigeLevelInfoEntry
    * PvpRewardEntry
    * RewardPackEntry
    * RewardPackXItemEntry

---
+++ WowPacketParserModule.V7_0_3_22248/Hotfix/PvpRewardEntry.cs
@@ -0,0 +1,13 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Hotfix;
+
+namespace WowPacketParserModule.V7_0_3_22248.Hotfix
+{
+    [HotfixStructure(DB2Hash.PvpReward, HasIndexInData = false)]
+    public class PvpRewardEntry
+    {
+        public uint HonorLevel { get; set; }
+        public uint Prestige { get; set; }
+        public uint RewardPackID { get; set; }
+    }
+}
---
+++ WowPacketParserModule.V7_0_3_22248/Hotfix/RewardPackEntry.cs
@@ -0,0 +1,16 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Hotfix;
+
+namespace WowPacketParserModule.V7_0_3_22248.Hotfix
+{
+    [HotfixStructure(DB2Hash.RewardPack, HasIndexInData = false)]
+    public class RewardPackEntry
+    {
+        public uint Money { get; set; }
+        public float ArtifactXPMultiplier { get; set; }
+        public byte ArtifactXPDifficulty { get; set; }
+        public byte ArtifactCategoryID { get; set; }
+        public uint TitleID { get; set; }
+        public uint Unused { get; set; }
+    }
+}
