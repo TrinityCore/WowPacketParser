a5943e5ec44ebeefa473e262f636091cb930a4f3
Golrag <Golrag@users.noreply.github.com>
2017-05-13 18:15:13 +0200

    Add hotfixes for PvpTalentEntry and PvpTalentUnlockEntry (#313)

---
+++ WowPacketParserModule.V7_0_3_22248/Hotfix/PvpTalentEntry.cs
@@ -0,0 +1,19 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Hotfix;
+
+namespace WowPacketParserModule.V7_0_3_22248.Hotfix
+{
+    [HotfixStructure(DB2Hash.PvpTalent, HasIndexInData = false)]
+    public class PvpTalentEntry
+    {
+        public uint SpellID { get; set; }
+        public uint OverridesSpellID { get; set; }
+        public string Description { get; set; }
+        public uint TierID { get; set; }
+        public uint ColumnIndex { get; set; }
+        public uint Flags { get; set; }
+        public uint ClassID { get; set; }
+        public uint SpecID { get; set; }
+        public uint Unknown { get; set; }
+    }
+}
