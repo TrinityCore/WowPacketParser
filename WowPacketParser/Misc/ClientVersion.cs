7b39552db536f3775cce6e97114a69f1c2b7c8a5
Hypnos <Daniel25@users.noreply.github.com>
2017-08-12 18:31:50 +0200

    Added support for build 7.2.5.24742 (#327)

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -182,7 +182,8 @@ namespace WowPacketParser.Misc
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V7_2_5_24414, new DateTime(2017, 06, 22, 21, 09, 06)),
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V7_2_5_24415, new DateTime(2017, 06, 22, 21, 41, 18)),
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V7_2_5_24430, new DateTime(2017, 06, 26)),
-            new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V7_2_5_24461, new DateTime(2017, 06, 29))
+            new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V7_2_5_24461, new DateTime(2017, 06, 29)),
+            new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V7_2_5_24742, new DateTime(2017, 08, 03))
         };
 
         private static ClientType _expansion;
@@ -372,6 +373,7 @@ namespace WowPacketParser.Misc
                     case ClientVersionBuild.V7_2_5_24415:
                     case ClientVersionBuild.V7_2_5_24430:
                     case ClientVersionBuild.V7_2_5_24461:
+                    case ClientVersionBuild.V7_2_5_24742:
                         return ClientVersionBuild.V7_0_3_22248;
                     case ClientVersionBuild.BattleNetV37165:
                         return ClientVersionBuild.BattleNetV37165;
