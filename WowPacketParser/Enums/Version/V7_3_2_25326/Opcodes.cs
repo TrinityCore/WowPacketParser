dbc1bb9d6141b30db5a8c768bf6267aea8ddb2fb
funjoker <torti-esser@web.de>
2017-11-07 02:58:26 +0100

    Add 7.3.x code

---
+++ WowPacketParser/Enums/Version/V7_3_2_25326/Opcodes.cs
@@ -0,0 +1,29 @@
+﻿using WowPacketParser.Misc;
+
+namespace WowPacketParser.Enums.Version.V7_3_2_25326
+{
+    public static class Opcodes_7_3_2
+    {
+        public static BiDictionary<Opcode, int> Opcodes(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.ClientToServer:
+                    return ClientOpcodes;
+                case Direction.ServerToClient:
+                    return ServerOpcodes;
+            }
+            return MiscOpcodes;
+        }
+
+        private static readonly BiDictionary<Opcode, int> ClientOpcodes = new BiDictionary<Opcode, int>
+        {
+        };
+
+        private static readonly BiDictionary<Opcode, int> ServerOpcodes = new BiDictionary<Opcode, int>
+        {
+        };
+
+        private static readonly BiDictionary<Opcode, int> MiscOpcodes = new BiDictionary<Opcode, int>();
+    }
+}
