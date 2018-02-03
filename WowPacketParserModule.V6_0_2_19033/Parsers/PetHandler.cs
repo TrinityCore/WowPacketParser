0c82ba79d4aacf54d8a95aa5ebd8a263e899246c
funjoker <torti-esser@web.de>
2018-02-04 00:19:41 +0100

    Sync PetParsing to WoD (#345)

--- WowPacketParserModule.V6_0_2_19033/Parsers/PetHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/PetHandler.cs
@@ -247,5 +247,34 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             packet.ReadInt32<SpellId>("SpellID");
             packet.ReadByteE<PetFeedback>("Response");
         }
+
+        [Parser(Opcode.CMSG_SET_PET_SLOT)]
+        public static void HandleSetPetSlot(Packet packet)
+        {
+            packet.ReadInt32("PetNumber");
+            packet.ReadByte("DestSlot");
+            packet.ReadPackedGuid128("StableMaster");
+        }
+
+        [Parser(Opcode.SMSG_PET_SLOT_UPDATED)]
+        public static void HandlePetSlotUpdated(Packet packet)
+        {
+            packet.ReadInt32("PetNumberA");
+            packet.ReadInt32("PetSlotA");
+            packet.ReadInt32("PetNumberB");
+            packet.ReadInt32("PetSlotB");
+        }
+
+        [Parser(Opcode.SMSG_PET_STABLE_RESULT)]
+        public static void HandlePetStableResult(Packet packet)
+        {
+            packet.ReadByteE<PetStableResult>("Result");
+        }
+
+        [Parser(Opcode.CMSG_REQUEST_STABLED_PETS)]
+        public static void HandleRequestStabledPets(Packet packet)
+        {
+            packet.ReadPackedGuid128("StableMaster");
+        }
     }
 }
