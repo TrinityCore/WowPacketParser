29d353ae2d615920f7ebeccbd0b15e019dce2687
ModoX <moardox@gmail.com>
2018-08-26 10:54:47 +0200

    using UpperCamelCase for ReportPvPAFKResult enum now

--- WowPacketParser/Enums/ReportPvPAFKResult.cs
+++ WowPacketParser/Enums/ReportPvPAFKResult.cs
@@ -2,9 +2,9 @@
 {
     public enum ReportPvPAFKResult : byte
     {
-        PVP_REPORT_AFK_SUCCESS          = 0,
-        PVP_REPORT_AFK_GENERIC_FAILURE  = 1, // there are more error codes but they are impossible to receive without modifying the client
-        PVP_REPORT_AFK_SYSTEM_ENABLED   = 5,
-        PVP_REPORT_AFK_SYSTEM_DISABLED  = 6
+        Success         = 0,
+        GenericFailure  = 1, // there are more error codes but they are impossible to receive without modifying the client
+        SystemEnabled   = 5,
+        SystemDisabled  = 6
     };
 }
