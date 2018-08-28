0a821a4edfab5ecf1a964d0eb034710a3457352a
funjoker <torti-esser@web.de>
2018-08-28 09:35:02 +0200

    Fix CMSG_MAIL_GET_LIST

---
+++ WowPacketParserModule.V7_0_3_22248/Parsers/MailHandler.cs
@@ -0,0 +1,15 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V7_0_3_22248.Parsers
+{
+    public static class MailHandler
+    {
+        [Parser(Opcode.CMSG_MAIL_GET_LIST)]
+        public static void HandleGetMailList(Packet packet)
+        {
+            packet.ReadPackedGuid128("Mailbox");
+        }
+    }
+}
