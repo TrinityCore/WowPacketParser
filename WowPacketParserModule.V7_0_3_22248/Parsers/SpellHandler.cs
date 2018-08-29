b360aa67d00e7063b22be26b57d8b4e687644018
funjoker <torti-esser@web.de>
2018-08-29 12:17:18 +0200

    Fix SMSG_NOTIFY_MISSILE_TRAJECTORY_COLLISION and CMSG_MISSILE_TRAJECTORY_COLLISION (#403)
    * Fix SMSG_NOTIFY_MISSILE_TRAJECTORY_COLLISION
    
    * Fix CMSG_MISSILE_TRAJECTORY_COLLISION

--- WowPacketParserModule.V7_0_3_22248/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/SpellHandler.cs
@@ -678,6 +678,23 @@ namespace WowPacketParserModule.V7_0_3_22248.Parsers
             packet.ReadPackedGuid128("CastID");
         }
 
+        [Parser(Opcode.SMSG_NOTIFY_MISSILE_TRAJECTORY_COLLISION)]
+        public static void HandleNotifyMissileTrajectoryCollision(Packet packet)
+        {
+            packet.ReadPackedGuid128("Caster");
+            packet.ReadPackedGuid128("CastID");
+            packet.ReadVector3("CollisionPos");
+        }
+
+        [Parser(Opcode.CMSG_MISSILE_TRAJECTORY_COLLISION)]
+        public static void HandleMissileTrajectoryCollision(Packet packet)
+        {
+            packet.ReadPackedGuid128("CasterGUID");
+            packet.ReadInt32<SpellId>("SpellID");
+            packet.ReadPackedGuid128("CastID");
+            packet.ReadVector3("CollisionPos");
+        }
+
         [Parser(Opcode.SMSG_SUPERCEDED_SPELLS)]
         public static void HandleSupercededSpells(Packet packet)
         {
