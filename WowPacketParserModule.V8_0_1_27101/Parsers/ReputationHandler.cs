13ec3e8fb29a78c4d6b393cf0167f4b53291e34a
funjoker <torti-esser@web.de>
2019-01-19 10:15:53 +0100

    Fix SMSG_INITIALIZE_FACTIONS & SMSG_FACTION_BONUS_INFO

---
+++ WowPacketParserModule.V8_0_1_27101/Parsers/ReputationHandler.cs
@@ -0,0 +1,31 @@
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V8_0_1_27101.Parsers
+{
+    public static class ReputationHandler
+    {
+        public static ushort FactionCount = 350; // 8.1.0
+
+        [Parser(Opcode.SMSG_FACTION_BONUS_INFO, ClientVersionBuild.V8_1_0_28724)]
+        public static void HandleFactionBonusInfo(Packet packet)
+        {
+            for (var i = 0; i < FactionCount; i++)
+                packet.ReadBit("FactionHasBonus", i);
+        }
+
+        [Parser(Opcode.SMSG_INITIALIZE_FACTIONS, ClientVersionBuild.V8_1_0_28724)]
+        public static void HandleInitializeFactions(Packet packet)
+        {
+            for (var i = 0; i < FactionCount; i++)
+            {
+                packet.ReadByteE<FactionFlag>("FactionFlags", i);
+                packet.ReadUInt32E<ReputationRank>("FactionStandings", i);
+            }
+
+            for (var i = 0; i < FactionCount; i++)
+                packet.ReadBit("FactionHasBonus", i);
+        }
+    }
+}
