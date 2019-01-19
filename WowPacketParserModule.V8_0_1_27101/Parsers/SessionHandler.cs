26eef09cfba6d383dc20cf55d678fd084a2b4c1f
funjoker <torti-esser@web.de>
2019-01-19 10:11:51 +0100

    Fix SMSG_REALM_QUERY_RESPONSE

--- WowPacketParserModule.V8_0_1_27101/Parsers/SessionHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/SessionHandler.cs
@@ -14,5 +14,27 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
             packet.ReadBytes("Digest", 32);
             packet.ReadBytes("SessionKey", sessionKeyLength);
         }
+
+        [Parser(Opcode.SMSG_REALM_QUERY_RESPONSE, ClientVersionBuild.V8_1_0_28724)]
+        public static void HandleRealmQueryResponse(Packet packet)
+        {
+            packet.ReadUInt32("VirtualRealmAddress");
+
+            var state = packet.ReadByte("LookupState");
+            if (state == 0)
+            {
+                packet.ResetBitReader();
+
+                packet.ReadBit("IsLocal");
+                packet.ReadBit("Unk bit");
+
+                var bits2 = packet.ReadBits(9);
+                var bits258 = packet.ReadBits(9);
+                packet.ReadBit();
+
+                packet.ReadWoWString("RealmNameActual", bits2);
+                packet.ReadWoWString("RealmNameNormalized", bits258);
+            }
+        }
     }
 }
