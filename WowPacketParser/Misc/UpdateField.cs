0638a536792329e0c7b2774d8dce4b2e6a2f7703
funjoker <torti-esser@web.de>
2019-02-17 18:21:45 +0100

    Fix some SQL ouput

--- WowPacketParser/Misc/UpdateField.cs
+++ WowPacketParser/Misc/UpdateField.cs
@@ -6,9 +6,34 @@ namespace WowPacketParser.Misc
     [StructLayout(LayoutKind.Explicit)]
     public struct UpdateField
     {
+        public UpdateField(byte val) : this()
+        {
+            UInt8Value = val;
+        }
+
+        public UpdateField(sbyte val) : this()
+        {
+            Int8Value = val;
+        }
+
+        public UpdateField(ushort val) : this()
+        {
+            UInt16Value = val;
+        }
+
+        public UpdateField(short val) : this()
+        {
+            Int16Value = val;
+        }
+
         public UpdateField(uint val) : this()
         {
             UInt32Value = val;
+        }
+
+        public UpdateField(int val) : this()
+        {
+            Int32Value = val;
         }
 
         public UpdateField(float val) : this()
@@ -16,9 +41,15 @@ namespace WowPacketParser.Misc
             FloatValue = val;
         }
 
+        [FieldOffset(0)] public readonly byte UInt8Value;
+        [FieldOffset(0)] public readonly sbyte Int8Value;
+        [FieldOffset(0)] public readonly ushort UInt16Value;
+        [FieldOffset(0)] public readonly short Int16Value;
         [FieldOffset(0)] public readonly uint UInt32Value;
         [FieldOffset(0)] public readonly int Int32Value;
         [FieldOffset(0)] public readonly float FloatValue;
+        [FieldOffset(4)] public readonly ulong UInt64Value;
+        [FieldOffset(4)] public readonly long Int64Value;
 
         public override bool Equals(object obj)
         {
@@ -30,6 +61,9 @@ namespace WowPacketParser.Misc
         public bool Equals(UpdateField other)
         {
             if (UInt32Value == other.UInt32Value)
+                return true;
+
+            if (UInt64Value == other.UInt64Value)
                 return true;
 
             if (Math.Abs(FloatValue - other.FloatValue) < float.Epsilon)
