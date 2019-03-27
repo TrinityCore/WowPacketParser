024578982ce3addbfb2b8771d809ec72113b8ebb
ModoX <moardox@gmail.com>
2019-03-27 19:02:28 +0100

    added SMSG_CHALLENGE_MODE_REWARDS

---
+++ WowPacketParserModule.V7_0_3_22248/Parsers/ChallengeModeHandler.cs
@@ -0,0 +1,16 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V7_0_3_22248.Parsers
+{
+    public static class ChallengeModeHandler
+    {
+        [Parser(Opcode.SMSG_CHALLENGE_MODE_REWARDS)]
+        public static void HandleChallengeModeRewards(Packet packet)
+        {
+            packet.ReadBit("IsWeeklyRewardAvailable");
+        }
+    }
+}
+
--- WowPacketParserModule.V8_0_1_27101/Parsers/ChallengeModeHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/ChallengeModeHandler.cs
@@ -8,5 +8,17 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
     {
         [Parser(Opcode.CMSG_REQUEST_CHALLENGE_MODE_AFFIXES)]
         public static void HandleChallengeModeZero(Packet packet) { }
+
+        [Parser(Opcode.SMSG_CHALLENGE_MODE_REWARDS)]
+        public static void HandleChallengeModeRewards(Packet packet)
+        {
+            packet.ReadBit("IsWeeklyRewardAvailable");
+
+            packet.ReadInt32("LastWeekHighestKeyCompleted");
+            packet.ReadInt32("LastWeekMapChallengeKeyEntry");
+            packet.ReadInt32("CurrentWeekHighestKeyCompleted");
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V8_1_0_28724))
+                packet.ReadInt32("UnkInt"); // always 13 for me
+        }
     }
 }
