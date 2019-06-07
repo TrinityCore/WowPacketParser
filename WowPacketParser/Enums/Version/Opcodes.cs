6ae5e6f54ece52a97ca745887a4f1073b4b584e1
ModoX <moardox@gmail.com>
2019-06-07 17:16:47 +0200

    Added build 8.1.5.30477 and 8.1.5.30669

--- WowPacketParser/Enums/Version/Opcodes.cs
+++ WowPacketParser/Enums/Version/Opcodes.cs
@@ -448,6 +448,8 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V8_1_5_29869:
                 case ClientVersionBuild.V8_1_5_29896:
                 case ClientVersionBuild.V8_1_5_29981:
+                case ClientVersionBuild.V8_1_5_30477:
+                case ClientVersionBuild.V8_1_5_30669:
                 {
                     return Opcodes_8_1_5.Opcodes(direction);
                 }
--- WowPacketParser/Enums/Version/UpdateFields.cs
+++ WowPacketParser/Enums/Version/UpdateFields.cs
@@ -513,6 +513,8 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V8_1_5_29869:
                 case ClientVersionBuild.V8_1_5_29896:
                 case ClientVersionBuild.V8_1_5_29981:
+                case ClientVersionBuild.V8_1_5_30477:
+                case ClientVersionBuild.V8_1_5_30669:
                 {
                     return "V8_1_5_29683";
                 }
