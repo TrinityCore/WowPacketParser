c4f2e506b6882a485e6a803d5aa2e0bf17166afa
funjoker <torti-esser@web.de>
2019-06-18 18:09:36 +0200

    Fix struct
    incorrect at TC

--- WowPacketParserModule.V8_0_1_27101/Hotfix/AnimationDataEntry.cs
+++ WowPacketParserModule.V8_0_1_27101/Hotfix/AnimationDataEntry.cs
@@ -6,9 +6,9 @@ namespace WowPacketParserModule.V8_0_1_27101.Hotfix
     [HotfixStructure(DB2Hash.AnimationData, HasIndexInData = false)]
     public class AnimationDataEntry
     {
-        public ushort Fallback { get; set; }
+        public ushort BehaviorID { get; set; }
         public byte BehaviorTier { get; set; }
-        public int BehaviorID { get; set; }
+        public int Fallback { get; set; }
         [HotfixArray(2)]
         public int[] Flags { get; set; }
     }
