7ed7ee1e0d568a63e6ed890cf41fb44853526a5b
ModoX <moardox@gmail.com>
2019-06-22 19:14:15 +0200

    Implement legacy implementation of updatefields for areatriggers (#443)

---
+++ WowPacketParser/Store/Objects/UpdateFields/LegacyImplementation/AreaTriggerData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+
+namespace WowPacketParser.Store.Objects.UpdateFields.LegacyImplementation
+{
+    public class AreaTriggerData : IAreaTriggerData
+    {
+        private WoWObject Object { get; }
+        private Dictionary<int, UpdateField> UpdateFields => Object.UpdateFields;
+
+        public AreaTriggerData(WoWObject obj)
+        {
+            Object = obj;
+        }
+
+        public int SpellID => UpdateFields.GetValue<AreaTriggerField, int>(AreaTriggerField.AREATRIGGER_SPELLID);
+        public uint TimeToTarget => UpdateFields.GetValue<AreaTriggerField, uint>(AreaTriggerField.AREATRIGGER_TIME_TO_TARGET);
+        public uint TimeToTargetScale => UpdateFields.GetValue<AreaTriggerField, uint>(AreaTriggerField.AREATRIGGER_TIME_TO_TARGET_SCALE);
+        public uint DecalPropertiesID => UpdateFields.GetValue<AreaTriggerField, uint>(AreaTriggerField.AREATRIGGER_DECAL_PROPERTIES_ID);
+    }
+}
