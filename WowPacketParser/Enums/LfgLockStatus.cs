4a0763bad687d45172d0e86f3144445df455b2ec
ModoX <moardox@gmail.com>
2019-07-06 09:28:03 +0200

    Added structs for * CMSG_LFG_LIST_INVITE_APPLICANT * SMSG_LFG_LIST_SEARCH_RESULTS * SMSG_LFG_LIST_SEARCH_STATUS * CMSG_LFG_LIST_CANCEL_APPLICATION * SMSG_LFG_LIST_UPDATE_STATUS * CMSG_LFG_LIST_UPDATE_REQUEST * SMSG_LFG_LIST_JOIN_RESULT * CMSG_LFG_LIST_DECLINE_APPLICANT * CMSG_LFG_LIST_APPLY_TO_GROUP * CMSG_LFG_LIST_SEARCH

---
+++ WowPacketParser/Enums/LfgLockStatus.cs
@@ -0,0 +1,19 @@
+﻿namespace WowPacketParser.Enums
+{
+    public enum LfgLockStatus
+    {
+        InsufficientExpansion        = 1,
+        TooLowLevel                  = 2,
+        TooHighLevel                 = 3,
+        TooLowGearScore              = 4,
+        TooHighGearScore             = 5,
+        RaidLocked                   = 6,
+        AttunementTooLowLevel        = 1001,
+        AttunementTooHighLevel       = 1002,
+        QuestNotCompleted            = 1022,
+        MissingItem                  = 1025,
+        NotInSeason                  = 1031,
+        MissingAchievement           = 1034,
+        TemporarilyDisabled          = 10000
+    }
+}
--- WowPacketParser/Enums/LocaleConstant.cs
+++ WowPacketParser/Enums/LocaleConstant.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace WowPacketParser.Enums
 {
@@ -17,5 +18,22 @@ namespace WowPacketParser.Enums
         none            = 9,
         ptBR            = 10,
         itIT            = 11
+    }
+
+    [Flags]
+    public enum LocaleConstantFlags : uint
+    {
+        enUS = (1 << 0),
+        koKR = (1 << 1),
+        frFR = (1 << 2),
+        deDE = (1 << 3),
+        zhCN = (1 << 4),
+        zhTW = (1 << 5),
+        esES = (1 << 6),
+        esMX = (1 << 7),
+        ruRU = (1 << 8),
+        none = (1 << 9),
+        ptBR = (1 << 10),
+        itIT = (1 << 11),
     }
 }
