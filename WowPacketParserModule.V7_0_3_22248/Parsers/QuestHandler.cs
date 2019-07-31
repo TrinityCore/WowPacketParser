7f3c7810242e6d2fefe6a173d23a34bf26c5bf07
ModoX <moardox@gmail.com>
2019-07-31 16:40:20 +0200

    Fixes SMSG_DISPLAY_PLAYER_CHOICE for patches < 7.3.5
    Closes #486

--- WowPacketParserModule.V7_0_3_22248/Parsers/QuestHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/QuestHandler.cs
@@ -632,7 +632,8 @@ namespace WowPacketParserModule.V7_0_3_22248.Parsers
             packet.ReadInt32("ChoiceID");
             var responseCount = packet.ReadUInt32();
             packet.ReadPackedGuid128("SenderGUID");
-            packet.ReadInt32("UiTextureKitID");
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V7_3_5_25848))
+                packet.ReadInt32("UiTextureKitID");
             packet.ResetBitReader();
             var questionLength = packet.ReadBits(8);
             if (ClientVersion.AddedInVersion(ClientVersionBuild.V7_2_5_24330))
