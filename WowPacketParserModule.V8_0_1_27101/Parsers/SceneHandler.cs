bc183ac31629212097d98ba38c4cc82569b0a85c
funjoker <funjoker109@gmail.com>
2019-12-29 01:13:33 +0100

    Fix BattlePet Opcodes * SMSG_SCENE_OBJECT_PET_BATTLE_REPLACEMENTS_MADE * SMSG_SCENE_OBJECT_PET_BATTLE_ROUND_RESULT * SMSG_SCENE_OBJECT_PET_BATTLE_FIRST_ROUND

---
+++ WowPacketParserModule.V8_0_1_27101/Parsers/SceneHandler.cs
@@ -0,0 +1,20 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V8_0_1_27101.Parsers
+{
+    public static class SceneHandler
+    {
+        [Parser(Opcode.SMSG_SCENE_OBJECT_PET_BATTLE_FIRST_ROUND)]
+        [Parser(Opcode.SMSG_SCENE_OBJECT_PET_BATTLE_ROUND_RESULT)]
+        [Parser(Opcode.SMSG_SCENE_OBJECT_PET_BATTLE_REPLACEMENTS_MADE)]
+        public static void HandleSceneObjectPetBattleRound(Packet packet)
+        {
+            packet.ReadPackedGuid128("SceneObjectGUID");
+            BattlePetHandler.ReadPetBattleRoundResult(packet, "MsgData");
+        }
+    }
+}
