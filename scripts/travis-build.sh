0b6e93c336273a8168b8f5767693c79b6fe065da
funjoker <funjoker109@gmail.com>
2020-01-18 20:50:25 +0100

    make me cry

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -6,7 +6,7 @@ if [ "${DOTNETCORE}" == "1" ]; then
     dotnet restore;
     dotnet build;
 else
-    xbuild /p:TargetFrameworkVersion="v4.6";
+    xbuild /p:TargetFrameworkVersion="v4.6.1";
     mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./WowPacketParser.Tests/bin/Debug/WowPacketParser.Tests.dll
 fi
 
