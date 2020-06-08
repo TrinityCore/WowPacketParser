c4282a1bacdae162ace199e0abbf0d2add8d4a0b
funjoker <funjoker109@gmail.com>
2020-06-09 00:17:10 +0200

    Fix SMSG_CRITERIA_UPDATE

---
+++ WowPacketParserModule.V8_0_1_27101/Parsers/AchievementHandler.cs
@@ -0,0 +1,37 @@
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.DBC;
+using System.Collections.Generic;
+
+namespace WowPacketParserModule.V8_0_1_27101.Parsers
+{
+    public static class AchievementHandler
+    {
+        public static Dictionary<int, ulong> FactionReputationStore { get; } = new Dictionary<int, ulong>();
+
+        [Parser(Opcode.SMSG_CRITERIA_UPDATE)]
+        public static void HandleCriteriaPlayer(Packet packet)
+        {
+            int criteriaId = packet.ReadInt32<CriteriaId>("CriteriaID");
+            ulong quantity = (ulong)packet.ReadInt64("Quantity");
+            packet.ReadPackedGuid128("PlayerGUID");
+            packet.ReadInt32("Flags");
+            packet.ReadPackedTime("CurrentTime");
+            packet.ReadTime("ElapsedTime");
+            packet.ReadTime("CreationTime");
+
+            var hasRafAcceptanceID = false;
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V8_2_5_31921))
+                hasRafAcceptanceID = packet.ReadBit();
+
+            if (hasRafAcceptanceID)
+                packet.ReadUInt64("RafAcceptanceID");
+
+            if (Settings.UseDBC)
+                if (DBC.Criteria.ContainsKey(criteriaId))
+                    if (DBC.Criteria[criteriaId].Type == 46)
+                        FactionReputationStore[DBC.Criteria[criteriaId].Asset] = quantity;
+        }
+    }
+}
