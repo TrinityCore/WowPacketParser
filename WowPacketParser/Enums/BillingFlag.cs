ff52d3463ad34636c7f95368058b33f3ec9d11df
Simplaf234 <68116034+Simplaf234@users.noreply.github.com>
2020-07-19 19:47:01 +0000

    Correct name for personal time

--- WowPacketParser/Enums/BillingFlag.cs
+++ WowPacketParser/Enums/BillingFlag.cs
@@ -10,7 +10,7 @@ namespace WowPacketParser.Enums
         Subscription     = 0x02,
         Trial            = 0x04,
         IGR              = 0x08,
-        PersonalPlan     = 0x10,
+        PersonalTime     = 0x10,
         TimeMixture      = 0x20,
         Restricted       = 0x40,
         EnableCais       = 0x80
