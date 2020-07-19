ca66e1cfd1db8d62fb9154de61daaae0496a834f
Simplaf234 <68116034+Simplaf234@users.noreply.github.com>
2020-07-19 19:37:59 +0000

    Give some billing flags better names

--- WowPacketParser/Enums/BillingFlag.cs
+++ WowPacketParser/Enums/BillingFlag.cs
@@ -7,10 +7,10 @@ namespace WowPacketParser.Enums
     {
         None             = 0x00,
         Unused           = 0x01,
-        RecurringBilling = 0x02,
-        Trial1           = 0x04,
-        Igr              = 0x08,
-        Usage            = 0x10,
+        Subscription     = 0x02,
+        Trial            = 0x04,
+        IGR              = 0x08,
+        PersonalPlan     = 0x10,
         TimeMixture      = 0x20,
         Restricted       = 0x40,
         EnableCais       = 0x80
