575ddb6869ac8f69821a8bc4c0de3d944bd767b0
funjoker <funjoker109@gmail.com>
2020-09-14 22:36:13 +0200

    Fix SMSG_RESUME_CAST

--- WowPacketParserModule.V8_0_1_27101/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/SpellHandler.cs
@@ -381,5 +381,16 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
                 packet.ReadByte("ConsumedCharges", i);
             }
         }
+
+        [Parser(Opcode.SMSG_RESUME_CAST)]
+        public static void HandleResumeCast(Packet packet)
+        {
+            packet.ReadPackedGuid128("CasterGUID");
+            packet.ReadInt32("SpellXSpellVisualID");
+            packet.ReadPackedGuid128("CastID");
+            packet.ReadPackedGuid128("Target");
+            packet.ReadInt32<SpellId>("SpellID");
+
+        }
     }
 }
