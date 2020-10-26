632ef4b7ae269c1796279bc1fee1793e872650bc
arcaniafr <56791020+arcaniafr@users.noreply.github.com>
2020-10-26 23:03:24 +0100

    Fix somes opcodes

---
+++ WowPacketParserModule.V9_0_1_36216/Parsers/CharacterHandler.cs
@@ -0,0 +1,157 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V9_0_1_36216.Parsers
+{
+    public static class CharacterHandler
+    {
+        public static void ReadCharactersData(Packet packet, params object[] idx)
+        {
+            packet.ReadPackedGuid128("Guid", idx);
+
+            packet.ReadUInt64("GuildClubMemberID", idx);
+
+            packet.ReadByte("ListPosition", idx);
+            var race = packet.ReadByteE<Race>("RaceID", idx);
+            var klass = packet.ReadByteE<Class>("ClassID", idx);
+            packet.ReadByteE<Gender>("SexID", idx);
+            packet.ReadByte("SkinID", idx);
+            packet.ReadByte("FaceID", idx);
+            packet.ReadByte("HairStyle", idx);
+            packet.ReadByte("HairColor", idx);
+            packet.ReadByte("FacialHairStyle", idx);
+
+            for (uint j = 0; j < 3; ++j)
+                packet.ReadByte("CustomDisplay", idx, j);
+
+            packet.ReadByte("ExperienceLevel", idx);
+            var zone = packet.ReadInt32<ZoneId>("ZoneID", idx);
+            var mapId = packet.ReadInt32<MapId>("MapID", idx);
+
+            var pos = packet.ReadVector3("PreloadPos", idx);
+
+            packet.ReadPackedGuid128("GuildGUID", idx);
+
+            packet.ReadUInt32("Flags", idx);
+            packet.ReadUInt32("Flags2", idx);
+            packet.ReadUInt32("Flags3", idx);
+            packet.ReadUInt32("PetCreatureDisplayID", idx);
+            packet.ReadUInt32("PetExperienceLevel", idx);
+            packet.ReadUInt32("PetCreatureFamilyID", idx);
+
+            for (uint j = 0; j < 2; ++j)
+                packet.ReadInt32("ProfessionIDs", idx, j);
+
+            for (uint j = 0; j < 23; ++j)
+            {
+                packet.ReadUInt32("DisplayID", idx, "VisualItems", j);
+                packet.ReadUInt32("DisplayEnchantID", idx, "VisualItems", j);
+                packet.ReadByteE<InventoryType>("InvType", idx, "VisualItems", j);
+                if (ClientVersion.AddedInVersion(ClientVersionBuild.V8_2_5_31921))
+                    packet.ReadByte("Subclass", idx, "VisualItems", j);
+            }
+
+            packet.ReadTime("LastPlayedTime", idx);
+
+            packet.ReadInt16("SpecID", idx);
+            packet.ReadInt32("Unknown703", idx);
+            packet.ReadInt32("InterfaceVersion", idx);
+            packet.ReadUInt32("Flags4", idx);
+            var unknown830lengths = new uint[packet.ReadUInt32()];
+            var unknown901lengths = new uint[packet.ReadUInt32()];
+
+            packet.ResetBitReader();
+
+            var nameLength = packet.ReadBits("Character Name Length", 6, idx);
+            var firstLogin = packet.ReadBit("FirstLogin", idx);
+            packet.ReadBit("BoostInProgress", idx);
+            packet.ReadBits("UnkWod61x", 5, idx);
+
+            for (var j = 0; j < unknown830lengths.Length; ++j)
+                unknown830lengths[j] = packet.ReadBits(6);
+
+            for (var j = 0; j < unknown830lengths.Length; ++j)
+                if (unknown830lengths[j] > 1)
+                    packet.ReadDynamicString("Unknown830", unknown830lengths[j], idx);
+
+            packet.ReadWoWString("Character Name", nameLength, idx);
+
+            if (firstLogin)
+            {
+                PlayerCreateInfo startPos = new PlayerCreateInfo { Race = race, Class = klass, Map = (uint)mapId, Zone = (uint)zone, Position = pos, Orientation = 0 };
+                Storage.StartPositions.Add(startPos, packet.TimeSpan);
+            }
+        }
+
+        [Parser(Opcode.SMSG_ENUM_CHARACTERS_RESULT)]
+        public static void HandleEnumCharactersResult(Packet packet)
+        {
+            packet.ReadBit("Success");
+            packet.ReadBit("IsDeletedCharacters");
+            packet.ReadBit("HasDemonHunterOnRealm");
+            packet.ReadBit("IsDemonHunterCreationAllowed");
+            var hasDisabledClassesMask = packet.ReadBit("HasDisabledClassesMask");
+            packet.ReadBit("IsAlliedRacesCreationAllowed");
+
+            var charsCount = packet.ReadUInt32("CharactersCount");
+            packet.ReadInt32("MaxCharacterLevel");
+            var raceUnlockCount = packet.ReadUInt32("RaceUnlockCount");			
+            var unlockedConditionalAppearanceCount = packet.ReadUInt32("UnlockedConditionalAppearanceCount");
+
+            if (hasDisabledClassesMask)
+                packet.ReadUInt32("DisabledClassesMask");
+
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V8_1_0_28724))
+            {
+                for (uint i = 0; i < unlockedConditionalAppearanceCount; ++i)
+                {
+                    packet.ReadUInt32("AchievementId", "UnlockedConditionalAppearance", i);
+                    packet.ReadUInt32("Unused", "UnlockedConditionalAppearance", i);
+                }
+            }
+
+            for (uint i = 0; i < charsCount; ++i)
+                ReadCharactersData(packet, i, "CharactersData");
+
+            for (var i = 0; i < raceUnlockCount; ++i)
+                V7_0_3_22248.Parsers.CharacterHandler.ReadRaceUnlockData(packet, i, "RaceUnlockData");
+        }
+
+        [Parser(Opcode.CMSG_CREATE_CHARACTER)]
+        public static void HandleClientCharCreate(Packet packet)
+        {
+            var nameLen = packet.ReadBits(6);
+            var hasTemplateSet = packet.ReadBit();
+
+            packet.ReadBit("IsTrialBoost");
+            packet.ReadByteE<Race>("RaceID");
+            packet.ReadByteE<Class>("ClassID");
+            packet.ReadByteE<Gender>("SexID");
+            packet.ReadByte("SkinID");
+            packet.ReadByte("FaceID");
+            packet.ReadByte("HairStyleID");
+            packet.ReadByte("HairColorID");
+            packet.ReadByte("FacialHairStyleID");
+            packet.ReadByte("OutfitID");
+
+            for (var i = 0; i < 3; i++)
+                packet.ReadByte("CustomDisplay", i);
+
+            packet.ReadWoWString("Name", nameLen);
+
+            if (hasTemplateSet)
+                packet.ReadInt32("TemplateSetID");
+        }
+
+        [Parser(Opcode.SMSG_CREATE_CHAR)]
+        public static void HandleCharResponse(Packet packet)
+        {
+            packet.ReadByteE<ResponseCode>("Response");
+            packet.ReadPackedGuid128("GUID");
+        }
+
+    }
+}
