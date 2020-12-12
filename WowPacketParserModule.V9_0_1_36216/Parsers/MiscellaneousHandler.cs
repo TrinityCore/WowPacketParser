7512a1d3267f9969e252049e67f7fa676df49d86
funjoker <funjoker109@gmail.com>
2020-12-12 23:32:22 +0100

    Update SMSG_FEATURE_SYSTEM_STATUS for shadowlands

--- WowPacketParserModule.V9_0_1_36216/Parsers/MiscellaneousHandler.cs
+++ WowPacketParserModule.V9_0_1_36216/Parsers/MiscellaneousHandler.cs
@@ -1,26 +1,106 @@
 using WowPacketParser.Enums;
 using WowPacketParser.Misc;
 using WowPacketParser.Parsing;
+using CoreParsers = WowPacketParser.Parsing.Parsers;
 
 namespace WowPacketParserModule.V9_0_1_36216.Parsers
 {
     public static class MiscellaneousHandler
     {
-        public static void ReadSavedThrottleObjectState(Packet packet, params object[] indexes)
+        [Parser(Opcode.SMSG_FEATURE_SYSTEM_STATUS)]
+        public static void HandleFeatureSystemStatus(Packet packet)
         {
-            packet.ReadUInt32("MaxTries", indexes);
-            packet.ReadUInt32("PerMilliseconds", indexes);
-            packet.ReadUInt32("TryCount", indexes);
-            packet.ReadUInt32("LastResetTimeBeforeNow", indexes);
-        }
-        public static void ReadEuropaTicketSystemStatus(Packet packet, params object[] indexes)
-        {
-            packet.ReadBit("TicketsEnabled", indexes);
-            packet.ReadBit("BugsEnabled", indexes);
-            packet.ReadBit("ComplaintsEnabled", indexes);
-            packet.ReadBit("SuggestionsEnabled", indexes);
+            packet.ReadByte("ComplaintStatus");
 
-            ReadSavedThrottleObjectState(packet, "SavedThrottleObjectState", indexes);
+            packet.ReadUInt32("ScrollOfResurrectionRequestsRemaining");
+            packet.ReadUInt32("ScrollOfResurrectionMaxRequestsPerDay");
+            packet.ReadUInt32("CfgRealmID");
+            packet.ReadInt32("CfgRealmRecID");
+            packet.ReadUInt32("MaxRecruits", "RAFSystem");
+            packet.ReadUInt32("MaxRecruitMonths", "RAFSystem");
+            packet.ReadUInt32("MaxRecruitmentUses", "RAFSystem");
+            packet.ReadUInt32("DaysInCycle", "RAFSystem");
+            packet.ReadUInt32("TwitterPostThrottleLimit");
+            packet.ReadUInt32("TwitterPostThrottleCooldown");
+            packet.ReadUInt32("TokenPollTimeSeconds");
+            packet.ReadUInt32("KioskSessionMinutes");
+            packet.ReadInt64("TokenBalanceAmount");
+            packet.ReadUInt32("BpayStoreProductDeliveryDelay");
+            packet.ReadUInt32("ClubsPresenceUpdateTimer");
+            packet.ReadUInt32("HiddenUIClubsPresenceUpdateTimer");
+
+            packet.ResetBitReader();
+            packet.ReadBit("VoiceEnabled");
+            var hasEuropaTicketSystemStatus = packet.ReadBit("HasEuropaTicketSystemStatus");
+            packet.ReadBit("ScrollOfResurrectionEnabled");
+            packet.ReadBit("BpayStoreEnabled");
+            packet.ReadBit("BpayStoreAvailable");
+            packet.ReadBit("BpayStoreDisabledByParentalControls");
+            packet.ReadBit("ItemRestorationButtonEnabled");
+            packet.ReadBit("BrowserEnabled");
+            var hasSessionAlert = packet.ReadBit("HasSessionAlert");
+            packet.ReadBit("Enabled", "RAFSystem");
+            packet.ReadBit("RecruitingEnabled", "RAFSystem");
+            packet.ReadBit("CharUndeleteEnabled");
+            packet.ReadBit("RestrictedAccount");
+            packet.ReadBit("CommerceSystemEnabled");
+            packet.ReadBit("TutorialsEnabled");
+            packet.ReadBit("TwitterEnabled");
+            packet.ReadBit("Unk67");
+            packet.ReadBit("WillKickFromWorld");
+            packet.ReadBit("KioskModeEnabled");
+            packet.ReadBit("CompetitiveModeEnabled");
+            packet.ReadBit("TokenBalanceEnabled");
+            packet.ReadBit("WarModeFeatureEnabled");
+            packet.ReadBit("ClubsEnabled");
+            packet.ReadBit("ClubsBattleNetClubTypeAllowed");
+            packet.ReadBit("ClubsCharacterClubTypeAllowed");
+            packet.ReadBit("ClubsPresenceUpdateEnabled");
+            packet.ReadBit("VoiceChatDisabledByParentalControl");
+            packet.ReadBit("VoiceChatMutedByParentalControl");
+            packet.ReadBit("QuestSessionEnabled");
+            packet.ReadBit("IsMuted");
+            packet.ReadBit("ClubFinderEnabled");
+            packet.ReadBit("Unknown901CheckoutRelated");
+
+            {
+                packet.ResetBitReader();
+                packet.ReadBit("ToastsDisabled");
+                packet.ReadSingle("ToastDuration");
+                packet.ReadSingle("DelayDuration");
+                packet.ReadSingle("QueueMultiplier");
+                packet.ReadSingle("PlayerMultiplier");
+                packet.ReadSingle("PlayerFriendValue");
+                packet.ReadSingle("PlayerGuildValue");
+                packet.ReadSingle("ThrottleInitialThreshold");
+                packet.ReadSingle("ThrottleDecayTime");
+                packet.ReadSingle("ThrottlePrioritySpike");
+                packet.ReadSingle("ThrottleMinThreshold");
+                packet.ReadSingle("ThrottlePvPPriorityNormal");
+                packet.ReadSingle("ThrottlePvPPriorityLow");
+                packet.ReadSingle("ThrottlePvPHonorThreshold");
+                packet.ReadSingle("ThrottleLfgListPriorityDefault");
+                packet.ReadSingle("ThrottleLfgListPriorityAbove");
+                packet.ReadSingle("ThrottleLfgListPriorityBelow");
+                packet.ReadSingle("ThrottleLfgListIlvlScalingAbove");
+                packet.ReadSingle("ThrottleLfgListIlvlScalingBelow");
+                packet.ReadSingle("ThrottleRfPriorityAbove");
+                packet.ReadSingle("ThrottleRfIlvlScalingAbove");
+                packet.ReadSingle("ThrottleDfMaxItemLevel");
+                packet.ReadSingle("ThrottleDfBestPriority");
+            }
+
+            if (hasSessionAlert)
+                V6_0_2_19033.Parsers.MiscellaneousHandler.ReadClientSessionAlertConfig(packet, "SessionAlert");
+
+            packet.ResetBitReader();
+            V8_0_1_27101.Parsers.MiscellaneousHandler.ReadVoiceChatManagerSettings(packet, "VoiceChatManagerSettings");
+
+            if (hasEuropaTicketSystemStatus)
+            {
+                packet.ResetBitReader();
+                V6_0_2_19033.Parsers.MiscellaneousHandler.ReadCliEuropaTicketConfig(packet, "EuropaTicketSystemStatus");
+            }
         }
 
         [Parser(Opcode.SMSG_FEATURE_SYSTEM_STATUS_GLUE_SCREEN)]
@@ -47,7 +127,7 @@ namespace WowPacketParserModule.V9_0_1_36216.Parsers
             packet.ResetBitReader();
 
             if (europaTicket)
-                ReadEuropaTicketSystemStatus(packet, "EuropaTicketSystemStatus");
+                V6_0_2_19033.Parsers.MiscellaneousHandler.ReadCliEuropaTicketConfig(packet, "EuropaTicketSystemStatus");
 
             packet.ReadUInt32("TokenPollTimeSeconds");
             packet.ReadUInt32("KioskSessionMinutes");
