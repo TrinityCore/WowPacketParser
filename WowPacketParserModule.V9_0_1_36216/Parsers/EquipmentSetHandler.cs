a1516452229b0d3048265173d788a36bd9b28af3
funjoker <funjoker109@gmail.com>
2020-12-12 23:37:35 +0100

    Update SMSG_LOAD_EQUIPMENT_SET for shadowlands

---
+++ WowPacketParserModule.V9_0_1_36216/Parsers/EquipmentSetHandler.cs
@@ -0,0 +1,49 @@
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V9_0_1_36216.Parsers
+{
+    public static class EquipmentSetHandler
+    {
+        private const int NumSlots = 19;
+
+        [Parser(Opcode.SMSG_LOAD_EQUIPMENT_SET)]
+        public static void HandleEquipmentSetList720(Packet packet)
+        {
+            var count = packet.ReadInt32("Count");
+
+            for (var i = 0; i < count; i++)
+            {
+                packet.ReadInt32("Type", i);
+                packet.ReadUInt64("Guid", i);
+                packet.ReadUInt32("SetID", i);
+                uint ignoreMask = packet.ReadUInt32("IgnoreMask");
+
+                for (var j = 0; j < NumSlots; j++)
+                {
+                    bool ignore = (ignoreMask & (1 << j)) != 0;
+                    packet.ReadPackedGuid128("Pieces" + (ignore ? " (Ignored)" : ""), i, j);
+                    packet.ReadInt32("Appearances", i);
+                }
+
+                for (var j = 0; j < 2; j++)
+                    packet.ReadInt32("Enchants", i, j);
+
+                packet.ReadInt32("Unknown901_1");
+                packet.ReadInt32("Unknown901_2");
+
+                packet.ResetBitReader();
+                var hasAssignedSpecIndex = packet.ReadBit("HasAssignedSpecIndex");
+                var setNameLen = packet.ReadBits(8);
+                var setIconLen = packet.ReadBits(9);
+
+                if (hasAssignedSpecIndex)
+                    packet.ReadInt32("AssignedSpecIndex", i);
+
+                packet.ReadWoWString("SetName", setNameLen, i);
+                packet.ReadWoWString("SetIcon", setIconLen, i);
+            }
+        }
+    }
+}
