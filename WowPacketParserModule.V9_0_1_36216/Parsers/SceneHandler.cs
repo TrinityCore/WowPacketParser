dd40b64aeda1b300af83292176af634580e70030
funjoker <funjoker109@gmail.com>
2020-12-14 00:55:29 +0100

    Update SMSG_PLAY_SCENE

---
+++ WowPacketParserModule.V9_0_1_36216/Parsers/SceneHandler.cs
@@ -0,0 +1,33 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V9_0_1_36216.Parsers
+{
+    public static class SceneHandler
+    {
+        [Parser(Opcode.SMSG_PLAY_SCENE)]
+        public static void HandlePlayScene(Packet packet)
+        {
+            var sceneId = packet.ReadInt32("SceneID");
+            SceneTemplate scene = new SceneTemplate
+            {
+                SceneID = (uint)sceneId
+            };
+
+            scene.Flags = (uint)packet.ReadInt32("PlaybackFlags");
+            packet.ReadInt32("SceneInstanceID");
+            scene.ScriptPackageID = (uint)packet.ReadInt32("SceneScriptPackageID");
+            packet.ReadPackedGuid128("TransportGUID");
+            packet.ReadVector3("Pos");
+            packet.ReadSingle("Facing");
+            // do we need to add that to template too?
+            packet.ReadBit("PerformTactCallbacks");
+
+            if (sceneId != 0) // SPELL_EFFECT_195 plays scenes by SceneScriptPackageID and sets SceneID = 0 (there are no Scenes which have SceneID = 0)
+                Storage.Scenes.Add(scene, packet.TimeSpan);
+        }
+    }
+}
