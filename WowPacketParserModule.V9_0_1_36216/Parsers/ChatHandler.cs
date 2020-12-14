d1ceaba1d2a5efe1fcadc63f08d0e7ef7d3f4286
funjoker <funjoker109@gmail.com>
2020-12-14 21:34:11 +0100

    Update SMSG_CHAT

---
+++ WowPacketParserModule.V9_0_1_36216/Parsers/ChatHandler.cs
@@ -0,0 +1,60 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V9_0_1_36216.Parsers
+{
+    public static class ChatHandler
+    {
+        [Parser(Opcode.SMSG_CHAT)]
+        public static void HandleServerChatMessage(Packet packet)
+        {
+            var text = new CreatureText
+            {
+                Type = (ChatMessageType)packet.ReadByteE<ChatMessageTypeNew>("SlashCmd"),
+                Language = packet.ReadUInt32E<Language>("Language"),
+                SenderGUID = packet.ReadPackedGuid128("SenderGUID")
+            };
+
+            packet.ReadPackedGuid128("SenderGuildGUID");
+            packet.ReadPackedGuid128("WowAccountGUID");
+            text.ReceiverGUID = packet.ReadPackedGuid128("TargetGUID");
+            packet.ReadUInt32("TargetVirtualAddress");
+            packet.ReadUInt32("SenderVirtualAddress");
+            packet.ReadPackedGuid128("PartyGUID");
+            packet.ReadInt32("AchievementID");
+            packet.ReadSingle("DisplayTime");
+
+            var senderNameLen = packet.ReadBits(11);
+            var receiverNameLen = packet.ReadBits(11);
+            var prefixLen = packet.ReadBits(5);
+            var channelLen = packet.ReadBits(7);
+            var textLen = packet.ReadBits(12);
+            packet.ReadBits("ChatFlags", 14);
+
+            packet.ReadBit("HideChatLog");
+            packet.ReadBit("FakeSenderName");
+            bool unk801bit = packet.ReadBit("Unk801_Bit");
+
+            text.SenderName = packet.ReadWoWString("Sender Name", senderNameLen);
+            text.ReceiverName = packet.ReadWoWString("Receiver Name", receiverNameLen);
+            packet.ReadWoWString("Addon Message Prefix", prefixLen);
+            packet.ReadWoWString("Channel Name", channelLen);
+
+            text.Text = packet.ReadWoWString("Text", textLen);
+            if (unk801bit)
+                packet.ReadUInt32("Unk801");
+
+            uint entry = 0;
+            if (text.SenderGUID.GetObjectType() == ObjectType.Unit)
+                entry = text.SenderGUID.GetEntry();
+            else if (text.ReceiverGUID.GetObjectType() == ObjectType.Unit)
+                entry = text.ReceiverGUID.GetEntry();
+
+            if (entry != 0)
+                Storage.CreatureTexts.Add(entry, text, packet.TimeSpan);
+        }
+    }
+}
