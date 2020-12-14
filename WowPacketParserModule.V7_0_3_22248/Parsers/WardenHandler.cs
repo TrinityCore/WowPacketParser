c0058d8b9f3fdd55c96db1d270769a369ab2632c
funjoker <funjoker109@gmail.com>
2020-12-14 02:59:42 +0100

    Plain Warden3 reading

---
+++ WowPacketParserModule.V7_0_3_22248/Parsers/WardenHandler.cs
@@ -0,0 +1,26 @@
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V7_0_3_22248.Parsers
+{
+    public static class WardenHandler
+    {
+        [Parser(Opcode.CMSG_WARDEN3_DATA)]
+        public static void HandleWardenData(Packet packet)
+        {
+            packet.ReadInt32("Unk");
+            var len = packet.ReadInt32();
+
+            packet.ReadBytes("WardenData", len);
+        }
+
+        [Parser(Opcode.SMSG_WARDEN3_DATA)]
+        public static void HandleWarden3Data(Packet packet)
+        {
+            var len = packet.ReadInt32();
+
+            packet.ReadBytes("WardenData", len);
+        }
+    }
+}
