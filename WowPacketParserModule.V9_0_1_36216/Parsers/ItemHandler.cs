70b44910729e91dc5d1f1a0ba2faf02fbed7f71e
funjoker <funjoker109@gmail.com>
2020-12-14 01:00:15 +0100

    Update CMSG_USE_ITEM

--- WowPacketParserModule.V9_0_1_36216/Parsers/ItemHandler.cs
+++ WowPacketParserModule.V9_0_1_36216/Parsers/ItemHandler.cs
@@ -6,6 +6,16 @@ namespace WowPacketParserModule.V9_0_1_36216.Parsers
 {
     public static class ItemHandler
     {
+        [Parser(Opcode.CMSG_USE_ITEM)]
+        public static void HandleUseItem(Packet packet)
+        {
+            packet.ReadByte("PackSlot");
+            packet.ReadByte("Slot");
+            packet.ReadPackedGuid128("CastItem");
+
+            SpellHandler.ReadSpellCastRequest(packet, "Cast");
+        }
+
         [Parser(Opcode.CMSG_USE_TOY)]
         public static void HandleUseToy(Packet packet)
         {
