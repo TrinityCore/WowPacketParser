bd0ad73bb285000d947bc124d31b20911fcc4723
funjoker <funjoker109@gmail.com>
2020-12-14 02:24:18 +0100

    Add more VAS packets to non parsing

--- WowPacketParserModule.V7_0_3_22248/Parsers/BattlePayHandler.cs
+++ WowPacketParserModule.V7_0_3_22248/Parsers/BattlePayHandler.cs
@@ -23,8 +23,23 @@ namespace WowPacketParserModule.V7_0_3_22248.Parsers
         [Parser(Opcode.SMSG_BATTLE_PAY_START_DISTRIBUTION_ASSIGN_TO_TARGET_RESPONSE)]
         [Parser(Opcode.SMSG_BATTLE_PAY_START_PURCHASE_RESPONSE)]
         [Parser(Opcode.SMSG_BATTLE_PAY_VALIDATE_PURCHASE_RESPONSE)]
+        [Parser(Opcode.CMSG_BATTLE_PAY_START_VAS_PURCHASE)]
+        [Parser(Opcode.CMSG_GET_VAS_ACCOUNT_CHARACTER_LIST)]
+        [Parser(Opcode.CMSG_GET_VAS_TRANSFER_TARGET_REALM_LIST)]
+        [Parser(Opcode.CMSG_UPDATE_VAS_PURCHASE_STATES)]
+        [Parser(Opcode.CMSG_VAS_CHECK_TRANSFER_OK)]
+        [Parser(Opcode.CMSG_VAS_GET_QUEUE_MINUTES)]
+        [Parser(Opcode.CMSG_VAS_GET_SERVICE_STATUS)]
         [Parser(Opcode.SMSG_BATTLE_PAY_VAS_GUILD_FOLLOW_INFO)]
         [Parser(Opcode.SMSG_BATTLE_PAY_VAS_GUILD_MASTER_LIST)]
+        [Parser(Opcode.SMSG_ENUM_VAS_PURCHASE_STATES_RESPONSE)]
+        [Parser(Opcode.SMSG_GET_VAS_ACCOUNT_CHARACTER_LIST_RESULT)]
+        [Parser(Opcode.SMSG_GET_VAS_TRANSFER_TARGET_REALM_LIST_RESULT)]
+        [Parser(Opcode.SMSG_VAS_CHECK_TRANSFER_OK_RESPONSE)]
+        [Parser(Opcode.SMSG_VAS_GET_QUEUE_MINUTES_RESPONSE)]
+        [Parser(Opcode.SMSG_VAS_GET_SERVICE_STATUS_RESPONSE)]
+        [Parser(Opcode.SMSG_VAS_PURCHASE_COMPLETE)]
+        [Parser(Opcode.SMSG_VAS_PURCHASE_STATE_UPDATE)]
         public static void HandleBattlePay(Packet packet)
         {
             packet.ReadToEnd();
