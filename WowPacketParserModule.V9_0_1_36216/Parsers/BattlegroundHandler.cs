515afee5f79bc875b5f092ada0b019f58c20b275
funjoker <funjoker109@gmail.com>
2020-12-14 02:00:22 +0100

    Update SMSG_SEASON_INFO

---
+++ WowPacketParserModule.V9_0_1_36216/Parsers/BattlegroundHandler.cs
@@ -0,0 +1,22 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V9_0_1_36216.Parsers
+{
+    public static class BattlegroundHandler
+    {
+        [Parser(Opcode.SMSG_SEASON_INFO)]
+        public static void HandleSeasonInfo(Packet packet)
+        {
+            packet.ReadInt32("MythicPlusSeasonID");
+            packet.ReadInt32("CurrentSeason");
+            packet.ReadInt32("PreviousSeason");
+            packet.ReadInt32("ConquestWeeklyProgressCurrencyID");
+            packet.ReadInt32("PvpSeasonID");
+            packet.ReadBit("WeeklyRewardChestsEnabled");
+        }
+    }
+}
