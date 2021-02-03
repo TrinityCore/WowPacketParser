09546b51e51e3e3ff89e763b1014874d76c1f6d0
Shauren <shauren.trinity@gmail.com>
2021-02-03 23:43:39 +0100

    Remove Tamir.SharpSsh

--- WowPacketParser/SQL/SSHTunnel.cs
+++ WowPacketParser/SQL/SSHTunnel.cs
@@ -1,54 +1,36 @@
-﻿using System;
+﻿using Renci.SshNet;
+using System;
 using System.Diagnostics;
-using Tamir.SharpSsh.jsch;
 using WowPacketParser.Misc;
 
 namespace WowPacketParser.SQL
 {
-    public class MyUserInfo : UserInfo
-    {
-        private readonly string _passwd;
-
-        public MyUserInfo(string password)
-        {
-            _passwd = password;
-        }
-
-        public string getPassword() { return _passwd; }
-        public bool promptYesNo(string str) { return true; }
-        public string getPassphrase() { return null; }
-        public bool promptPassphrase(string message) { return true; }
-        public bool promptPassword(string message) { return true; }
-        public void showMessage(string message) { }
-    }
-
     public static class SSHTunnel
     {
         [ThreadStatic]
-        private static Session _session;
+        private static SshClient _session;
         public static bool Enabled = Settings.SSHEnabled;
 
         public static void Connect()
         {
             try
-             {
-                 JSch jsch = new JSch();
+            {
+                _session = new SshClient(Settings.SSHHost, Settings.SSHPort, Settings.SSHUsername, Settings.SSHPassword);
+                _session.Connect();
+                uint port;
+                if (!uint.TryParse(Settings.Port, out port))
+                    port = 3306;
 
-                 _session = jsch.getSession(Settings.SSHUsername, Settings.SSHHost, Settings.SSHPort);
-                 _session.setHost(Settings.SSHHost);
-                 _session.setPassword(Settings.SSHPassword);
-                 UserInfo ui = new MyUserInfo(Settings.SSHPassword);
-                 _session.setUserInfo(ui);
-                 _session.connect();
-                 int port;
-                 if (!int.TryParse(Settings.Port, out port))
-                     port = 3306;
-                 _session.setPortForwardingL(Settings.SSHLocalPort, "localhost", port);
-                 if (!_session.isConnected())
-                    Enabled = false;
-             }
-             catch (Exception ex)
-             {
+                var portForward = new ForwardedPortLocal((uint)Settings.SSHLocalPort, "127.0.0.1", port);
+
+                _session.AddForwardedPort(portForward);
+
+                portForward.Start();
+
+                Enabled = _session.IsConnected;
+            }
+            catch (Exception ex)
+            {
                 Enabled = false;
                 Trace.WriteLine(ex.Message + " at ssh connect.");
                 Disconnect();
@@ -57,12 +39,17 @@ namespace WowPacketParser.SQL
 
         public static bool Connected()
         {
-            return _session != null && _session.isConnected();
+            return _session != null && _session.IsConnected;
         }
 
         public static void Disconnect()
         {
-            _session?.disconnect();
+            if (_session != null)
+            {
+                _session.Disconnect();
+                _session.Dispose();
+                _session = null;
+            }
         }
     }
 }
