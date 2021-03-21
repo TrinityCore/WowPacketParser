a054028bf5f86ca4853bf73b9bcb6598f3f06d00
Fabian <fabian@arctium.io>
2021-03-21 15:51:37 +0100

    Fixed ActivePlayer Update in 9.0.1 and 9.0.2

--- WowPacketParserModule.V9_0_1_36216/Parsers/UpdateFieldsHandler901.cs
+++ WowPacketParserModule.V9_0_1_36216/Parsers/UpdateFieldsHandler901.cs
@@ -2896,6 +2896,7 @@ namespace WowPacketParserModule.V9_0_1_36216.UpdateFields.V9_0_1_36216
                     }
                 }
             }
+            packet.ResetBitReader();
             if (changesMask[0])
             {
                 if (changesMask[22])
--- WowPacketParserModule.V9_0_1_36216/Parsers/UpdateFieldsHandler902.cs
+++ WowPacketParserModule.V9_0_1_36216/Parsers/UpdateFieldsHandler902.cs
@@ -2898,6 +2898,7 @@ namespace WowPacketParserModule.V9_0_1_36216.UpdateFields.V9_0_2_36639
                     }
                 }
             }
+            packet.ResetBitReader();
             if (changesMask[0])
             {
                 if (changesMask[22])
