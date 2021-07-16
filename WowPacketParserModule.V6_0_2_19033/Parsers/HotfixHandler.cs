ebec41e98b13a26e616c73f78d91c3e501bd4896
Bartosz Korczyński <b.andysc@gmail.com>
2021-07-16 21:19:24 +0100

    SMSG_DB_REPLY to protobuf parsing

--- WowPacketParserModule.V6_0_2_19033/Parsers/HotfixHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/HotfixHandler.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Google.Protobuf.WellKnownTypes;
 using WowPacketParser.Enums;
 using WowPacketParser.Hotfix;
 using WowPacketParser.Loading;
 using WowPacketParser.Misc;
 using WowPacketParser.Parsing;
+using WoWPacketParser.Proto;
 using WowPacketParser.Store;
 using WowPacketParser.Store.Objects;
 
@@ -31,11 +33,14 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
         [Parser(Opcode.SMSG_DB_REPLY)]
         public static void HandleDBReply(Packet packet)
         {
+            var dbReply = packet.Holder.DbReply = new();
             var type = packet.ReadUInt32E<DB2Hash>("TableHash");
-            var entry = packet.ReadInt32("RecordID");
+            dbReply.TableHash = (uint)type;
+            var entry = dbReply.RecordId = packet.ReadInt32("RecordID");
             var allow = true;
             var timeStamp = packet.ReadUInt32();
-            packet.AddValue("Timestamp", Utilities.GetDateTimeFromUnixTime(timeStamp));
+            var time = packet.AddValue("Timestamp", Utilities.GetDateTimeFromUnixTime(timeStamp));
+            dbReply.Time = Timestamp.FromDateTime(DateTime.SpecifyKind(time, DateTimeKind.Utc));
             if (ClientVersion.AddedInVersion(ClientVersionBuild.V6_2_0_20173))
                 allow = packet.ReadBit("Allow");
 
@@ -44,11 +49,13 @@ namespace WowPacketParserModule.V6_0_2_19033.Parsers
             var db2File = new Packet(data, packet.Opcode, packet.Time, packet.Direction, packet.Number, packet.Writer, packet.FileName);
             if (entry < 0 || !allow)
             {
+                dbReply.Status = PacketDbReplyRecordStatus.RecordStatusRecordRemoved;
                 packet.WriteLine("Row {0} has been removed.", -entry);
                 HotfixStoreMgr.RemoveRecord(type, entry);
             }
             else
             {
+                dbReply.Status = PacketDbReplyRecordStatus.RecordStatusValid;
                 packet.AddSniffData(StoreNameType.None, entry, type.ToString());
                 HotfixStoreMgr.AddRecord(type, entry, db2File);
                 db2File.ClosePacket(false);
