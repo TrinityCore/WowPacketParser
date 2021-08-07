82382b0f381d0644c5ec6f7ba2c010600acc8b88
funjoker <funjoker109@gmail.com>
2021-08-07 12:40:34 +0200

    Update some field names in hotfix structures

--- WowPacketParserModule.V9_0_1_36216/Hotfix/ContentTuningXExpectedEntry.cs
+++ WowPacketParserModule.V9_0_1_36216/Hotfix/ContentTuningXExpectedEntry.cs
@@ -7,9 +7,12 @@ namespace WowPacketParserModule.V9_0_1_36216.Hotfix
     public class ContentTuningXExpectedEntry
     {
         public int ExpectedStatModID { get; set; }
+        [HotfixVersion(ClientVersionBuild.V9_1_0_39185, true)]
         public int MythicPlusSeasonID { get; set; }
         [HotfixVersion(ClientVersionBuild.V9_1_0_39185, false)]
-        public int MythicPlusSeasonID2 { get; set; }
+        public int MinMythicPlusSeasonID { get; set; }
+        [HotfixVersion(ClientVersionBuild.V9_1_0_39185, false)]
+        public int MaxMythicPlusSeasonID { get; set; }
         public uint ContentTuningID { get; set; }
     }
 }
