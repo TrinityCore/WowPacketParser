ea6cfffed26120182169994170ab385f50145fcb
François B <445742+Selenium@users.noreply.github.com>
2021-10-01 10:50:54 +0200

    Add CastFlagsEx Enum

--- WowPacketParser/Enums/CastFlag.cs
+++ WowPacketParser/Enums/CastFlag.cs
@@ -39,4 +39,28 @@ namespace WowPacketParser.Enums
         HealPrediction = 0x40000000, // 4.x
         Unknown27      = 0x80000000
     }
+
+    [Flags]
+    public enum CastFlagsEx : uint
+    {
+        None         = 0x00000000,
+        Unknown1     = 0x00000001,
+        Unknown2     = 0x00000002,
+        Unknown3     = 0x00000004,
+        Unknown4     = 0x00000008,
+        Unknown5     = 0x00000010,
+        Unknown6     = 0x00000020,
+        Unknown7     = 0x00000040,
+        Unknown8     = 0x00000080,
+        Unknown9     = 0x00000100,
+        Unknown10    = 0x00000200,
+        Unknown11    = 0x00000400,
+        Unknown12    = 0x00000800,
+        Unknown13    = 0x00001000,
+        Unknown14    = 0x00002000,
+        Unknown15    = 0x00004000,
+        ToyCooldown  = 0x00008000,
+        Unknown17    = 0x00010000,
+        Unknown18    = 0x00020000
+    };
 }
