5eccd85a983f8b9c82b95e3a67f6dcd18e513fd1
ModoX <moardox@gmail.com>
2021-10-03 16:36:13 +0200

    Fixed Map.db2 structure for 9.1.0

--- WowPacketParser/DBC/Structures/Shadowlands/MapEntry.cs
+++ WowPacketParser/DBC/Structures/Shadowlands/MapEntry.cs
@@ -9,7 +9,6 @@ namespace WowPacketParser.DBC.Structures.Shadowlands
         public uint ID;
         public string Directory;
         public string MapName;
-        public string InternalName;
         public string MapDescription0;
         public string MapDescription1;
         public string PvpShortDescription;
