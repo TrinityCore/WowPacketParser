ab154b0387d5c91a1ccd946a200751003f687f6d
MistiX <47088016+Mistixx@users.noreply.github.com>
2022-01-20 00:25:36 -0800

    * Add structures for SMSG_CHROMIE_TIME_OPEN_NPC, CMSG_CHROMIE_TIME (#651)
    * * Add structures for
    SMSG_CHROMIE_TIME_OPEN_NPC
    CMSG_CHROMIE_TIME
    
    * transferred to NpcHanlder Chromie structure
    
    * tabs

--- WowPacketParserModule.V9_0_1_36216/Parsers/NpcHandler.cs
+++ WowPacketParserModule.V9_0_1_36216/Parsers/NpcHandler.cs
@@ -85,5 +85,18 @@ namespace WowPacketParserModule.V9_0_1_36216.Parsers
                 }
             }
         }
+		
+        [Parser(Opcode.SMSG_CHROMIE_TIME_OPEN_NPC)]
+        public static void HandleChromieTimeOpenNpc(Packet packet)
+        {
+            packet.ReadPackedGuid128("GUID");
+        }
+
+        [Parser(Opcode.CMSG_CHROMIE_TIME_SELECT_EXPANSION)]
+        public static void HandleChromieTimeSelectExpansion(Packet packet)
+        {
+            packet.ReadPackedGuid128("GUID");
+            packet.ReadUInt32("Expansion");
+        }
     }
 }
