cd363f05c5e6e35e20d900723e9b1ae5fd7e1913
ModoX <moardox@gmail.com>
2022-01-23 04:57:31 +0100

    Added CovenantSanctumFollower to GarrisonFollowerType enum

--- WowPacketParser/Enums/GarrisonFollowerType.cs
+++ WowPacketParser/Enums/GarrisonFollowerType.cs
@@ -2,10 +2,11 @@
 {
     public enum GarrisonFollowerType : int
     {
-        None                   = 0,
-        DraenorFollower        = 1,
-        DraenorNaval           = 2,
-        ClassOrderHallFollower = 4,
-        BfA                    = 22
+        None                    = 0,
+        DraenorFollower         = 1,
+        DraenorNaval            = 2,
+        ClassOrderHallFollower  = 4,
+        BfA                     = 22,
+        CovenantSanctumFollower = 123,
     }
 }
