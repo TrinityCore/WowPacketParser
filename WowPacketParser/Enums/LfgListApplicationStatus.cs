574939dea19bd154ff7019e4d9b6632a8baf5fd9
ModoX <moardox@gmail.com>
2022-01-23 05:44:40 +0100

    Sync LfgListApplicationStatus names with client

--- WowPacketParser/Enums/LfgListApplicationStatus.cs
+++ WowPacketParser/Enums/LfgListApplicationStatus.cs
@@ -2,16 +2,16 @@
 {
     public enum LfgListApplicationStatus
     {
-        None            = 0,
-        Applied         = 1,
-        Invited         = 2,
-        Failed          = 3,
-        Cancelled       = 4,
-        Declined        = 5,
-        GroupFull       = 6,
-        Delisted        = 7,
-        TimeOut         = 8,
-        InviteDeclined  = 9,
-        InviteAccepted  = 10
+        None                = 0,
+        Applied             = 1,
+        Invited             = 2,
+        Failed              = 3,
+        Cancelled           = 4,
+        Declined            = 5,
+        DeclinedFull        = 6,
+        DeclinedDelisted    = 7,
+        TimedOut            = 8,
+        InviteDeclined      = 9,
+        InviteAccepted      = 10
     }
 }
