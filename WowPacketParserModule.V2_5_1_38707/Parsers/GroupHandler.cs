79b859c93f396d4095283a09e49b49ea323c08b7
ModoX <moardox@gmail.com>
2022-02-11 17:36:40 +0100

    Fixed CMSG_PARTY_INVITE structure for Classic TBC 2.5.2 and 2.5.3

---
+++ WowPacketParserModule.V2_5_1_38707/Parsers/GroupHandler.cs
@@ -0,0 +1,26 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+
+namespace WowPacketParserModule.V2_5_1_38707.Parsers
+{
+    public static class GroupHandler
+    {
+        [Parser(Opcode.CMSG_PARTY_INVITE, ClientVersionBuild.V2_5_2_39570)]
+        public static void HandleClientPartyInvite(Packet packet)
+        {
+            packet.ReadByte("PartyIndex");
+            packet.ResetBitReader();
+
+            var lenTargetName = packet.ReadBits(9);
+            var lenTargetRealm = packet.ReadBits(9);
+
+            packet.ReadInt32("ProposedRoles");
+            packet.ReadPackedGuid128("TargetGuid");
+
+            packet.ReadWoWString("TargetName", lenTargetName);
+            packet.ReadWoWString("TargetRealm", lenTargetRealm);
+        }
+    }
+}
