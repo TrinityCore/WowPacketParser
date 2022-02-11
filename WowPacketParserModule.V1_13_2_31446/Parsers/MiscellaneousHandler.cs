923a2688ed8915948ed6ccf15b51d39078aa3c4d
ModoX <moardox@gmail.com>
2022-02-11 13:43:06 +0100

    Buildfix *hide*

--- WowPacketParserModule.V1_13_2_31446/Parsers/MiscellaneousHandler.cs
+++ WowPacketParserModule.V1_13_2_31446/Parsers/MiscellaneousHandler.cs
@@ -1,6 +1,6 @@
 ﻿using WowPacketParser.Enums;
 using WowPacketParser.Misc;
-using WowPacketParser.Parsing
+using WowPacketParser.Parsing;
 using CoreParsers = WowPacketParser.Parsing.Parsers;
 
 namespace WowPacketParserModule.V1_13_2_31446.Parsers
