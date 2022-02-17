7113d16965b575ffff647cc4cb5e73a2365fafe0
ModoX <moardox@gmail.com>
2022-02-17 23:30:59 +0100

    Fixed structure of CMSG_CHAT_JOIN_CHANNEL for 2.5.1

---
+++ WowPacketParserModule.V2_5_1_38707/Parsers/ChannelHandler.cs
@@ -0,0 +1,21 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V2_5_1_38707.Parsers
+{
+    public static class ChannelHandler
+    {
+        [Parser(Opcode.CMSG_CHAT_JOIN_CHANNEL)]
+        public static void HandleChannelJoin(Packet packet)
+        {
+            packet.ReadInt32("ChatChannelId");
+
+            var channelLength = packet.ReadBits(7);
+            var passwordLength = packet.ReadBits(7);
+
+            packet.ReadWoWString("ChannelName", channelLength);
+            packet.ReadWoWString("Password", passwordLength);
+        }
+    }
+}
