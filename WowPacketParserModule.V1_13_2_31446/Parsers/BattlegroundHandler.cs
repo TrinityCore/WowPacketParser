7018957c1b4f57b0d98f0ce86bcf4a6ba03084d6
ModoX <moardox@gmail.com>
2022-03-03 19:50:32 +0100

    Fixed structure of SMSG_BATTLEFIELD_LIST and CMSG_BATTLEMASTER_JOIN in 1.13.2 - 2.5.3

---
+++ WowPacketParserModule.V1_13_2_31446/Parsers/BattlegroundHandler.cs
@@ -0,0 +1,41 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V1_13_2_31446.Parsers
+{
+    public static class BattlegroundHandler
+    {
+        [Parser(Opcode.SMSG_BATTLEFIELD_LIST)]
+        public static void HandleBattlefieldList(Packet packet)
+        {
+            packet.ReadPackedGuid128("BattlemasterGuid");
+            packet.ReadInt32("UnkID"); // some kind of serverside verification id its only being used in CMSG_BATTLEMASTER_JOIN again
+            packet.ReadInt32("BattlemasterListID");
+            packet.ReadByte("MinLevel");
+            packet.ReadByte("MaxLevel");
+            var battlefieldsCount = packet.ReadUInt32("BattlefieldsCount");
+            for (var i = 0; i < battlefieldsCount; ++i) 
+                packet.ReadInt32("Battlefield", i);
+
+            packet.ResetBitReader();
+            packet.ReadBit("PvpAnywhere");
+            packet.ReadBit("HasRandomWinToday");
+        }
+
+        [Parser(Opcode.CMSG_BATTLEMASTER_JOIN)]
+        public static void HandleBattlemasterJoin(Packet packet)
+        {
+            packet.ReadInt64("QueueID");
+            packet.ReadByte("Roles");
+
+            for (int i = 0; i < 2; i++)
+                packet.ReadInt32("BlacklistMap", i);
+
+            packet.ReadPackedGuid128("BattlemasterGuid");
+            packet.ReadInt32("UnkID"); // reused from SMSG_BATTLEFIELD_LIST
+            packet.ReadInt32("BattlefieldIndexSpecific");
+            packet.ReadBit("JoinAsGroup");
+        }
+    }
+}
