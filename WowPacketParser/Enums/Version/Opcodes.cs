8499548e575c7fb097c6909a63bcc1d16c8e98ad
killerwife <killerwife@gmail.com>
2022-07-29 02:54:07 +0200

    Add V2_5_4_44833 (#688)

--- WowPacketParser/Enums/Version/Opcodes.cs
+++ WowPacketParser/Enums/Version/Opcodes.cs
@@ -668,6 +668,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V2_5_4_44036:
                 case ClientVersionBuild.V2_5_4_44171:
                 case ClientVersionBuild.V2_5_4_44400:
+                case ClientVersionBuild.V2_5_4_44833:
                     return ClientVersionBuild.V2_5_4_42695;
                 default:
                     return ClientVersionBuild.V3_3_5a_12340;
