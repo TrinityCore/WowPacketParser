79ab792ee22e4c29eaf497a6b8be3d9c53ec51f7
funjoker <funjoker109@gmail.com>
2022-09-02 14:11:42 +0200

    Fix SMSG_FEATURE_SYSTEM_STATUS_GLUE_SCREEN for 3.4.x

---
+++ WowPacketParserModule.V3_4_0_45166/Parsers/MiscellaneousHandler.cs
@@ -0,0 +1,78 @@
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Proto;
+using WowPacketParser.Store;
+using CoreParsers = WowPacketParser.Parsing.Parsers;
+
+namespace WowPacketParserModule.V3_4_0_45166.Parsers
+{
+    public static class MiscellaneousHandler
+    {
+        public static void ReadGameRuleValuePair(Packet packet, params object[] indexes)
+        {
+            packet.ReadInt32("Rule", indexes);
+            packet.ReadInt32("Value", indexes);
+        }
+        [Parser(Opcode.SMSG_FEATURE_SYSTEM_STATUS_GLUE_SCREEN)]
+        public static void HandleFeatureSystemStatusGlueScreen(Packet packet)
+        {
+            packet.ReadBit("BpayStoreEnabled");
+            packet.ReadBit("BpayStoreAvailable");
+            packet.ReadBit("BpayStoreDisabledByParentalControls");
+            packet.ReadBit("CharUndeleteEnabled");
+            packet.ReadBit("CommerceSystemEnabled");
+            packet.ReadBit("Unk14");
+            packet.ReadBit("WillKickFromWorld");
+            packet.ReadBit("IsExpansionPreorderInStore");
+
+            packet.ReadBit("KioskModeEnabled");
+            packet.ReadBit("IsCompetitiveModeEnabled");
+            packet.ReadBit("TrialBoostEnabled");
+            packet.ReadBit("TokenBalanceEnabled");
+            packet.ReadBit("LiveRegionCharacterListEnabled");
+            packet.ReadBit("LiveRegionCharacterCopyEnabled");
+            packet.ReadBit("LiveRegionAccountCopyEnabled");
+            packet.ReadBit("LiveRegionKeyBindingsCopyEnabled");
+
+            packet.ReadBit("UnusedBit1_340");
+            packet.ReadBit("UnusedBit2_340");
+            var europaTicket = packet.ReadBit("IsEuropaTicketSystemStatusEnabled");
+            packet.ReadBit("Unknown901CheckoutRelated");
+            bool launchETA = packet.ReadBit("IsLaunchETA");
+            packet.ReadBit("TBCInfoPaneEnabled");
+            packet.ReadBit("TBCInfoPanePriceEnabled");
+            packet.ReadBit("TBCTransitionUIEnabled");
+
+            packet.ReadBit("SoMNotificationEnabled");
+            packet.ResetBitReader();
+
+            if (europaTicket)
+                V6_0_2_19033.Parsers.MiscellaneousHandler.ReadCliEuropaTicketConfig(packet, "EuropaTicketSystemStatus");
+
+            packet.ReadUInt32("TokenPollTimeSeconds");
+            packet.ReadUInt32("KioskSessionMinutes");
+            packet.ReadInt64("TokenBalanceAmount");
+            packet.ReadInt32("MaxCharactersPerRealm");
+            var liveRegionCharacterCopySourceRegionsCount = packet.ReadUInt32("LiveRegionCharacterCopySourceRegionsCount");
+            packet.ReadUInt32("BpayStoreProductDeliveryDelay");
+            packet.ReadInt32("ActiveCharacterUpgradeBoostType");
+            packet.ReadInt32("ActiveClassTrialBoostType");
+            packet.ReadInt32("MinimumExpansionLevel");
+            packet.ReadInt32("MaximumExpansionLevel");
+            packet.ReadInt32("GameRuleUnknown1");
+            var gameRuleValuesCount = packet.ReadUInt32("GameRuleValuesCount");
+            packet.ReadInt16("MaxPlayerNameQueriesPerPacket");
+            packet.ReadInt16("PlayerNameQueryTelemetryInterval");
+
+            if (launchETA)
+                packet.ReadPackedTime("LaunchETA");
+
+            for (int i = 0; i < liveRegionCharacterCopySourceRegionsCount; i++)
+                packet.ReadUInt32("LiveRegionCharacterCopySourceRegion", i);
+
+            for (var i = 0; i < gameRuleValuesCount; ++i)
+                ReadGameRuleValuePair(packet, "GameRuleValues");
+        }
+    }
+}
