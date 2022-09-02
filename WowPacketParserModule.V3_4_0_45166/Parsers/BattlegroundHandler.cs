6c361c0657b822cb57ee9a7174b6b457825a0f09
funjoker <funjoker109@gmail.com>
2022-09-02 22:00:56 +0200

    Fix SMSG_RATED_PVP_INFO for 3.4.x

---
+++ WowPacketParserModule.V3_4_0_45166/Parsers/BattlegroundHandler.cs
@@ -0,0 +1,37 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V3_4_0_45166.Parsers
+{
+    public static class BattlegroundHandler
+    {
+        public static void ReadRatedPvpBracketInfo(Packet packet, params object[] idx)
+        {
+            packet.ReadInt32("PersonalRating", idx);
+            packet.ReadInt32("Ranking", idx);
+            packet.ReadInt32("SeasonPlayed", idx);
+            packet.ReadInt32("SeasonWon", idx);
+            packet.ReadInt32("WeeklyPlayed", idx);
+            packet.ReadInt32("WeeklyWon", idx);
+            packet.ReadInt32("WeeklyBest", idx);
+            packet.ReadInt32("SeasonBest", idx);
+            packet.ReadInt32("LastWeeksBestRating", idx);
+            packet.ReadInt32("PvpTierID", idx);
+            packet.ReadInt32("Unused1", idx);
+            packet.ReadInt32("Unused2", idx);
+            packet.ReadInt32("Unused3", idx);
+            packet.ReadInt32("Unused4", idx);
+            packet.ReadInt32("Unused5", idx);
+            packet.ResetBitReader();
+            packet.ReadBit("Disqualified", idx);
+        }
+
+        [Parser(Opcode.SMSG_RATED_PVP_INFO)]
+        public static void HandleRatedBattlefieldInfo(Packet packet)
+        {
+            for (int i = 0; i < 6; i++)
+                ReadRatedPvpBracketInfo(packet, i);
+        }
+    }
+}
