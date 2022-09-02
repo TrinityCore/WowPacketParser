af84b92635a38c956ff1266c76a4b819812b52f0
funjoker <funjoker109@gmail.com>
2022-09-02 12:02:44 +0200

    Fix SMSG_SET_TIME_ZONE_INFORMATION for 3.4.x

---
+++ WowPacketParserModule.V3_4_0_45166/Parsers/SessionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using CoreParsers = WowPacketParser.Parsing.Parsers;
+
+namespace WowPacketParserModule.V3_4_0_45166.Parsers
+{
+    public static class SessionHandler
+    {
+        [Parser(Opcode.SMSG_SET_TIME_ZONE_INFORMATION)]
+        public static void HandleSetTimeZoneInformation(Packet packet)
+        {
+            var len1 = packet.ReadBits(7);
+            var len2 = packet.ReadBits(7);
+            var len3 = packet.ReadBits(7);
+
+            packet.ReadWoWString("ServerTimeTZ", len1);
+            packet.ReadWoWString("GameTimeTZ", len2);
+            packet.ReadWoWString("ServerRegionalTZ", len3);
+        }
+    }
+}
