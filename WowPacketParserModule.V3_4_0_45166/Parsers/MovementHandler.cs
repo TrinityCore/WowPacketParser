45ad10fcaa78abbd3f1f04765b187be103e6c5a2
funjoker <funjoker109@gmail.com>
2022-09-02 22:00:41 +0200

    Fix SMSG_LOGIN_SET_TIME_SPEED for 3.4.x

---
+++ WowPacketParserModule.V3_4_0_45166/Parsers/MovementHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Google.Protobuf.WellKnownTypes;
+using WowPacketParser.DBC;
+using WowPacketParser.Enums;
+using WowPacketParser.Enums.Version;
+using WowPacketParser.Misc;
+using WowPacketParser.PacketStructures;
+using WowPacketParser.Parsing;
+using WowPacketParser.Proto;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+using CoreParsers = WowPacketParser.Parsing.Parsers;
+using MovementFlag = WowPacketParser.Enums.v4.MovementFlag;
+using MovementFlag2 = WowPacketParser.Enums.v4.MovementFlag2;
+using SplineFacingType = WowPacketParserModule.V6_0_2_19033.Enums.SplineFacingType;
+
+namespace WowPacketParserModule.V3_4_0_45166.Parsers
+{
+    public static class MovementHandler
+    {
+        [Parser(Opcode.SMSG_LOGIN_SET_TIME_SPEED)]
+        public static void HandleLoginSetTimeSpeed(Packet packet)
+        {
+            PacketLoginSetTimeSpeed setTime = packet.Holder.LoginSetTimeSpeed = new();
+            setTime.ServerTime = packet.ReadPackedTime("ServerTime").ToUniversalTime().ToTimestamp();
+            setTime.GameTime = packet.ReadPackedTime("GameTime").ToUniversalTime().ToTimestamp();
+            setTime.NewSpeed = packet.ReadSingle("NewSpeed");
+            setTime.ServerTimeHolidayOffset = packet.ReadInt32("ServerTimeHolidayOffset");
+            setTime.GameTimeHolidayOffset = packet.ReadInt32("GameTimeHolidayOffset");
+        }
+    }
+}
