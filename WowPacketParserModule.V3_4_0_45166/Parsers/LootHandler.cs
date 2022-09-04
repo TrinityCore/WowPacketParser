081aa4d28d9459e1d45e53ba8c6ced4a2423a277
funjoker <funjoker109@gmail.com>
2022-09-04 21:58:21 +0200

    Fix SMSG_LOOT_MONEY_NOTIFY for 3.4.x

---
+++ WowPacketParserModule.V3_4_0_45166/Parsers/LootHandler.cs
@@ -0,0 +1,18 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V3_4_0_45166.Parsers
+{
+    public static class LootHandler
+    {
+        [Parser(Opcode.SMSG_LOOT_MONEY_NOTIFY)]
+        public static void HandleLootMoneyNotify(Packet packet)
+        {
+            packet.ReadUInt64("Money");
+            packet.ReadUInt64("Mod");
+            packet.ResetBitReader();
+            packet.ReadBit("SoleLooter");
+        }
+    }
+}
