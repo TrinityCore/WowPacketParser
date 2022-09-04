778b4e7139c3a196dcdb31efae4d85dc1c0e0b6e
funjoker <funjoker109@gmail.com>
2022-09-04 21:53:45 +0200

    Fix SMSG_LEVEL_UP_INFO for 3.4.x

---
+++ WowPacketParserModule.V3_4_0_45166/Parsers/CharacterHandler.cs
@@ -0,0 +1,27 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V3_4_0_45166.Parsers
+{
+    public static class CharacterHandler
+    {
+        [Parser(Opcode.SMSG_LEVEL_UP_INFO)]
+        public static void HandleLevelUpInfo(Packet packet)
+        {
+            packet.ReadInt32("Level");
+            packet.ReadInt32("HealthDelta");
+
+            for (var i = 0; i < 7; i++)
+                packet.ReadInt32("PowerDelta", (PowerType)i);
+
+            for (var i = 0; i < 5; i++)
+                packet.ReadInt32("StatDelta", (StatType)i);
+
+            packet.ReadInt32("NumNewTalents");
+            packet.ReadInt32("NumNewPvpTalentSlots");
+        }
+    }
+}
