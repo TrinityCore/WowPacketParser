19f3f01ad5e6412e3fe9d73c42c96342f4915445
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-09-22 14:36:40 -0400

    Update UpdateFields.cs

--- WowPacketParser/Enums/Version/UpdateFields.cs
+++ WowPacketParser/Enums/Version/UpdateFields.cs
@@ -889,6 +889,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V3_4_0_45435:
                 case ClientVersionBuild.V3_4_0_45572:
                 case ClientVersionBuild.V3_4_0_45613:
+                case ClientVersionBuild.V3_4_0_45704:
                 {
                     return "V3_4_0_45166";
                 }
