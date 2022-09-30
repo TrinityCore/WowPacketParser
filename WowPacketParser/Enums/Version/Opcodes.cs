bd3869ecd1aa5b2eaeda4aca74d3c8a41c4376a4
M'Dic <joshua.lee.betts@gmail.com>
2022-09-30 17:15:30 -0400

    Added build 3.4.0.45854 (#706)

--- WowPacketParser/Enums/Version/Opcodes.cs
+++ WowPacketParser/Enums/Version/Opcodes.cs
@@ -697,6 +697,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V3_4_0_45613:
                 case ClientVersionBuild.V3_4_0_45704:
                 case ClientVersionBuild.V3_4_0_45772:
+                case ClientVersionBuild.V3_4_0_45854:
                     return ClientVersionBuild.V3_4_0_45166;
                 default:
                     return ClientVersionBuild.V3_3_5a_12340;
