954a5c1c394112bd2c4f4f9d577740c49252ee2f
Shauren <shauren.trinity@gmail.com>
2023-01-28 16:31:51 +0100

    10.0.5 updatefields

---
+++ WowPacketParserModule.Substructures/PerksProgramHandler.cs
@@ -0,0 +1,23 @@
+﻿using WowPacketParser.Misc;
+
+namespace WowPacketParserModule.Substructures
+{
+    public static class PerksProgramHandler
+    {
+        public static void ReadPerksVendorItem(Packet packet, params object[] indexes)
+        {
+            packet.ResetBitReader();
+
+            packet.ReadInt32("VendorItemID", indexes);
+            packet.ReadInt32("MountID", indexes);
+            packet.ReadInt32("BattlePetSpeciesID", indexes);
+            packet.ReadInt32("TransmogSetID", indexes);
+            packet.ReadInt32("ItemModifiedAppearanceID", indexes);
+            packet.ReadInt32("Field_14", indexes);
+            packet.ReadInt32("Field_18", indexes);
+            packet.ReadInt32("Price", indexes);
+            packet.ReadTime64("AvailableUntil", indexes);
+            packet.ReadBit("Disabled", indexes);
+        }
+    }
+}
