cdc9156ed5925612c46888c27b25aab483d4083b
MaxtorCoder <warsongkiller.s8@gmail.com>
2023-03-01 06:39:09 -0500

    Added empower spell packets * SMSG_SPELL_EMPOWER_SET_STAGE * SMSG_SPELL_EMPOWER_UPDATE * CMSG_SPELL_EMPOWER_RELEASE * CMSG_SPELL_EMPOWER_RESTART * SMSG_SPELL_EMPOWER_START
    Closes #742

--- WowPacketParserModule.V10_0_0_46181/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V10_0_0_46181/Parsers/SpellHandler.cs
@@ -45,5 +45,67 @@ namespace WowPacketParserModule.V10_0_0_46181.Parsers
             for (var i = 0; i < spellCount; ++i)
                 ReadLearnedSpellInfo(packet, "ClientLearnedSpellData", i);
         }
+
+        [Parser(Opcode.SMSG_SPELL_EMPOWER_SET_STAGE)]
+        public static void HandleEmpowerSetStage(Packet packet)
+        {
+            packet.ReadPackedGuid128("CastGUID");
+            packet.ReadPackedGuid128("CasterGUID");
+            packet.ReadUInt32("StageID");
+        }
+
+        [Parser(Opcode.SMSG_SPELL_EMPOWER_UPDATE)]
+        public static void HandleEmpowerUpdate(Packet packet)
+        {
+            packet.ReadPackedGuid128("CastGUID");
+            packet.ReadPackedGuid128("CasterGUID");
+            packet.ReadInt32("TimeRemaining");
+            var stageCount = packet.ReadUInt32("RemainingStageCount");
+            packet.ResetBitReader();
+            packet.ReadBit("UnkBit");
+
+            for (int i = 0; i < stageCount; i++)
+                packet.ReadUInt32("Duration", "Stage", i);
+        }
+
+        [Parser(Opcode.CMSG_SPELL_EMPOWER_RELEASE)]
+        [Parser(Opcode.CMSG_SPELL_EMPOWER_RESTART)]
+        public static void HandleEmpowerRelease(Packet packet)
+        {
+            packet.ReadUInt32<SpellId>("SpellID");
+        }
+
+        [Parser(Opcode.SMSG_SPELL_EMPOWER_START)]
+        public static void HandleEmpowerStart(Packet packet)
+        {
+            packet.ReadPackedGuid128("CastGUID");
+            packet.ReadPackedGuid128("CasterGUID");
+
+            var targetsCount = packet.ReadUInt32("TargetsCount");
+            packet.ReadInt32<SpellId>("SpellID");
+
+            V9_0_1_36216.Parsers.SpellHandler.ReadSpellCastVisual(packet, "Visual");
+
+            packet.ReadUInt32("Duration");
+            packet.ReadUInt32("FirstStageDuration");
+            packet.ReadUInt32("FinalStageDuration");
+            var stageCount = packet.ReadUInt32("StageCount");
+
+            for (var i = 0; i < targetsCount; ++i)
+                packet.ReadPackedGuid128("Guid", "Targets", i);
+
+            for (var i = 0; i < stageCount; ++i)
+                packet.ReadUInt32("Duration", "Stage", i);
+
+            packet.ResetBitReader();
+            var hasInterruptImmunities = packet.ReadBit("HasInterruptImmunities");
+            var hasHealPrediction = packet.ReadBit("HasHealPrediction");
+
+            if (hasInterruptImmunities)
+                V6_0_2_19033.Parsers.SpellHandler.ReadSpellChannelStartInterruptImmunities(packet, "InterruptImmunities");
+
+            if (hasHealPrediction)
+                V6_0_2_19033.Parsers.SpellHandler.ReadSpellTargetedHealPrediction(packet, "HealPrediction");
+        }
     }
 }
