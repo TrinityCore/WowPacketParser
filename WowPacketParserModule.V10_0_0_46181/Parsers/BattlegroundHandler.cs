cbbaebca04795445ac72dca75091e24ba8078076
ModoX <moardox@gmail.com>
2023-03-21 22:19:14 +0100

    Update BattlegroundHandler.cs

--- WowPacketParserModule.V10_0_0_46181/Parsers/BattlegroundHandler.cs
+++ WowPacketParserModule.V10_0_0_46181/Parsers/BattlegroundHandler.cs
@@ -7,7 +7,7 @@ namespace WowPacketParserModule.V10_0_0_46181.Parsers
     public static class BattlegroundHandler
     {
         [Parser(Opcode.CMSG_BATTLEMASTER_JOIN_RATED_SOLO_SHUFFLE)]
-        public static void HandleBattleMasterJoinRatedShuffle(Packet packet)
+        public static void HandleBattlemasterJoinRatedSoloShuffle(Packet packet)
         {
             packet.ReadByteE<LfgRoleFlag>("RolesMask");
         }
