78c5a10091c46925ffaa5b9e2d02d29ad58d575f
HerokDeV <123419223+HerokDeV@users.noreply.github.com>
2023-03-22 02:50:10 +0530

    Adding structure for CMSG_BATTLEMASTER_JOIN_RATED_SOLO_SHUFFLE (#747)

---
+++ WowPacketParserModule.V10_0_0_46181/Parsers/BattlegroundHandler.cs
@@ -0,0 +1,15 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V10_0_0_46181.Parsers
+{
+    public static class BattlegroundHandler
+    {
+        [Parser(Opcode.CMSG_BATTLEMASTER_JOIN_RATED_SOLO_SHUFFLE)]
+        public static void HandleBattlemasterJoinRatedSoloShuffle(Packet packet)
+        {
+            packet.ReadByteE<LfgRoleFlag>("RolesMask");
+        }
+    }
+}
