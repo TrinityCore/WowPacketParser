0f87ccde4c11c3a8bd07c8a4e351f9295d195e52
HerokDeV <123419223+HerokDeV@users.noreply.github.com>
2023-04-04 15:53:28 +0200

    Added structure for SMSG_PERKS_PROGRAM_ACTIVITY_UPDATE
    Closes #743

---
+++ WowPacketParserModule.V10_0_0_46181/Parsers/PerksProgramHandler.cs
@@ -0,0 +1,17 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V10_0_0_46181.Parsers
+{
+    public static class PerksProgramHandler
+    {
+        [Parser(Opcode.SMSG_PERKS_PROGRAM_ACTIVITY_UPDATE)]
+        public static void HandlePerksProgramAcitivtyUpdate(Packet packet)
+        {
+            var activityCount = packet.ReadUInt32("ActivityCount");
+            for (var i = 0; i < activityCount; i++)
+                packet.ReadInt32("ActivityID", i);
+        }
+    }
+}
