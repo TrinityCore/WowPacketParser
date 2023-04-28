e310ca401bf6224b96f0cc2132cdd9f2c79e8afe
SargeroDeV <neohd251535@gmail.com>
2023-04-27 22:07:20 -0400

    Added structure for CMSG_GET_ACCOUNT_CHARACTER_LIST

---
+++ WowPacketParserModule.V10_0_0_46181/Parsers/AccountDataHandler.cs
@@ -0,0 +1,18 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V10_0_0_46181.Parsers
+{
+    public static class AccountDataHandler
+    {
+        [Parser(Opcode.CMSG_GET_ACCOUNT_CHARACTER_LIST)]
+        public static void HandleGetAccountCharacterList(Packet packet)
+        {
+            packet.ReadUInt32("UnkInt32");
+
+            packet.ResetBitReader();
+            packet.ReadBit("UnkBit");
+        }
+    }
+}
