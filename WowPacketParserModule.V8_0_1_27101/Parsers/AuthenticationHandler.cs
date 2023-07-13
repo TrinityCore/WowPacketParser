478fd95054ca39ecc9dab835ba9d46728f6f26ce
Shauren <shauren.trinity@gmail.com>
2023-07-13 12:38:20 +0200

    SMSG_AUTH_RESPONSE for real this time

--- WowPacketParserModule.V8_0_1_27101/Parsers/AuthenticationHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/AuthenticationHandler.cs
@@ -36,8 +36,6 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
                 packet.ReadUInt32("TimeRested");
                 packet.ReadByte("ActiveExpansionLevel");
                 packet.ReadByte("AccountExpansionLevel");
-                if (ClientVersion.AddedInVersion(ClientVersionBuild.V10_0_2_46479))
-                    packet.ReadByte("MinActiveExpansionLevel");
                 packet.ReadUInt32("TimeSecondsUntilPCKick");
                 var classes = packet.ReadUInt32("AvailableClasses");
                 var templates = packet.ReadUInt32("Templates");
