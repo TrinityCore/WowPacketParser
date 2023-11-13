f3424930c3d5de3dbd0ac686c39b34d9b8f91a19
funjoker <funjoker109@gmail.com>
2023-11-13 11:46:06 +0100

    3.4.3 SMSG_INITIALIZE_FACTIONS

---
+++ WowPacketParserModule.V3_4_0_45166/Parsers/ReputationHandler.cs
@@ -0,0 +1,24 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V3_4_0_45166.Parsers
+{
+    public static class ReputationHandler
+    {
+        public const int FactionCount = 443;
+
+        [Parser(Opcode.SMSG_INITIALIZE_FACTIONS)]
+        public static void HandleInitializeFactions(Packet packet)
+        {
+            for (var i = 0; i < FactionCount; i++)
+            {
+                packet.ReadUInt16E<FactionFlag>("FactionFlags", i);
+                packet.ReadUInt32E<ReputationRank>("FactionStandings", i);
+            }
+
+            for (var i = 0; i < FactionCount; i++)
+                packet.ReadBit("FactionHasBonus", i);
+        }
+    }
+}
