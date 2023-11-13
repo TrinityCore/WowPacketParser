f621924cd17465dd1a9faeff503f7141dda3e5fb
funjoker <funjoker109@gmail.com>
2023-11-13 11:46:19 +0100

    3.4.3 SMSG_ACCOUNT_DATA_TIMES

--- WowPacketParserModule.V3_4_0_45166/Parsers/AccountDataHandler.cs
+++ WowPacketParserModule.V3_4_0_45166/Parsers/AccountDataHandler.cs
@@ -42,5 +42,15 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
                 ReadAccountCharacterList(packet, i);
             }
         }
+
+        [Parser(Opcode.SMSG_ACCOUNT_DATA_TIMES, ClientVersionBuild.V3_4_3_51505)]
+        public static void HandleAccountDataTimes(Packet packet)
+        {
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadTime64("ServerTime");
+
+            for (var i = 0; i < 15; ++i)
+                packet.ReadTime64($"[{(AccountDataType)i}] Time", i);
+        }
     }
 }
