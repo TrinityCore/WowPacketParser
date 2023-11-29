74767f44e9950f59111cdc89addc85f18002a0fb
funjoker <funjoker109@gmail.com>
2023-11-29 14:12:38 +0100

    Add WotlkClassic exception

--- WowPacketParser/SQL/Builders/UnitMisc.cs
+++ WowPacketParser/SQL/Builders/UnitMisc.cs
@@ -696,7 +696,7 @@ namespace WowPacketParser.SQL.Builders
             var levels = GetLevels(units);
             var usesCurrentExpansionLevels = new Dictionary<uint, long>();
             var expansionBaseLevel = 0;
-            if (Settings.TargetedDatabase >= TargetedDatabase.WarlordsOfDraenor && Settings.TargetedDatabase != TargetedDatabase.Dragonflight && Settings.DBEnabled)
+            if (Settings.TargetedDatabase >= TargetedDatabase.WarlordsOfDraenor && Settings.TargetedDatabase != TargetedDatabase.Dragonflight && Settings.TargetedDatabase != TargetedDatabase.WotlkClassic && Settings.DBEnabled)
             {
                 usesCurrentExpansionLevels = SQLDatabase.GetDict<uint, long>($"SELECT entry, 1 FROM {Settings.TDBDatabase}.creature_template WHERE HealthScalingExpansion = -1");
                 switch (Settings.TargetedDatabase)
