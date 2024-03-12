ef23dc795905e43f045ad74e962e6aed535da8a3
MaxtorCoder <warsongkiller.s8@gmail.com>
2024-03-12 16:42:48 +0100

    Add new AccountDataType entries

--- WowPacketParser/Enums/AccountDataType.cs
+++ WowPacketParser/Enums/AccountDataType.cs
@@ -2,18 +2,20 @@ namespace WowPacketParser.Enums
 {
     public enum AccountDataType
     {
-        GlobalConfigCache         = 0,
-        PerCharacterConfigCache   = 1,
-        GlobalBindingsCache       = 2,
-        PerCharacterBindingsCache = 3,
-        GlobalMacrosCache         = 4,
-        PerCharacterMacrosCache   = 5,
-        PerCharacterLayoutCache   = 6,
-        PerCharacterChatCache     = 7,
-        GlobalTTSCache            = 8,
-        PerCharacterTTSCache      = 9,
-        GlobalFlaggedCache        = 10,
-        PerCharacterFlaggedCache  = 11,
-        PerCharacterClickBindingsCache = 12,
+        GlobalConfigCache               = 0,
+        PerCharacterConfigCache         = 1,
+        GlobalBindingsCache             = 2,
+        PerCharacterBindingsCache       = 3,
+        GlobalMacrosCache               = 4,
+        PerCharacterMacrosCache         = 5,
+        PerCharacterLayoutCache         = 6,
+        PerCharacterChatCache           = 7,
+        GlobalTTSCache                  = 8,
+        PerCharacterTTSCache            = 9,
+        GlobalFlaggedCache              = 10,
+        PerCharacterFlaggedCache        = 11,
+        PerCharacterClickBindingsCache  = 12,
+        GlobalEditModeCache             = 13,
+        PerCharacterEditModeCache       = 14,
     }
 }
