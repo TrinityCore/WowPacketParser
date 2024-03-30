360db43cad70bcb19491613a8c41319be202566e
ModoX <moardox@gmail.com>
2024-03-30 16:37:17 +0100

    Implemented SMSG_CHANGE_PLAYER_DIFFICULTY_RESULT

---
+++ WowPacketParser/Enums/DifficultyChangeResult.cs
@@ -0,0 +1,19 @@
+﻿namespace WowPacketParser.Enums
+{
+    public enum DifficultyChangeResult
+    {
+        Cooldown = 0,
+        EventInProgress = 1,
+        EncounterInProgress = 2,
+        PlayerCombat = 3,
+        PlayerBusy = 4,
+        PlayerOnVehicle = 5,
+        LoadingScreenEnable = 6,
+        DifficultyChangeAlreadyInProgress = 7,
+        MapDifficultyConditionNotSatisfied = 8,
+        PlayerAlreadyLockedToDifferentInstance = 9,
+        HeroicInstanceAlreadyRunningOtherParty = 10,
+        DisabledInLFG = 11,
+        Success = 12
+    }
+}
--- WowPacketParserModule.V10_0_0_46181/Parsers/GroupHandler.cs
+++ WowPacketParserModule.V10_0_0_46181/Parsers/GroupHandler.cs
@@ -15,5 +15,35 @@ namespace WowPacketParserModule.V10_0_0_46181.Parsers
             if (hasPartyIndex)
                 packet.ReadByte("PartyIndex");
         }
+
+        [Parser(Opcode.SMSG_CHANGE_PLAYER_DIFFICULTY_RESULT)]
+        public static void HandleChangePlayerDifficultyResult(Packet packet)
+        {
+            var result = packet.ReadBitsE<DifficultyChangeResult>("Result", 4);
+
+            switch (result)
+            {
+                case DifficultyChangeResult.Cooldown:
+                    packet.ReadBit("InCombat");
+                    packet.ReadInt64("Cooldown");
+                    break;
+                case DifficultyChangeResult.LoadingScreenEnable:
+                    packet.ReadBit("Unused");
+                    packet.ReadInt64("NextDifficultyChangeTime");
+                    break;
+                case DifficultyChangeResult.MapDifficultyConditionNotSatisfied:
+                    packet.ReadInt32E<MapDifficulty>("MapDifficultyID");
+                    break;
+                case DifficultyChangeResult.PlayerAlreadyLockedToDifferentInstance:
+                    packet.ReadPackedGuid128("PlayerGUID");
+                    break;
+                case DifficultyChangeResult.Success:
+                    packet.ReadInt32<MapId>("MapID");
+                    packet.ReadInt32<DifficultyId>("DifficultyID");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
