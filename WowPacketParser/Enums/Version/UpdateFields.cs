a9cecd719605adfadfd7f388467819b6f52ec74d
Naddley <NaddleyTC@gmail.com>
2024-04-12 15:07:27 +0200

    Added build 10.2.6.54205

--- WowPacketParser/Enums/Version/UpdateFields.cs
+++ WowPacketParser/Enums/Version/UpdateFields.cs
@@ -858,6 +858,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V10_2_6_53913:
                 case ClientVersionBuild.V10_2_6_53989:
                 case ClientVersionBuild.V10_2_6_54070:
+                case ClientVersionBuild.V10_2_6_54205:
                 {
                     return "V10_2_6_53840";
                 }
