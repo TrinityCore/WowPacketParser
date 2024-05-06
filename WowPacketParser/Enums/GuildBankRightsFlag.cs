5e25371f95e9f96e7c40b8601496de165222c551
RioMcBoo <artem.jkl@gmail.com>
2024-05-06 16:51:19 +0300

    Correction GuildBankRightsFlag enum

--- WowPacketParser/Enums/GuildBankRightsFlag.cs
+++ WowPacketParser/Enums/GuildBankRightsFlag.cs
@@ -7,7 +7,7 @@ namespace WowPacketParser.Enums
     {
         None         = 0x00,
         ViewTab      = 0x01,
-        WithdrawItem = 0x02,
+        PutItem      = 0x02,
         UpdateText   = 0x04,
         All          = -1
     }
