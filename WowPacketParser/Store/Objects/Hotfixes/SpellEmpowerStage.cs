5ff6af3e3a1228d72996468f6950d67aebeee2ec
Meji <alvaro.megias@outlook.com>
2024-05-16 20:31:31 +0200

    Update hotfixes to 10.2.7 (#852)

---
+++ WowPacketParser/Store/Objects/Hotfixes/SpellEmpowerStage.cs
@@ -0,0 +1,25 @@
+using WowPacketParser.Misc;
+using WowPacketParser.SQL;
+
+namespace WowPacketParser.Store.Objects
+{
+    [Hotfix]
+    [DBTableName("spell_empower_stage")]
+    public sealed record SpellEmpowerStageHotfix1000: IDataModel
+    {
+        [DBFieldName("ID", true)]
+        public uint? ID;
+
+        [DBFieldName("Stage")]
+        public int? Stage;
+
+        [DBFieldName("DurationMs")]
+        public int? DurationMs;
+
+        [DBFieldName("SpellEmpowerID")]
+        public uint? SpellEmpowerID;
+
+        [DBFieldName("VerifiedBuild")]
+        public int? VerifiedBuild = ClientVersion.BuildInt;
+    }
+}
