b4b61510bc3ff0053cb5ecafd4eb20dbe8b8ac18
funjoker <funjoker109@gmail.com>
2024-06-02 01:17:51 +0200

    Add 4.4.0 SMSG_FACTION_BONUS_INFO, SMSG_INITIALIZE_FACTIONS

--- WowPacketParser/Enums/FactionFlag.cs
+++ WowPacketParser/Enums/FactionFlag.cs
@@ -14,4 +14,21 @@ namespace WowPacketParser.Enums
         Inactive   = 0x20,
         Rival      = 0x40
     }
+
+    [Flags]
+    public enum ReputationFlags
+    {
+        None                        = 0x0000,
+        Visible                     = 0x0001,                   // makes visible in client (set or can be set at interaction with target of this faction)
+        AtWar                       = 0x0002,                   // enable AtWar-button in client. player controlled (except opposition team always war state), Flag only set on initial creation
+        Hidden                      = 0x0004,                   // hidden faction from reputation pane in client (player can gain reputation, but this update not sent to client)
+        Header                      = 0x0008,                   // Display as header in UI
+        Peaceful                    = 0x0010,
+        Inactive                    = 0x0020,                   // player controlled (CMSG_SET_FACTION_INACTIVE)
+        ShowPropagated              = 0x0040,
+        HeaderShowsBar              = 0x0080,                   // Header has its own reputation bar
+        CapitalCityForRaceChange    = 0x0100,
+        Guild                       = 0x0200,
+        GarrisonInvasion            = 0x0400
+    };
 }
---
+++ WowPacketParserModule.V4_4_0_54481/Parsers/ReputationHandler.cs
@@ -0,0 +1,31 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V4_4_0_54481.Parsers
+{
+    public static class ReputationHandler
+    {
+        public const int FactionCount = 1000;
+
+        [Parser(Opcode.SMSG_FACTION_BONUS_INFO)]
+        public static void HandleFactionBonusInfo(Packet packet)
+        {
+            for (var i = 0; i < FactionCount; i++)
+                packet.ReadBit("FactionHasBonus", i);
+        }
+
+        [Parser(Opcode.SMSG_INITIALIZE_FACTIONS)]
+        public static void HandleInitializeFactions(Packet packet)
+        {
+            for (var i = 0; i < FactionCount; i++)
+            {
+                packet.ReadUInt16E<ReputationFlags>("ReputationFlags", i);
+                packet.ReadUInt32E<ReputationRank>("FactionStandings", i);
+            }
+
+            for (var i = 0; i < FactionCount; i++)
+                packet.ReadBit("FactionHasBonus", i);
+        }
+    }
+}
