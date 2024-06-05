5715330d21a86e1ad4085fe5d7a1a33f951028bb
ModoX <moardox@gmail.com>
2024-06-05 21:10:24 +0200

    prepare merge

---
+++ WowPacketParser/Enums/LossOfControlType.cs
@@ -0,0 +1,21 @@
+﻿
+namespace WowPacketParser.Enums
+{
+    public enum LossOfControlType
+    {
+        None                = 0,
+        Possess             = 1,
+        Confuse             = 2,
+        Charm               = 3,
+        Fear                = 4,
+        Stun                = 5,
+        Pacify              = 6,
+        Root                = 7,
+        Silence             = 8,
+        PacifySilence       = 9,
+        Disarm              = 10,
+        SchoolInterrupt     = 11,
+        FearMechanic        = 12,
+        StunMechanic        = 13,
+    }
+}
