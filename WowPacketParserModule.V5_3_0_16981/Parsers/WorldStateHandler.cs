703ca1e58a38e13ffc7c2421f72abdf24e6ec9b3
Bartosz Korczynski <b.andysc@gmail.com>
2024-06-06 12:12:27 +0100

    Added parsing SMSG_INIT_WORLD_STATES and SMSG_UPDATE_WORLD_STATE to protobuf format

--- WowPacketParserModule.V5_3_0_16981/Parsers/WorldStateHandler.cs
+++ WowPacketParserModule.V5_3_0_16981/Parsers/WorldStateHandler.cs
@@ -1,6 +1,7 @@
 using WowPacketParser.Enums;
 using WowPacketParser.Misc;
 using WowPacketParser.Parsing;
+using WowPacketParser.Proto;
 using CoreParsers = WowPacketParser.Parsing.Parsers;
 
 namespace WowPacketParserModule.V5_3_0_16981.Parsers
@@ -10,13 +11,14 @@ namespace WowPacketParserModule.V5_3_0_16981.Parsers
         [Parser(Opcode.SMSG_INIT_WORLD_STATES)]
         public static void HandleInitWorldStates(Packet packet)
         {
-            packet.ReadInt32<MapId>("Map ID");
-            CoreParsers.WorldStateHandler.CurrentZoneId = packet.ReadInt32<ZoneId>("Zone Id");
-            CoreParsers.WorldStateHandler.CurrentAreaId = packet.ReadInt32<AreaId>("Area Id");
+            PacketInitWorldStates worldStatesPacket = packet.Holder.InitWorldStates = new PacketInitWorldStates();
+            worldStatesPacket.MapId = packet.ReadInt32<MapId>("Map ID");
+            worldStatesPacket.ZoneId = CoreParsers.WorldStateHandler.CurrentZoneId = packet.ReadInt32<ZoneId>("Zone Id");
+            worldStatesPacket.AreaId = CoreParsers.WorldStateHandler.CurrentAreaId = packet.ReadInt32<AreaId>("Area Id");
 
             var numFields = packet.ReadBits("Field Count", 21);
             for (var i = 0; i < numFields; i++)
-                CoreParsers.WorldStateHandler.ReadWorldStateBlock(packet);
+                worldStatesPacket.WorldStates.Add(CoreParsers.WorldStateHandler.ReadWorldStateBlock(packet));
         }
     }
 }
