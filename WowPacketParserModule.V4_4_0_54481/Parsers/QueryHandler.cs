72bfa394ccd94c573bcb4c5e19520f858431738f
funjoker <funjoker109@gmail.com>
2024-06-08 21:47:09 +0200

    Add 4.4.0 SMSG_QUERY_PLAYER_NAMES_RESPONSE

--- WowPacketParserModule.V4_4_0_54481/Parsers/QueryHandler.cs
+++ WowPacketParserModule.V4_4_0_54481/Parsers/QueryHandler.cs
@@ -9,6 +9,37 @@ namespace WowPacketParserModule.V4_4_0_54481.Parsers
 {
     public static class QueryHandler
     {
+        public static PacketQueryPlayerNameResponse ReadNameCacheLookupResult(Packet packet, params object[] idx)
+        {
+            var response = new PacketQueryPlayerNameResponse();
+            packet.ResetBitReader();
+            packet.ReadByte("Result", idx);
+            packet.ReadPackedGuid128("Player", idx);
+            var hasPlayerGuidLookupData = packet.ReadBit("HasPlayerGuidLookupData", idx);
+            var hasThingy = packet.ReadBit("HasNameCacheUnused920", idx);
+
+            if (hasPlayerGuidLookupData)
+            {
+                var data = CharacterHandler.ReadPlayerGuidLookupData(packet, idx, "PlayerGuidLookupData");
+                response.Race = (uint)data.Race;
+                response.Gender = (uint)data.Gender;
+                response.Class = (uint)data.Class;
+                response.Level = data.Level;
+                response.HasData = true;
+            }
+
+            if (hasThingy)
+            {
+                packet.ResetBitReader();
+                packet.ReadUInt32("Unused1", idx, "NameCacheUnused920");
+                packet.ReadPackedGuid128("Unused2", idx, "NameCacheUnused920");
+                var length = packet.ReadBits(7);
+                packet.ReadWoWString("Unused3", length, idx, "NameCacheUnused920");
+            }
+
+            return response;
+        }
+
         [HasSniffData]
         [Parser(Opcode.SMSG_QUERY_CREATURE_RESPONSE)]
         public static void HandleCreatureQueryResponse(Packet packet)
@@ -306,6 +337,16 @@ namespace WowPacketParserModule.V4_4_0_54481.Parsers
         {
             packet.ReadInt32("Entry");
             packet.ReadPackedGuid128("Guid");
+        }
+
+        [Parser(Opcode.SMSG_QUERY_PLAYER_NAMES_RESPONSE)]
+        public static void HandleNameQueryResponse(Packet packet)
+        {
+            var response = packet.Holder.QueryPlayerNameResponse = new();
+
+            var count = packet.ReadUInt32("Count");
+            for (var i = 0; i < count; ++i)
+                response.Responses.Add(ReadNameCacheLookupResult(packet, i));
         }
     }
 }
