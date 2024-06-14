8e1626e5fb1b02702526051673ed5de6bc392b35
Naddley <NaddleyTC@gmail.com>
2024-06-14 22:21:59 +0200

    Added build 10.2.7.55142

--- WowPacketParser/Enums/Version/Opcodes.cs
+++ WowPacketParser/Enums/Version/Opcodes.cs
@@ -695,6 +695,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V10_2_7_54847:
                 case ClientVersionBuild.V10_2_7_54904:
                 case ClientVersionBuild.V10_2_7_54988:
+                case ClientVersionBuild.V10_2_7_55142:
                     return ClientVersionBuild.V10_2_7_54577;
                 case ClientVersionBuild.V1_13_2_31446:
                 case ClientVersionBuild.V1_13_2_31650:
