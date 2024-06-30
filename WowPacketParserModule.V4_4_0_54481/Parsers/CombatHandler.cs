ebe574f6312fc9c96bcf1363ab56ec0086541a86
Ovahlord <dreadkiller@gmx.de>
2024-07-01 00:32:34 +0200

    Added SMSG_HIGHEST_THREAT_UPDATE and SMSG_THREAT_UPDATE parsers for 4.4.0

--- WowPacketParserModule.V4_4_0_54481/Parsers/CombatHandler.cs
+++ WowPacketParserModule.V4_4_0_54481/Parsers/CombatHandler.cs
@@ -36,5 +36,34 @@ namespace WowPacketParserModule.V4_4_0_54481.Parsers
         {
             packet.ReadPackedGuid128("Guid");
         }
+
+        [Parser(Opcode.SMSG_HIGHEST_THREAT_UPDATE)]
+        public static void HandleHighestThreatlistUpdate(Packet packet)
+        {
+            packet.ReadPackedGuid128("UnitGUID");
+            packet.ReadPackedGuid128("HighestThreatGUID");
+
+            var count = packet.ReadUInt32("ThreatListCount");
+
+            // ThreatInfo
+            for (var i = 0; i < count; i++)
+            {
+                packet.ReadPackedGuid128("UnitGUID", i);
+                packet.ReadInt64("Threat", i);
+            }
+        }
+
+        [Parser(Opcode.SMSG_THREAT_UPDATE)]
+        public static void HandleThreatlistUpdate(Packet packet)
+        {
+            packet.ReadPackedGuid128("UnitGUID");
+            var count = packet.ReadInt32("ThreatListCount");
+
+            for (int i = 0; i < count; i++)
+            {
+                packet.ReadPackedGuid128("TargetGUID", i);
+                packet.ReadInt64("Threat", i);
+            }
+        }
     }
 }
