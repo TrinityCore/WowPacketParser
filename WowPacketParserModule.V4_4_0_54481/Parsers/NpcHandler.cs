dc825fc2e9fc6361ed1681dbee9bcde993ab9513
Ovahlord <dreadkiller@gmx.de>
2024-07-08 17:57:07 +0200

    Added 4.4.0 parser for SMSG_VENDOR_INVENTORY

--- WowPacketParserModule.V4_4_0_54481/Parsers/NpcHandler.cs
+++ WowPacketParserModule.V4_4_0_54481/Parsers/NpcHandler.cs
@@ -268,6 +268,48 @@ namespace WowPacketParserModule.V4_4_0_54481.Parsers
             CoreParsers.NpcHandler.AddToCreatureTrainers(trainer.Id, packet.TimeSpan);
         }
 
+        [Parser(Opcode.SMSG_VENDOR_INVENTORY)]
+        public static void HandleVendorInventory(Packet packet)
+        {
+            uint entry = packet.ReadPackedGuid128("VendorGUID").GetEntry();
+            packet.ReadByte("Reason");
+            int count = packet.ReadInt32("VendorItems");
+
+            for (int i = 0; i < count; ++i)
+            {
+                NpcVendor vendor = new NpcVendor
+                {
+                    Entry = entry,
+                };
+
+                packet.ReadInt64("Price", i);
+                vendor.Slot = packet.ReadInt32("Muid", i);
+                vendor.Type = (uint)packet.ReadInt32("Type", i);
+                packet.ReadInt32("Durability", i);
+                int buyCount = packet.ReadInt32("StackCount", i);
+                int maxCount = packet.ReadInt32("Quantity", i);
+                vendor.ExtendedCost = packet.ReadUInt32("ExtendedCostID", i);
+                vendor.PlayerConditionID = packet.ReadUInt32("PlayerConditionFailed", i);
+
+                packet.ResetBitReader();
+                packet.ReadBit("Locked", i);
+                vendor.IgnoreFiltering = packet.ReadBit("DoNotFilterOnVendor", i);
+                packet.ReadBit("Refundable", i);
+
+                packet.ResetBitReader();
+                vendor.Item = Substructures.ItemHandler.ReadItemInstance(packet, i).ItemID;
+
+                vendor.MaxCount = maxCount == -1 ? 0 : (uint)maxCount; // TDB
+                if (vendor.Type == 2)
+                    vendor.MaxCount = (uint)buyCount;
+
+                Storage.NpcVendors.Add(vendor, packet.TimeSpan);
+            }
+
+            CoreParsers.NpcHandler.LastGossipOption.Reset();
+            CoreParsers.NpcHandler.TempGossipOptionPOI.Reset();
+        }
+
         [Parser(Opcode.CMSG_BANKER_ACTIVATE)]
         [Parser(Opcode.CMSG_BINDER_ACTIVATE)]
         [Parser(Opcode.SMSG_BINDER_CONFIRM)]
