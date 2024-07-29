f1505605114c75a8c5a83ea584ccf6e5fc0b7685
Ovahlord <dreadkiller@gmx.de>
2024-07-29 05:24:02 +0200

    Added 4.4.0 parser for CMSG_REQUEST_PVP_REWARDS and SMSG_REQUEST_PVP_REWARDS_RESPONSE

--- WowPacketParserModule.V4_4_0_54481/Parsers/BattlegroundHandler.cs
+++ WowPacketParserModule.V4_4_0_54481/Parsers/BattlegroundHandler.cs
@@ -17,8 +17,22 @@ namespace WowPacketParserModule.V4_4_0_54481.Parsers
 
         [Parser(Opcode.CMSG_REQUEST_BATTLEFIELD_STATUS)]
         [Parser(Opcode.CMSG_REQUEST_RATED_PVP_INFO)]
+        [Parser(Opcode.CMSG_REQUEST_PVP_REWARDS)]
         public static void HandleBattlegroundZero(Packet packet)
         {
+        }
+
+        [Parser(Opcode.SMSG_REQUEST_PVP_REWARDS_RESPONSE)]
+        public static void HandleRequestPVPRewardsResponse(Packet packet)
+        {
+            LfgHandler.ReadLfgPlayerQuestReward(packet, "FirstRandomBGWinRewards");
+            LfgHandler.ReadLfgPlayerQuestReward(packet, "FirstRandomBGLossRewards");
+            LfgHandler.ReadLfgPlayerQuestReward(packet, "NthRandomBGWinRewards");
+            LfgHandler.ReadLfgPlayerQuestReward(packet, "NthRandomBGLossRewards");
+            LfgHandler.ReadLfgPlayerQuestReward(packet, "RatedBGRewards");
+            LfgHandler.ReadLfgPlayerQuestReward(packet, "Arena2v2Rewards");
+            LfgHandler.ReadLfgPlayerQuestReward(packet, "Arena3v3Rewards");
+            LfgHandler.ReadLfgPlayerQuestReward(packet, "Arena5v5Rewards");
         }
     }
 }
