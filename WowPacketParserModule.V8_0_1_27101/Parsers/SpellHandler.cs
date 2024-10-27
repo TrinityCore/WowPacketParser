dc01a27e3fa62397d0f327e45209405b105603dc
Shauren <shauren.trinity@gmail.com>
2024-10-27 17:25:32 +0100

    Update 11.0.5 combat log packet structures

--- WowPacketParserModule.V8_0_1_27101/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/SpellHandler.cs
@@ -193,6 +193,11 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
             packet.ReadInt32("AttackPower", idx);
             packet.ReadInt32("SpellPower", idx);
             packet.ReadInt32("Armor", idx);
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V11_0_5_57171))
+            {
+                packet.ReadInt32("Unknown_1105_1", idx);
+                packet.ReadInt32("Unknown_1105_2", idx);
+            }
 
             packet.ResetBitReader();
 
@@ -204,7 +209,7 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
                 if (ClientVersion.RemovedInVersion(ClientType.TheWarWithin))
                     packet.ReadInt32E<PowerType>("PowerType", idx, i);
                 else
-                    packet.ReadByteE<PowerType>("PowerType");
+                    packet.ReadByteE<PowerType>("PowerType", idx, i);
                 packet.ReadInt32("Amount", idx, i);
                 packet.ReadInt32("Cost", idx, i);
             }
