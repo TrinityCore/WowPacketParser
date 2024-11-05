f47fdee7c9bbc83e25ccabe76406b1e86a0901a1
funjoker <funjoker109@gmail.com>
2024-11-05 23:33:07 +0100

    Fix 4.4.1 SMSG_INSPECT_RESULT

--- WowPacketParserModule.V10_0_0_46181/Parsers/TraitHandler.cs
+++ WowPacketParserModule.V10_0_0_46181/Parsers/TraitHandler.cs
@@ -14,11 +14,26 @@ namespace WowPacketParserModule.V10_0_0_46181.Parsers
             packet.ReadInt32("GrantedRanks", indexes);
         }
 
+        public static void ReadTraitSubTreeCache(Packet packet, params object[] indexes)
+        {
+            packet.ReadInt32("TraitSubTreeID", indexes);
+            var entries = packet.ReadUInt32();
+
+            for (var i = 0u; i < entries; ++i)
+                ReadTraitEntry(packet, indexes, "TraitEntry", i);
+
+            packet.ReadBit("Active");
+        }
+
         public static void ReadTraitConfig(Packet packet, params object[] indexes)
         {
             packet.ReadInt32("ID", indexes);
             var type = packet.ReadInt32("Type", indexes);
             var entries = packet.ReadUInt32();
+
+            uint subtrees = 0;
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V4_4_1_57294))
+                subtrees = packet.ReadUInt32();
 
             switch (type)
             {
@@ -40,6 +55,9 @@ namespace WowPacketParserModule.V10_0_0_46181.Parsers
 
             var nameLength = packet.ReadBits(9);
             packet.ResetBitReader();
+
+            for (var i = 0u; i < subtrees; ++i)
+                ReadTraitSubTreeCache(packet, indexes, "TraitSubTreeCache", i);
 
             packet.ReadWoWString("Name", nameLength, indexes);
         }
