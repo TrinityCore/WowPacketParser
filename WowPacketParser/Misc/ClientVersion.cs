cd11571e711b6d23c0d7f93542947ee50f300c69
Naddley <NaddleyTC@gmail.com>
2024-11-21 15:14:32 +0100

    Added build 11.0.5.57689

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -581,6 +581,7 @@ namespace WowPacketParser.Misc
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V11_0_5_57292, new DateTime(2024, 10, 30)),
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V11_0_5_57388, new DateTime(2024, 11, 01)),
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V11_0_5_57534, new DateTime(2024, 11, 14)),
+            new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V11_0_5_57689, new DateTime(2024, 11, 20)),
 
             // no classic info, pkt contain build in header
         };
@@ -1154,6 +1155,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V11_0_5_57292:
                 case ClientVersionBuild.V11_0_5_57388:
                 case ClientVersionBuild.V11_0_5_57534:
+                case ClientVersionBuild.V11_0_5_57689:
                     return ClientVersionBuild.V11_0_0_55666;
                 //Classic
                 case ClientVersionBuild.V1_13_2_31446:
