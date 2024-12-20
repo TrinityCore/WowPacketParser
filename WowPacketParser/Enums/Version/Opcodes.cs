70bd05c5d8c8f8e330d475d38f9d3b10f5815406
Naddley <NaddleyTC@gmail.com>
2024-12-21 00:34:54 +0100

    Added build 11.0.7.58238

--- WowPacketParser/Enums/Version/Opcodes.cs
+++ WowPacketParser/Enums/Version/Opcodes.cs
@@ -744,6 +744,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V11_0_7_58123:
                 case ClientVersionBuild.V11_0_7_58162:
                 case ClientVersionBuild.V11_0_7_58187:
+                case ClientVersionBuild.V11_0_7_58238:
                     return ClientVersionBuild.V11_0_7_58123;
                 case ClientVersionBuild.V1_13_2_31446:
                 case ClientVersionBuild.V1_13_2_31650:
