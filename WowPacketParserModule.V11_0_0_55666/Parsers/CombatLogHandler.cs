c45c3f470ec2347bb1bce900e9abeba288a6dd6d
Muru <timurdelimin@gmail.com>
2025-01-17 18:48:33 +0700

    Fixed SMSG_SPELL_ENERGIZE_LOG structure for 11.0 (#910)

---
+++ WowPacketParserModule.V11_0_0_55666/Parsers/CombatLogHandler.cs
@@ -0,0 +1,28 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V11_0_0_55666.Parsers
+{
+    public static class CombatLogHandler
+    {
+        [Parser(Opcode.SMSG_SPELL_ENERGIZE_LOG)]
+        public static void HandleSpellEnergizeLog(Packet packet)
+        {
+            packet.ReadPackedGuid128("CasterGUID");
+            packet.ReadPackedGuid128("TargetGUID");
+
+            packet.ReadInt32<SpellId>("SpellID");
+            packet.ReadByteE<PowerType>("Type");
+
+            packet.ReadInt32("Amount");
+            packet.ReadInt32("OverEnergize");
+
+            packet.ResetBitReader();
+
+            var hasLogData = packet.ReadBit("HasLogData");
+            if (hasLogData)
+                V6_0_2_19033.Parsers.SpellHandler.ReadSpellCastLogData(packet);
+        }
+    }
+}
