7bd7ffd5c55cdea6bb341c5a9a089108ed621816
Night Stalker <timurdelimin@gmail.com>
2025-01-24 10:13:31 +0700

    Fixed SMSG_SET_TIME_ZONE_INFORMATION structure for 11.0

---
+++ WowPacketParserModule.V11_0_0_55666/Parsers/SessionHandler.cs
@@ -0,0 +1,21 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V11_0_0_55666.Parsers
+{
+    public static class SessionHandler
+    {
+        [Parser(Opcode.SMSG_SET_TIME_ZONE_INFORMATION)]
+        public static void HandleSetTimeZoneInformation(Packet packet)
+        {
+            var len1 = packet.ReadBits(7);
+            var len2 = packet.ReadBits(7);
+            var len3 = packet.ReadBits(7);
+
+            packet.ReadWoWString("ServerTimeTZ", len1);
+            packet.ReadWoWString("GameTimeTZ", len2);
+            packet.ReadWoWString("ServerRegionalTZ", len3);
+        }
+    }
+}
