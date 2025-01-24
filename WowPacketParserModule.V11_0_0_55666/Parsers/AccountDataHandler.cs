14fafac422bfa8c3c4bc1214ab0f0a91ec23870a
Night Stalker <timurdelimin@gmail.com>
2025-01-24 10:40:57 +0700

    Fixed SMSG_ACCOUNT_DATA_TIMES structure for 11.0

---
+++ WowPacketParserModule.V11_0_0_55666/Parsers/AccountDataHandler.cs
@@ -0,0 +1,21 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V11_0_0_55666.Parsers
+{
+    public static class AccountDataHandler
+    {
+        [Parser(Opcode.SMSG_ACCOUNT_DATA_TIMES)]
+        public static void HandleAccountDataTimes(Packet packet)
+        {
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadTime64("ServerTime");
+
+            var count = 17;
+
+            for (var i = 0; i < count; ++i)
+                packet.ReadTime64($"[{(AccountDataType)i}] Time", i);
+        }
+    }
+}
