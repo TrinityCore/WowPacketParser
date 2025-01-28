d632e06e3d590356900a766ebb5a09374d70a0f3
Shauren <shauren.trinity@gmail.com>
2025-01-28 23:15:12 +0100

    Fixed SMSG_ACCOUNT_DATA_TIMES structure for 10.x

---
+++ WowPacketParserModule.V10_0_0_46181/Parsers/AccountDataHandler.cs
@@ -0,0 +1,21 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V10_0_0_46181.Parsers
+{
+    public static class AccountDataHandler
+    {
+        [Parser(Opcode.SMSG_ACCOUNT_DATA_TIMES)]
+        public static void HandleAccountDataTimes(Packet packet)
+        {
+            packet.ReadPackedGuid128("Guid");
+            packet.ReadTime64("ServerTime");
+
+            var count = ClientVersion.AddedInVersion(ClientVersionBuild.V10_2_6_53840) ? 16 : 15;
+
+            for (var i = 0; i < count; ++i)
+                packet.ReadTime64($"[{(AccountDataType)i}] Time", i);
+        }
+    }
+}
