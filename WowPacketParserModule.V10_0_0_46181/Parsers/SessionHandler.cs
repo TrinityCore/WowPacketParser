dfae6148af36116812aa04928dbecfd343639a8e
Muru <timurdelimin@gmail.com>
2025-01-29 02:13:20 +0700

    Fixed SMSG_SET_TIME_ZONE_INFORMATION structure for 10.0 (#920)

---
+++ WowPacketParserModule.V10_0_0_46181/Parsers/SessionHandler.cs
@@ -0,0 +1,21 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V10_0_0_46181.Parsers
+{
+    public static class SessionHandler
+    {
+        [Parser(Opcode.SMSG_SET_TIME_ZONE_INFORMATION)]
+        public static void HandleSetTimeZoneInformation(Packet packet)
+        {
+            var gameTimeTZLength = packet.ReadBits(7);
+            var serverTimeTZLength = packet.ReadBits(7);
+            var serverRegionalTimeTZLength = packet.ReadBits(7);
+
+            packet.ReadWoWString("GameTimeTZ", gameTimeTZLength);
+            packet.ReadWoWString("ServerTimeTZ", serverTimeTZLength);
+            packet.ReadWoWString("ServerRegionalTimeTZ", serverRegionalTimeTZLength);
+        }
+    }
+}
