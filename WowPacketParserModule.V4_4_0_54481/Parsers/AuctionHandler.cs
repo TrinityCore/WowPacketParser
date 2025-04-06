a6facb3bbd92574f681fe1d87258d125554fdf04
funjoker <funjoker109@gmail.com>
2025-04-06 23:59:30 +0200

    Add 4.4.2 CMSG_AUCTION_BROWSE_QUERY

--- WowPacketParserModule.V4_4_0_54481/Parsers/AuctionHandler.cs
+++ WowPacketParserModule.V4_4_0_54481/Parsers/AuctionHandler.cs
@@ -504,6 +504,64 @@ namespace WowPacketParserModule.V4_4_0_54481.Parsers
                 ReadAuctionFavoriteInfo(packet, "FavoriteInfo", i);
         }
 
+        public static void ReadAuctionListFilterSubClass(Packet packet, params object[] idx)
+        {
+            packet.ReadUInt64("InvTypeMask", idx);
+            packet.ReadInt32("ItemSubclass", idx);
+        }
+
+        public static void ReadAuctionListFilterClass(Packet packet, params object[] idx)
+        {
+            packet.ReadInt32("FilterClass", idx);
+            packet.ResetBitReader();
+            var subClassFilterCount = packet.ReadBits("SubClassFilterCount", 5, idx);
+            for (var i = 0; i < subClassFilterCount; i++)
+                ReadAuctionListFilterSubClass(packet, i, "SubClassFilter", i, idx);
+        }
+
+        public static void ReadAuctionSortDef(Packet packet, params object[] idx)
+        {
+            packet.ResetBitReader();
+            packet.ReadByte("SortOrder", idx);
+            packet.ReadBit("ReverseSort", idx);
+        }
+
+        [Parser(Opcode.CMSG_AUCTION_BROWSE_QUERY)]
+        public static void HandleAuctionBrowseQuery(Packet packet)
+        {
+            packet.ReadPackedGuid128("Auctioneer");
+            packet.ReadUInt32("Offset");
+            packet.ReadByte("MinLevel");
+            packet.ReadByte("MaxLevel");
+            packet.ReadByte("Unused1007_1");
+            packet.ReadByte("Unused1007_2");
+            packet.ReadUInt32("Filters");
+
+            var knownPetsSize = packet.ReadUInt32();
+            packet.ReadByte("MaxPetLevel");
+            packet.ReadUInt32("Unused1026");
+
+            for (var i = 0; i < knownPetsSize; i++)
+                packet.ReadByte("KnownPetMask", i);
+
+            packet.ResetBitReader();
+            var taintedBy = packet.ReadBit("TaintedBy");
+            var nameLen = packet.ReadBits(8);
+            var itemClassFilterSize = packet.ReadBits(3);
+            var sortsSize = packet.ReadBits(2);
+
+            if (taintedBy)
+                AddonHandler.ReadAddOnInfo(packet, "TaintedBy");
+
+            packet.ReadWoWString("Name", nameLen);
+
+            for (var i = 0; i < itemClassFilterSize; i++)
+                ReadAuctionListFilterClass(packet, i, "FilterClass");
+            
+            for (var i = 0; i < sortsSize; i++)
+                ReadAuctionSortDef(packet, i, "SortDef");
+        }
+
         [Parser(Opcode.CMSG_AUCTION_LIST_PENDING_SALES)]
         public static void HandleAuctionZero(Packet packet)
         {
