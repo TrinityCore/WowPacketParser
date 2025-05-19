a45f8e48647c2054bbea97f4b45df0ae59fc072c
funjoker <funjoker109@gmail.com>
2025-05-19 21:21:43 +0200

    Add 3.4.4 SMSG_ENUM_CHARACTERS_RESULT

--- WowPacketParserModule.V3_4_0_45166/Parsers/CharacterHandler.cs
+++ WowPacketParserModule.V3_4_0_45166/Parsers/CharacterHandler.cs
@@ -26,7 +26,10 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
         public static void ReadRaceLimitDisableInfo(Packet packet, params object[] idx)
         {
-            packet.ReadInt32E<Race>("RaceID", idx);
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                packet.ReadSByteE<Race>("RaceID", idx);
+            else
+                packet.ReadInt32E<Race>("RaceID", idx);
             packet.ReadInt32("BlockReason", idx);
         }
 
@@ -115,18 +118,167 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
         public static void ReadRaceUnlockData(Packet packet, params object[] idx)
         {
-            packet.ReadInt32E<Race>("RaceID", idx);
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                packet.ReadByteE<Race>("RaceID", idx);
+            else
+                packet.ReadInt32E<Race>("RaceID", idx);
             packet.ResetBitReader();
             packet.ReadBit("HasExpansion", idx);
             packet.ReadBit("HasAchievement", idx);
             packet.ReadBit("HasHeritageArmor", idx);
             packet.ReadBit("IsLocked", idx);
+
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                packet.ReadBit("Unused1027", idx);
+        }
+
+        public static void ReadVisualItemInfo344(Packet packet, params object[] idx)
+        {
+            packet.ReadUInt32("DisplayID", idx);
+            packet.ReadByteE<InventoryType>("InvType", idx);
+            packet.ReadUInt32("DisplayEnchantID", idx);
+            packet.ReadByte("Subclass", idx);
+            packet.ReadInt32("SecondaryItemModifiedAppearanceID", idx);
+            packet.ReadInt32("ItemID", idx);
+            packet.ReadInt32("TransmogrifiedItemID", idx);
+        }
+
+        public static void ReadBasicCharacterListEntry(Packet packet, params object[] idx)
+        {
+            var playerGuid = packet.ReadPackedGuid128("Guid", idx);
+            packet.ReadUInt32("VirtualRealmAddress", idx);
+            packet.ReadByte("ListPosition", idx);
+            var race = packet.ReadByteE<Race>("RaceID", idx);
+            packet.ReadByteE<Gender>("SexID", idx);
+            var @class = packet.ReadByteE<Class>("ClassID", idx);
+            packet.ReadInt16("SpecID", idx);
+            var customizationCount = packet.ReadUInt32();
+            var level = packet.ReadByte("ExperienceLevel", idx);
+            var mapId = packet.ReadInt32<MapId>("MapID", idx);
+            var zone = packet.ReadInt32<ZoneId>("ZoneID", idx);
+            var pos = packet.ReadVector3("PreloadPos", idx);
+            packet.ReadUInt64("GuildClubMemberID", idx);
+            packet.ReadPackedGuid128("GuildGUID", idx);
+            packet.ReadUInt32("Flags", idx);
+            packet.ReadUInt32("Flags2", idx);
+            packet.ReadUInt32("Flags3", idx);
+            packet.ReadUInt32("Flags4", idx);
+            packet.ReadByte("CantLoginReason", idx);
+            packet.ReadUInt32("PetCreatureDisplayID", idx);
+            packet.ReadUInt32("PetExperienceLevel", idx);
+            packet.ReadUInt32("PetCreatureFamilyID", idx);
+
+            for (uint j = 0; j < 19; ++j)
+                ReadVisualItemInfo344(packet, idx, "VisualItems", j);
+
+            packet.ReadInt32("SaveVersion", idx);
+            packet.ReadTime64("LastPlayedTime", idx);
+            packet.ReadInt32("LastLoginVersion", idx);
+
+            ReadCustomTabardInfo(packet, idx, "PersonalTabard");
+
+            for (uint j = 0; j < 2; ++j)
+                packet.ReadInt32("ProfessionIDs", idx, j);
+
+            packet.ReadInt32("TimerunningSeasonID", idx);
+            packet.ReadUInt32("OverrideSelectScreenFileDataID", idx);
+            packet.ReadUInt32("Unused1110_1", idx);
+
+            for (var j = 0u; j < customizationCount; ++j)
+                ReadChrCustomizationChoice(packet, idx, "Customizations", j);
+
+            packet.ResetBitReader();
+
+            var nameLength = packet.ReadBits(6);
+            var firstLogin = packet.ReadBit("FirstLogin", idx);
+            packet.ReadBit("Unused1110_2", idx);
+            packet.ReadBit("Unused1110_3", idx);
+
+            var name = packet.ReadWoWString("Character Name", nameLength, idx);
+
+            if (firstLogin)
+            {
+                PlayerCreateInfo startPos = new PlayerCreateInfo { Race = race, Class = @class, Map = (uint)mapId, Zone = (uint)zone, Position = pos, Orientation = 0 };
+                Storage.StartPositions.Add(startPos, packet.TimeSpan);
+            }
+
+            var playerInfo = new Player { Race = race, Class = @class, Name = name, FirstLogin = firstLogin, Level = level, Type = ObjectType.Player };
+            if (Storage.Objects.ContainsKey(playerGuid))
+                Storage.Objects[playerGuid] = new Tuple<WoWObject, TimeSpan?>(playerInfo, packet.TimeSpan);
+            else
+                Storage.Objects.Add(playerGuid, playerInfo, packet.TimeSpan);
+        }
+
+        public static void ReadCharacterRestrictionAndMailData(Packet packet, params object[] idx)
+        {
+            packet.ResetBitReader();
+            packet.ReadBit("BoostInProgress", idx);
+            packet.ReadBit("RpeResetAvailable", idx);
+            packet.ReadBit("RpeResetQuestClearAvailable", idx);
+
+            packet.ReadUInt32("RestrictionFlags", idx);
+            var mailSenderLengths = new uint[packet.ReadUInt32()];
+            var mailSenderTypes = packet.ReadUInt32();
+
+            for (var j = 0; j < mailSenderTypes; ++j)
+                packet.ReadUInt32("MailSenderType", idx, j);
+
+            packet.ResetBitReader();
+
+            for (var j = 0; j < mailSenderLengths.Length; ++j)
+                mailSenderLengths[j] = packet.ReadBits(6);
+
+            for (var j = 0; j < mailSenderLengths.Length; ++j)
+                packet.ReadDynamicString("MailSender", mailSenderLengths[j], idx);
+        }
+
+        public static void ReadCharacterListEntry344(Packet packet, params object[] idx)
+        {
+            ReadBasicCharacterListEntry(packet, idx, "Basic");
+            ReadCharacterRestrictionAndMailData(packet, idx, "RestrictionsAndMails");
+        }
+
+        public static void ReadRegionwideCharacterListEntry(Packet packet, params object[] idx)
+        {
+            ReadBasicCharacterListEntry(packet, idx, "Basic");
+            packet.ReadUInt64("Money", idx);
+            packet.ReadSingle("AvgEquippedItemLevel", idx);
+            packet.ReadSingle("CurrentSeasonMythicPlusOverallScore", idx);
+            packet.ReadInt32("CurrentSeasonBestPvpRating", idx);
+            packet.ReadSByte("PvpRatingBracket", idx);
+            packet.ReadInt16("PvpRatingAssociatedSpecID", idx);
+        }
+
+        public static void ReadWarbandGroupMember(Packet packet, params object[] idx)
+        {
+            packet.ReadInt32("WarbandScenePlacementID", idx);
+            var type = packet.ReadInt32("Type", idx);
+            if (type == 0)
+                packet.ReadPackedGuid128("Guid", idx);
+        }
+
+        public static void ReadWarbandGroup(Packet packet, params object[] idx)
+        {
+            packet.ReadUInt64("GroupID", idx);
+            packet.ReadByte("OrderIndex", idx);
+            packet.ReadInt32("WarbandSceneID", idx);
+            packet.ReadInt32("Flags", idx);
+            var memberCount = packet.ReadUInt32();
+            for (var i = 0u; i < memberCount; ++i)
+                ReadWarbandGroupMember(packet, idx, "Members", i);
+
+            var nameLength = packet.ReadBits(9);
+            packet.ReadWoWString("Name", nameLength, idx);
         }
 
         [Parser(Opcode.SMSG_ENUM_CHARACTERS_RESULT, ClientVersionBuild.V3_4_3_51505)]
         public static void HandleEnumCharactersResult(Packet packet)
         {
             packet.ReadBit("Success");
+
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                packet.ReadBit("Realmless");
+
             packet.ReadBit("IsDeletedCharacters");
             packet.ReadBit("IsNewPlayerRestrictionSkipped");
             packet.ReadBit("IsNewPlayerRestricted");
@@ -134,11 +286,23 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
             packet.ReadBit("IsTrialAccountRestricted");
             var hasDisabledClassesMask = packet.ReadBit("HasDisabledClassesMask");
 
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                packet.ReadBit("DontCreateCharacterDisplays");
+
             var charsCount = packet.ReadUInt32("CharactersCount");
+
+            uint regionwideCharsCount = 0;
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                regionwideCharsCount = packet.ReadUInt32("RegionwideCharactersCount");
+
             packet.ReadInt32("MaxCharacterLevel");
             var raceUnlockCount = packet.ReadUInt32("RaceUnlockCount");
             var unlockedConditionalAppearanceCount = packet.ReadUInt32("UnlockedConditionalAppearanceCount");
             var raceLimitDisablesCount = packet.ReadUInt32("RaceLimitDisablesCount");
+
+            uint warbandGroupsCount = 0;
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                warbandGroupsCount = packet.ReadUInt32("WarbandGroupsCount");
 
             if (hasDisabledClassesMask)
                 packet.ReadUInt32("DisabledClassesMask");
@@ -150,10 +314,21 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
                 ReadRaceLimitDisableInfo(packet, "RaceLimitDisableInfo", i);
 
             for (var i = 0u; i < charsCount; ++i)
-                ReadCharactersListEntry(packet, i, "Characters");
+            {
+                if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                    ReadCharacterListEntry344(packet, i, "Characters");
+                else
+                    ReadCharactersListEntry(packet, i, "Characters");
+            }
+
+            for (var i = 0u; i < regionwideCharsCount; ++i)
+                ReadRegionwideCharacterListEntry(packet, i, "RegionwideCharacters");
 
             for (var i = 0u; i < raceUnlockCount; ++i)
                 ReadRaceUnlockData(packet, i, "RaceUnlockData");
+
+            for (var i = 0u; i < warbandGroupsCount; ++i)
+                ReadWarbandGroup(packet, i, "WarbandGroups");
         }
 
         [Parser(Opcode.SMSG_LEVEL_UP_INFO)]
