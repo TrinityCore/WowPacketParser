bc7d53b4fef066895b0f0f8d5dc67f7e0bd4772d
funjoker <funjoker109@gmail.com>
2025-06-02 10:49:04 +0200

    Update wotlk_calssic ReadTraitConfig

--- WowPacketParserModule.V3_4_0_45166/Parsers/TraitHandler.cs
+++ WowPacketParserModule.V3_4_0_45166/Parsers/TraitHandler.cs
@@ -14,11 +14,28 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
             packet.ReadInt32("GrantedRanks", indexes);
         }
 
+        public static void ReadTraitSubTreeCache(Packet packet, params object[] indexes)
+        {
+            packet.ResetBitReader();
+
+            packet.ReadInt32("TraitSubTreeID", indexes);
+            var entries = packet.ReadUInt32();
+
+            for (var i = 0u; i < entries; ++i)
+                ReadTraitEntry(packet, indexes, "TraitEntry", i);
+
+            packet.ReadBit("Active", indexes);
+        }
+
         public static void ReadTraitConfig(Packet packet, params object[] indexes)
         {
             packet.ReadInt32("ID", indexes);
             var type = packet.ReadInt32("Type", indexes);
             var entries = packet.ReadUInt32();
+
+            uint subtrees = 0;
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_4_4_59817))
+                subtrees = packet.ReadUInt32();
 
             switch (type)
             {
@@ -38,8 +55,11 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
             for (var i = 0u; i < entries; ++i)
                 ReadTraitEntry(packet, indexes, "TraitEntry", i);
 
-            var nameLength = packet.ReadBits(9);
             packet.ResetBitReader();
+            var nameLength = packet.ReadBits(9);
+
+            for (var i = 0u; i < subtrees; ++i)
+                ReadTraitSubTreeCache(packet, indexes, "TraitSubTreeCache", i);
 
             packet.ReadWoWString("Name", nameLength, indexes);
         }
