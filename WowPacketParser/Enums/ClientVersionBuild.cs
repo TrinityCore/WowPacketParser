e3b54deabddff15682afbb74486a4fc8955d1dff
killerwife <killerwife@gmail.com>
2025-10-01 07:27:25 +0200

    Add V1_15_7_63306

--- WowPacketParser/Enums/ClientVersionBuild.cs
+++ WowPacketParser/Enums/ClientVersionBuild.cs
@@ -798,6 +798,7 @@ namespace WowPacketParser.Enums
         V1_15_7_61257 = 61257, // live
         V1_15_7_61582 = 61582, // live
         V1_15_7_62797 = 62797, // live
+        V1_15_7_63306 = 63306, // live
 
         // TBC Classic
         V2_5_1_38598 = 38598, // ptr
--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -1566,6 +1566,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V1_15_7_61257:
                 case ClientVersionBuild.V1_15_7_61582:
                 case ClientVersionBuild.V1_15_7_62797:
+                case ClientVersionBuild.V1_15_7_63306:
                 case ClientVersionBuild.V4_4_2_60192:
                 case ClientVersionBuild.V4_4_2_60895:
                     return ClientVersionBuild.V4_4_0_54481;
@@ -2017,6 +2018,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V1_15_7_61257:
                 case ClientVersionBuild.V1_15_7_61582:
                 case ClientVersionBuild.V1_15_7_62797:
+                case ClientVersionBuild.V1_15_7_63306:
                     return true;
                 default:
                     return false;
