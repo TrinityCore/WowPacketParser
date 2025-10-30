1e5eb70519e79952097fba87dc9402cc92be1bb1
Nik <102656911+notwonderful@users.noreply.github.com>
2025-10-30 21:55:24 +0300

    Added build V11_2_5_64154 (#963)

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -642,6 +642,7 @@ namespace WowPacketParser.Misc
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V11_2_5_63825, new DateTime(2025, 10, 14)),
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V11_2_5_63834, new DateTime(2025, 10, 15)),
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V11_2_5_63906, new DateTime(2025, 10, 21)),
+            new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V11_2_5_64154, new DateTime(2025, 10, 30)),
 
             // no classic info, pkt contain build in header
         };
@@ -1273,6 +1274,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V11_2_5_63825:
                 case ClientVersionBuild.V11_2_5_63834:
                 case ClientVersionBuild.V11_2_5_63906:
+                case ClientVersionBuild.V11_2_5_64154:
                     return ClientVersionBuild.V11_0_0_55666;
                 //Classic
                 case ClientVersionBuild.V1_13_2_31446:
