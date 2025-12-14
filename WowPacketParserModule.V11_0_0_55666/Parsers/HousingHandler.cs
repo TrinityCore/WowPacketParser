291a21b1f51b776590d124339a3f769175c0193d
Luzifix <luzifix19@gmail.com>
2025-12-14 21:21:32 +0100

    Add parsing for CMSG_HOUSING_FIXTURE_SET_EDITOR_MODE_ACTIVE and CMSG_HOUSING_ROOM_SET_EDITOR_MODE_ACTIVE

--- WowPacketParserModule.V11_0_0_55666/Parsers/HousingHandler.cs
+++ WowPacketParserModule.V11_0_0_55666/Parsers/HousingHandler.cs
@@ -5,9 +5,11 @@ using WowPacketParser.Parsing;
 namespace WowPacketParserModule.V11_0_0_55666.Parsers
 {
     public static class HousingHandler
-    {
+    {        
         [Parser(Opcode.CMSG_HOUSING_DECOR_SET_EDITOR_MODE_ACTIVE)]
-        public static void HandleDecorSetEditorModeActive(Packet packet)
+        [Parser(Opcode.CMSG_HOUSING_FIXTURE_SET_EDITOR_MODE_ACTIVE)]
+        [Parser(Opcode.CMSG_HOUSING_ROOM_SET_EDITOR_MODE_ACTIVE)]
+        public static void HandleHousingSetEditorModeActive(Packet packet)
         {
             packet.ReadBool("Active");
         }
