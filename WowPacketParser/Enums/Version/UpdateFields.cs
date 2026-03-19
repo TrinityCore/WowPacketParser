042b9077e22ea611fe89b6cf1a214bb1ddeb1d7e
Raknar <raknar900619@gmail.com>
2026-03-19 14:22:48 +0100

    Update Mop Classic parser to 5.5.3 66382

--- WowPacketParser/Enums/Version/UpdateFields.cs
+++ WowPacketParser/Enums/Version/UpdateFields.cs
@@ -1481,6 +1481,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V5_5_3_65988:
                 case ClientVersionBuild.V5_5_3_66128:
                 case ClientVersionBuild.V5_5_3_66290:
+                case ClientVersionBuild.V5_5_3_66382:
                     {
                     return "V5_5_3_64802";
                 }
--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -1699,6 +1699,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V5_5_3_65988:
                 case ClientVersionBuild.V5_5_3_66128:
                 case ClientVersionBuild.V5_5_3_66290:
+                case ClientVersionBuild.V5_5_3_66382:
                 case ClientVersionBuild.V1_15_8_63829:
                 case ClientVersionBuild.V1_15_8_64057:
                 case ClientVersionBuild.V1_15_8_64130:
@@ -2408,6 +2409,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V5_5_3_65988:
                 case ClientVersionBuild.V5_5_3_66128:
                 case ClientVersionBuild.V5_5_3_66290:
+                case ClientVersionBuild.V5_5_3_66382:
                     return true;
                 default:
                     return false;
