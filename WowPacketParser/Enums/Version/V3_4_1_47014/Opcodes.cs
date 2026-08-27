74db402a5a5b6867cf308b7795b42c748790ca40
killerwife <killerwife@gmail.com>
2026-08-27 10:19:00 +0200

    Add more parsable opcodes to 3.4.1

--- WowPacketParser/Enums/Version/V3_4_1_47014/Opcodes.cs
+++ WowPacketParser/Enums/Version/V3_4_1_47014/Opcodes.cs
@@ -23,7 +23,8 @@ namespace WowPacketParser.Enums.Version.V3_4_1_47014
 
             { Opcode.CMSG_ENTER_ENCRYPTED_MODE_ACK, 0x3767 },
             { Opcode.CMSG_AREA_TRIGGER, 0x31D8 },
-            { Opcode.CMSG_CAST_SPELL, 0x3295 },
+            { Opcode.CMSG_CAST_SPELL, 0x329B },
+            { Opcode.CMSG_USE_ITEM, 0x3297 },
         };
 
         private static readonly BiDictionary<Opcode, int> ServerOpcodes = new()
@@ -40,6 +41,20 @@ namespace WowPacketParser.Enums.Version.V3_4_1_47014
             { Opcode.SMSG_GOSSIP_COMPLETE, 0x2A97 },
             { Opcode.SMSG_GOSSIP_MESSAGE, 0x2A98 },
             { Opcode.SMSG_PET_SPELLS_MESSAGE, 0x2C25 },
+            { Opcode.SMSG_QUERY_CREATURE_RESPONSE, 0x2914 },
+            { Opcode.SMSG_QUERY_GAME_OBJECT_RESPONSE, 0x2915 },
+            { Opcode.SMSG_TEXT_EMOTE, 0x2677 },
+            { Opcode.SMSG_ATTACK_START, 0x293C },
+            { Opcode.SMSG_ATTACK_STOP, 0x293D },
+            { Opcode.SMSG_ATTACKER_STATE_UPDATE, 0x2951 },
+            { Opcode.SMSG_SPELL_NON_MELEE_DAMAGE_LOG, 0x2C32 },
+            { Opcode.SMSG_AI_REACTION, 0x26B7 },
+            { Opcode.SMSG_SPELL_INSTAKILL_LOG, 0x2C33 },
+            { Opcode.SMSG_HIGHEST_THREAT_UPDATE, 0x26DB },
+            { Opcode.SMSG_THREAT_CLEAR, 0x26DE },
+            { Opcode.SMSG_THREAT_REMOVE, 0x26DD },
+            { Opcode.SMSG_THREAT_UPDATE, 0x26DC },
+            { Opcode.SMSG_CAST_FAILED, 0x2C57 },
         };
 
         private static readonly BiDictionary<Opcode, int> MiscOpcodes = new BiDictionary<Opcode, int>();
