f88a296b43184c5b41dce7759663ff990d501325
killerwife <killerwife@gmail.com>
2026-09-04 20:10:00 +0200

    Add 2.4.3 SMSG_ATTACKER_STATE_UPDATE handler

---
+++ WowPacketParserModule.V2_4_3_8606/Enums/SpellHitInfo243.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WowPacketParserModule.V2_4_3_8606.Enums
+{
+    [Flags]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public enum SpellHitInfo243
+    {
+        HITINFO_DEBUG          = 0x00000001, // unused - debug flag, probably debugging visuals, no effect in non-ptr client
+        HITINFO_AFFECTS_VICTIM = 0x00000002,
+        HITINFO_OFFHAND        = 0x00000004,
+        HITINFO_UNK3           = 0x00000008, // unused (3.3.5a)
+        HITINFO_MISS           = 0x00000010,
+        HITINFO_ABSORB         = 0x00000020,
+        HITINFO_RESIST         = 0x00000040,
+        HITINFO_CRITICALHIT    = 0x00000080,
+        HITINFO_UNK8           = 0x00000100,
+        HITINFO_UNK9           = 0x00000200,
+        HITINFO_UNK10          = 0x00000400,
+        HITINFO_BLOCK          = 0x00000800,
+        HITINFO_UNK12          = 0x00001000,
+        HITINFO_UNK13          = 0x00002000,
+        HITINFO_GLANCING       = 0x00004000,
+        HITINFO_CRUSHING       = 0x00008000,
+        HITINFO_NOACTION       = 0x00010000,
+        HITINFO_UNK17          = 0x00020000,
+        HITINFO_UNK18          = 0x00040000,
+        HITINFO_SWINGNOHITSOUND = 0x00080000,
+    };
+}
---
+++ WowPacketParserModule.V2_4_3_8606/Parsers/CombatHandler.cs
@@ -0,0 +1,53 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParserModule.V2_4_3_8606.Enums;
+
+namespace WowPacketParserModule.V2_4_3_8606.Parsers
+{
+    public static class CombatHandler
+    {
+        [Parser(Opcode.SMSG_ATTACKER_STATE_UPDATE, ClientVersionBuild.V2_3_0_7561, ClientVersionBuild.V3_0_2_9056)]
+        public static void HandleAttackerStateUpdate(Packet packet)
+        {
+            var hitInfo = packet.ReadInt32E<SpellHitInfo243>("HitInfo");
+            packet.ReadPackedGuid("AttackerGUID");
+            packet.ReadPackedGuid("TargetGUID");
+            packet.ReadInt32("Damage");
+
+            var subDmgCount = packet.ReadByte();
+            for (var i = 0; i < subDmgCount; ++i)
+            {
+                packet.ReadInt32("SchoolMask", i);
+                packet.ReadSingle("Float Damage", i);
+                packet.ReadInt32("Int Damage", i);
+                packet.ReadInt32("Damage Absorbed", i);
+                packet.ReadInt32("Damage Resisted", i);
+            }
+
+            packet.ReadInt32E<VictimStates>("VictimState");
+            packet.ReadInt32("AttackerState");
+            packet.ReadInt32<SpellId>("Melee Spell ID ");
+            packet.ReadInt32("Block Amount");
+
+            if (hitInfo.HasAnyFlag(SpellHitInfo243.HITINFO_DEBUG))
+            {
+                packet.ReadInt32("Armor");
+                packet.ReadSingle("CritRollNeeded");
+                packet.ReadSingle("CombatRoll");
+                packet.ReadSingle("MissChance");
+                packet.ReadSingle("DodgeChance");
+                packet.ReadSingle("ParryChance");
+                packet.ReadSingle("BlockChance");
+                packet.ReadSingle("GlanceChance");
+                packet.ReadSingle("CrushChance");
+                for (var i = 0; i < 5; ++i)
+                {
+                    packet.ReadSingle("MinDamage", i);
+                    packet.ReadSingle("MaxDamage", i);
+                }
+                packet.ReadInt32("SinceLastSwing");
+            }
+        }
+    }
+}
